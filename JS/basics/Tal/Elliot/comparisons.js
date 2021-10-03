de178013fbd0644149724893cae7dd4f7709f030
ElliotKupchik <elliotkupchik@gmail.com>
2021-10-03 13:09:38 +0300

    Update comparisons.js

--- JS/basics/Tal/Elliot/comparisons.js
+++ JS/basics/Tal/Elliot/comparisons.js
@@ -8,3 +8,7 @@ let y = "2";
 
 console.log(x == y);//Old javascript recognized these as equal
 console.log(x === y);//Better to use 3 based on Tal Reccomendation, since it can tell differences in var types
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
