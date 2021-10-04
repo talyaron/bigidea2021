0ef38b977a488deae93edc5bdd21128418b03285
Tal <tal.yaron@gmail.com>
2021-10-04 13:01:40 +0300

    cont

--- JS/basics/jaime/interestrate.js
+++
@@ -1,9 +0,0 @@
-function interestRate(years, principle, rate) {
-    let total = principle + (principle * (rate * years));
-    console.log(total);
-}
-
-//rate must be written as a decimal
-interestRate(5, 100, 0.05);
-interestRate(10, 245, 0.03);
-interestRate(40, 115, 0.04);
