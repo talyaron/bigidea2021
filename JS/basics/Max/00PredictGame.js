aeff7c7a8ac1126331e336e14afc9acce1f22029
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-04 13:47:42 +0300

    games

--- JS/basics/Max/00PredictGame.js
+++ JS/basics/Max/00PredictGame.js
@@ -1,9 +1,10 @@
 let videoGameList = [
-    {Genre:'stratagy', TimePlayed:'94'},
-    {Genre:'fighting', TimePlayed:'384'},
-    {Genre:'stratagy', TimePlayed:'27'},
-    {Genre:'fighting', TimePlayed:'25'},
-    {Genre:'stratagy', TimePlayed:'600'}
+    {Genre:'strategy', TimePlayed:'6'},
+    {Genre:'fighting', TimePlayed:'90'},
+    {Genre:'strategy', TimePlayed:'100'},
+    {Genre:'fighting', TimePlayed:'60'},
+    {Genre:'fighting', TimePlayed:'0'},
+    {Genre:'strategy', TimePlayed:'100'}
 
 ];
 
@@ -12,26 +13,38 @@ let fightingPoints = 0;
 
 
 function GamePredict (videoGameList){
-    for (let x=0; x>videoGameList.length; x++){
-        if (TimePlayed <= 50 && videoGameList[x].Genre === 'strategy' || TimePlayed >= 50 && videoGameList[x].Genre === 'fighting'){
-        fightingPoints++;
+    // for (let x=0; videoGameList.forEach; x++){
+    //     if (videoGameList[x].TimePlayed <= 50 && videoGameList[x].Genre == 'strategy' || videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'fighting'){
+    //     fightingPoints++;
+    //     console.log('fight')
+    //     }
+    //     else {strategyPoints++; console.log('strat')}
+    // }
+    for (let x=0; videoGameList.forEach; x++){
+        if (videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'strategy'){
+            strategyPoints++;
+            console.log('strat');
         }
-        else {strategyPoints++};
-        if (strategyPoints > fightingPoints){
-            return "Strategy Game Preffered";
+        else if(videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'fighting'){
+            fightingPoints++;
+            console.log('fight');
+           }
         }
-        else{ 
-            if( fightingPoints > strategyPoints){
+
+    if (strategyPoints > fightingPoints){
+        return "Strategy Game Preffered";
+        }
+        else if( fightingPoints > strategyPoints){
             return "Fighting Game Preffered";
             }
-            else{
-                return "Tie";
+        else if( fightingPoints === strategyPoints){
+            return "Tie";
             }
 
         }
 
-    }
-}
+
+
 
 console.log(GamePredict(videoGameList))
 
