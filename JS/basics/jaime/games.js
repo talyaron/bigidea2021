96c139cab9bdb7923c419ab53e39d22c9e7c5376
jaimemesq <jaimemesq@outlook.com>
2021-10-04 13:42:37 +0300

    Update games.js

--- JS/basics/jaime/games.js
+++ JS/basics/jaime/games.js
@@ -1,4 +1,9 @@
 let game = {genre = 'fighting', timePlayed = 50};
 let videoGamesList = [
-
+    {genre = 'fighting', timePlayed = 50};
+    {genre = 'strategy', timePlayed = 235};
+    {genre = 'fighting', timePlayed = 72};
+    {genre = 'fighting', timePlayed = 91};
+    {genre = 'strategy', timePlayed = 134};
 ]
+
