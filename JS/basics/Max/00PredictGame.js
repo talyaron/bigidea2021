aec2c1b4c57c5afee62e402e6a936f3280ae57bd
Tal <tal.yaron@gmail.com>
2021-10-04 14:33:35 +0300

    cont

--- JS/basics/Max/00PredictGame.js
+++ JS/basics/Max/00PredictGame.js
@@ -1,10 +1,10 @@
 let videoGameList = [
-    {Genre:'strategy', TimePlayed:'6'},
-    {Genre:'fighting', TimePlayed:'90'},
-    {Genre:'strategy', TimePlayed:'100'},
-    {Genre:'fighting', TimePlayed:'60'},
-    {Genre:'fighting', TimePlayed:'0'},
-    {Genre:'strategy', TimePlayed:'100'}
+    { Genre: 'strategy', TimePlayed: '6' },
+    { Genre: 'fighting', TimePlayed: '90' },
+    { Genre: 'strategy', TimePlayed: '100' },
+    { Genre: 'fighting', TimePlayed: '60' },
+    { Genre: 'fighting', TimePlayed: '0' },
+    { Genre: 'strategy', TimePlayed: '100' }
 
 ];
 
@@ -12,36 +12,34 @@ let strategyPoints = 0;
 let fightingPoints = 0;
 
 
-function GamePredict (videoGameList){
-    // for (let x=0; videoGameList.forEach; x++){
-    //     if (videoGameList[x].TimePlayed <= 50 && videoGameList[x].Genre == 'strategy' || videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'fighting'){
-    //     fightingPoints++;
-    //     console.log('fight')
-    //     }
-    //     else {strategyPoints++; console.log('strat')}
-    // }
-    for (let x=0; videoGameList.forEach; x++){
-        if (videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'strategy'){
+function GamePredict(videoGameList) {
+   
+    videoGameList.forEach(function (game) {
+
+        if (game.TimePlayed >= 50 && game.Genre == 'strategy') {
             strategyPoints++;
             console.log('strat');
         }
-        else if(videoGameList[x].TimePlayed >= 50 && videoGameList[x].Genre == 'fighting'){
+        else if (game.TimePlayed >= 50 && game.Genre == 'fighting') {
             fightingPoints++;
             console.log('fight');
-           }
         }
 
-    if (strategyPoints > fightingPoints){
+
+    })
+
+
+    if (strategyPoints > fightingPoints) {
         return "Strategy Game Preffered";
-        }
-        else if( fightingPoints > strategyPoints){
-            return "Fighting Game Preffered";
-            }
-        else if( fightingPoints === strategyPoints){
-            return "Tie";
-            }
+    }
+    else if (fightingPoints > strategyPoints) {
+        return "Fighting Game Preffered";
+    }
+    else if (fightingPoints === strategyPoints) {
+        return "Tie";
+    }
 
-        }
+}
 
 
 
