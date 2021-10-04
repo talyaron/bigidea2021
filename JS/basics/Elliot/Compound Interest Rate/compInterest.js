2da287650dba6e6af63e5a62a21cf0212e47b6ee
ElliotKupchik <elliotkupchik@gmail.com>
2021-10-04 11:25:50 +0300

    f

--- JS/basics/Elliot/Compound Interest Rate/compInterest.js
+++ JS/basics/Elliot/Compound Interest Rate/compInterest.js
@@ -7,5 +7,14 @@ function compoundInterest(initial, rate, years, perYear){
     final = final * (variableMultiplier ^ exponVal);
     return final;
 }
-
 console.log(`Your compound Interest Value is ${compoundInterest(100, 0.05, 2, 1)}`);
+
+function compound(initial,Interest,frequency,tim){
+    let timeVar=d;
+    while(c*timevar>0){
+        initial=initial*Interest*(frequency/tim);
+        timeVar--;
+    }
+    return initial;
+}
+console.log(compound(a,b,c,d));
