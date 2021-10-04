e90359f2774fa538b64b99f8e83715c9a5fcd6bb
SamuelSpiz <samuelspiz@yahoo.it>
2021-10-04 13:20:22 +0300

    Update 07-games.js

--- JS/Sam/07-games.js
+++ JS/Sam/07-games.js
@@ -1,12 +1,19 @@
 let x = {genre:'strategy', timePlayed:50};
 let videoGameList = [
-    {genre:'strategy', timePlayed:50},
+    {genre:'RPG', timePlayed:50},
     {genre:'fighting', timePlayed:23},
     {genre:'strategy', timePlayed:53},
-    {genre:'strategy', timePlayed:36},
-    {genre:'fighting', timePlayed:201},
+    {genre:'adventure', timePlayed:36},
+    {genre:'simulation', timePlayed:201},
     {genre:'strategy', timePlayed:90},
-    {genre:'strategy', timePlayed:200}
+    {genre:'shooter', timePlayed:200}
 
 ]
-console.log(Math.max.apply(Math,videoGameList.map(function (o){return o.timePlayed;})));
+let maxHours = Math.max.apply(Math,videoGameList.map(function (o){return o.timePlayed;}));
+
+let i=0;
+for (i=0; i<videoGameList.length; i++) {
+    if(maxHours === videoGameList[i].timePlayed) {
+        console.log("You should play a " + videoGameList[i].genre + " game.");
+    }
+}
