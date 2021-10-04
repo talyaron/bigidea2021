336860f8f38b7ab3fede7f9bf4cdb7dd07f5de62
ykonsta <yoav.konsta@gmail.com>
2021-10-04 01:45:51 -0700

    group project percent

--- JS/basics/Yoav/05-objects.js
+++ JS/basics/Yoav/05-objects.js
@@ -10,12 +10,58 @@ function factorial(n) {
 }
 
 
+function gameLikelihood(genre, gamesList) {
+    let sumTime = 0;
+    let genreTime = 0;
+    let percentChance = 0; 
+    for (i = 0; i < gamesList.length; i++) {
+        sumTime += gamesList[i].timePlayed;
+    }
+
+    //console.log(sumTime);
+
+    for (i = 0; i < gamesList.length; i++) {
+        if (gamesList[i].genre == genre)
+            genreTime += gamesList[i].timePlayed;
+    }
+
+    //console.log(genreTime);
+
+    percentChance = 100 * (genreTime / sumTime);
+
+    return (percentChance)
+
+}
+
 
 //array
-const a = [1, 6, 4, 3.45556, 0, 5];
+const a = [1, 2, 3, 4, 5, 6];
 
 
 //code
 a.forEach(function(element) {
     console.log(factorial(element));
 })
+
+
+//Objects
+
+
+
+const videoGamesList = [
+    {genre: 'strategy', timePlayed: 50},
+    {genre: 'fighting', timePlayed: 25},
+    {genre: 'multiplayer', timePlayed: 20},
+    {genre: 'puzzle', timePlayed: 10},
+    {genre: 'multiplayer', timePlayed: 25}
+]
+
+
+console.log(gameLikelihood('strategy', videoGamesList) + "% chance of playing this genre");
+
+
+
+
+
+
+
