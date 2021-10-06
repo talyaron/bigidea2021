0d8660116709e0483e03d3ad957e6e4af326cfcc
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-06 10:55:27 +0300

    funk

--- JS/basics/Max/func.js
+++ JS/basics/Max/func.js
@@ -1,31 +1,54 @@
-function multiply(a, b){
-    return a * b;
+// function multiply(a, b){
+//     return a * b;
 
 
-}
-console.log(multiply(2,5));
+// }
+// console.log(multiply(2,5));
 
 
 
 
-function sayHi(name){
+// function sayHi(name){
 
-    console.log(`hello ${name}`);
-}
+//     console.log(`hello ${name}`);
+// }
 
-sayHi("beki kisha")
+// sayHi("beki kisha")
 
 
-function factorial(n){
-    let final = 1;
-    while(n > 1){
-    final =  final * n;
-    n--;
+// function factorial(n){
+//     let final = 1;
+//     while(n > 1){
+//     final =  final * n;
+//     n--;
        
-    }
+//     }
 
-return(final)
+// return(final)
+// }
+
+// console.log(factorial(5))
+
+
+
+
+// // functions USE THIS 
+
+// const add = (a, b) =>{
+//     return a + b;
+// }
+
+// //call back 
+
+// let arr = [1, 2, 3, 4, 5, 'poop'];
+// arr.forEach((element)=>{
+//     console.log(add(2, element))
+// })
+
+let arr = ['max', "becki", "dan"]
+
+const cb = hello =>{
+    console.log('hello', hello, 'how are you?')
 }
 
-console.log(factorial(5))
-
+arr.forEach(cb)
