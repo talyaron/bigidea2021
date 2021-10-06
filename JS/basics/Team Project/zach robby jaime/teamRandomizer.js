889db710b87b3cff455e36b457bcb48193ba4f81
robertUfberg <robertufberg@gmail.com>
2021-10-06 13:41:33 +0300

    h

--- JS/basics/Team Project/zach robby jaime/teamRandomizer.js
+++ JS/basics/Team Project/zach robby jaime/teamRandomizer.js
@@ -3,11 +3,14 @@ let namelist= ['Jaime','Robby','Elliot','Tal','Eytan','ALex','Max','Szymon','Yoa
 const shuffled = namelist.sort((a, b) => 0.5 - Math.random());
 let length=namelist.length-1;
 
-let arrultimate= [][];
+var arrultimate= [[], []];
 for(let i=0; i<=namelist.length; i++){
 
 }
 
 for (let i=0; i<=length;i++){
-    arrultimate[Math.random()*namelist.length/numTeams][numTeams]
+    for (let k=0; k<=numTeams;k++){
+    let arrultimate=[i][k]arrultimate[Math.random()*namelist.length][Math.random()*namelist.length];
     }
+    }
+    console.log(arrultimate);
