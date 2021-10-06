c3d0f0c8d9cf64d2815297d3e7dba11a7fb088a3
ElliotKupchik <elliotkupchik@gmail.com>
2021-10-06 11:19:08 +0300

    working?

--- JS/basics/Elliot/lessons/04-functions.js
+++ JS/basics/Elliot/lessons/04-functions.js
@@ -31,11 +31,11 @@ const add = (a, b) => {
 }
 
 
-let arr = [1,2,4,9,2,4,8,3,2,'b'];
+/*let arr = [1,2,4,9,2,4,8,3,2,'b'];
 
 arr.forEach((element)=>{
     console.log(add(2, element));
-})
+})*/
 //Note that putting in 'b' resulted in the value 2b being returned
 
 
@@ -50,8 +50,49 @@ let cb = element => {
     console.log(add(2, element));
 }*/
 
-let nameArray = ["Jim", "Bob", "Alex", "Jack", "Ryan"];
+/*let nameArray = ["Jim", "Bob", "Alex", "Jack", "Ryan"];
 
 nameArray.forEach((element) => {
-    console.log("Hello there, " + element);
+    console.log(`Hello there ${element}`);
 })
+*/
+
+let nameArrayMain = ["Jim", "Bob", "Alex", "Jack", "Ryan", "Jesse", "Max", "Arnold", "William", "Evan"];
+let nameArrayRandHalf = [];
+let nameArrayOtherHalf = [];
+
+let randHalfSelector = element => {
+    let halfSize = (element.length / 2);
+    let randSelector;
+    let mainArrayIndex = 0;
+    while((nameArrayRandHalf.length < halfSize) && (nameArrayOtherHalf.length < halfSize)){
+        randSelector = Math.random();
+        if (randSelector >= 0.5) {
+            nameArrayRandHalf.push(element[mainArrayIndex]);
+        }
+        else{
+            nameArrayOtherHalf.push(element[mainArrayIndex]);
+        }
+        mainArrayIndex++;
+    }
+
+    if(nameArrayRandHalf.length < halfSize) {
+        for(let index = mainArrayIndex; index < nameArrayMain.length; index++){
+            nameArrayRandHalf.push(element[mainArrayIndex]);
+        }
+    }
+    else if (nameArrayOtherHalf.length < halfSize){
+        for(let index = mainArrayIndex; index < nameArrayMain.length; index++){
+            nameArrayOtherHalf.push(element[mainArrayIndex]);
+        }
+    }
+
+    nameArrayRandHalf.forEach((element) => {
+        console.log(`Hello there ${element}`);
+    })
+    nameArrayOtherHalf.forEach((element) => {
+        console.log(`Hi ${element}`);
+    })
+}
+
+randHalfSelector(nameArrayMain);
