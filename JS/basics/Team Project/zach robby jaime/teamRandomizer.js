05536161afa77af45076dabbb411399aa4a2fcd2
jaimemesq <jaimemesq@outlook.com>
2021-10-06 16:29:32 +0300

    Update teamRandomizer.js

--- JS/basics/Team Project/zach robby jaime/teamRandomizer.js
+++ JS/basics/Team Project/zach robby jaime/teamRandomizer.js
@@ -1,23 +1,23 @@
-let namelist= ['Jaime','Robby','Elliot','Eytan','Alex','Max','Szymon','Yoav','Samuel','Michael','Becky'];
+let nameList= ['Jaime','Robby','Elliot','Eytan','Alex','Max','Szymon','Yoav','Samuel','Michael','Becky'];
 let numTeams; 
 
-function getNumTeams(memPerTeam) {
-    numTeams = Math.ceil(memPerTeam / namelist.length);
-    return numTeams;
+const shuffled = nameList.sort((a, b) => 0.5 - Math.random());
+
+function splitTeam(array, memPerTeam){
+
+    let teamArray = [], arrultimate= [];
+
+    array.forEach(function(member){
+        if(teamArray.length >= memPerTeam) {
+            arrultimate.push(teamArray);
+            teamArray = [];
+        }
+        teamArray.push(member);
+    })
+    arrultimate.push(teamArray);
+    console.log(arrultimate);
 }
 
-const shuffled = namelist.sort((a, b) => 0.5 - Math.random());
-
-var arrultimate= [];
-
-for(let h=0; h<=numTeams; h++) {
-    for(let k=0; k<=memPerTeam; k++)
-    {
-        arrultimate[h].push(nameList[k]);
-    }
-
-}
-
-for (let i=0; i<=length;i++){
-    arrultimate[Math.random()*namelist.length/numTeams][numTeams]
-    }
+splitTeam(shuffled, 3);
+splitTeam(shuffled, 2);
+splitTeam(shuffled, 4);
