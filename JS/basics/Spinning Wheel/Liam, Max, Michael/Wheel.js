8b57dc087cc46e81933995e592550f8ae272d01d
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-06 13:36:44 +0300

    up

--- JS/basics/Spinning Wheel/Liam, Max, Michael/Wheel.js
+++ JS/basics/Spinning Wheel/Liam, Max, Michael/Wheel.js
@@ -2,5 +2,9 @@ let namelist = ['Alex', 'Becky', 'Elliot', 'Eytan', 'Jaime', 'Liam', 'Max', 'Mic
 
 const scrambler = element =>{
     let NumberOfTeams = 4;
-    let ranNum = Math.floor(Math.random()*NumberOfTeams)
+    let ranNum = (Math.floor(Math.random()*NumberOfTeams))+1
+    let MaxteamNum = namelist.length/NumberOfTeams;
+    console.log(element, 'is on team',ranNum);
 }
+
+namelist.forEach(scrambler);
