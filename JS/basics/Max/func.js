c497a4ab487908882ddb78f4c770ddae5fd47a58
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-06 13:19:56 +0300

    push

--- JS/basics/Max/func.js
+++ JS/basics/Max/func.js
@@ -45,10 +45,46 @@
 //     console.log(add(2, element))
 // })
 
-let arr = ['max', "becki", "dan"]
+// let arr = ['max', "becki", "dan"]
 
-const cb = hello =>{
-    console.log('hello', hello, 'how are you?')
+// const cb = hello =>{
+//     console.log('hello', hello, 'how are you?')
+// }
+
+// arr.forEach(cb)
+
+let nameListFull = ['becky','max','yoav', 'samuel', 'szymon','eytan', 'jamie','robby','elliot', 'alex', 'zach', 'micheal', 'liam'];
+let Team0 = [];
+let Team1 = [];
+let Team2 = [];
+let Team3 = [];
+let TeamOverflow = [];
+
+const scrambler = element =>{
+    let NumberOfTeams = 4;
+    let ranNum = Math.floor(Math.random()*NumberOfTeams)            //Needs to be scalable, maybe use objects assosiated with the number random, also need to fix the part where it just skips players and puts them in overflow 
+    console.log(ranNum)
+    if(ranNum == 0 && Team0.length <= nameListFull.length/NumberOfTeams){
+        Team0.push(element);
+        console.log(Team0);
+    }
+    else if(ranNum == 1 && Team1.length <= nameListFull.length/NumberOfTeams){
+        Team1.push(element);
+        console.log(Team1);
+    }
+    else if(ranNum == 2 && Team2.length <= nameListFull.length/NumberOfTeams){
+        Team2.push(element);
+        console.log(Team2);
+    }
+    else if(ranNum == 3 && Team3.length <= nameListFull.length/NumberOfTeams){
+        Team3.push(element);
+        console.log(Team3);
+    }
+    else{
+        
+    }
+
 }
 
-arr.forEach(cb)
+nameListFull.forEach(scrambler);
+console.log('Team 0------>',Team0, 'Team 1------>',Team1,'Team 2------>',Team2,'Team 3------>',Team3, 'overflow------->',TeamOverflow);
