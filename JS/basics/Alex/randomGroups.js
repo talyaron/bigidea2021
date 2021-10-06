215b4d281dc2cef70905c4c8759ad4f74a8be333
Alex Rothenberg <alex37roth@gmail.com>
2021-10-06 13:42:17 +0300

    pushy push

--- JS/basics/Alex/randomGroups.js
+++ JS/basics/Alex/randomGroups.js
@@ -6,14 +6,14 @@ let ans =[];
 function pickRandom(names,numNames){
     let iLength = names.length;
      for(i=0;i<iLength/numNames;i++){
-     while(tempAns.length < numNames){
-         strPick = Math.floor((Math.random() * names.length))
-         tempAns.push(names[strPick]);
-         names.splice(strPick,1);
-     }
-     ans.push(tempAns);
-     tempAns = [];
- }
+        while(tempAns.length < numNames){
+            strPick = Math.floor((Math.random() * names.length))
+            tempAns.push(names[strPick]);
+            names.splice(strPick,1);
+        }
+        ans.push(tempAns);
+        tempAns = [];
+    }
  }
 
  pickRandom(names, 2);
