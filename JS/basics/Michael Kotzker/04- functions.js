f51e99d34509e472e74ec64fd8f435adc1b5e1d8
Mnkjango <mkotzker493@gmail.com>
2021-10-06 12:00:26 +0300

    Update 04- functions.js

--- JS/basics/Michael Kotzker/04- functions.js
+++ JS/basics/Michael Kotzker/04- functions.js
@@ -1,24 +1,3 @@
-//functions
-
-//function is a segment of the code that do some calculations or sone "job";
-
-//declare function
-
-function multiply(a, b){
-    return a * b;
-}
-
-console.log(multiply(2,5));
-
-//declaration
-function sayHello(name){
-    console.log(`Hello ${name}`);
-}
-
-//invocation
-sayHello("Samuel");
-sayHello("Eitan");
-
 //callback
 let names = ['Michael', 'Simon', 'Elliot', 'Liam', 'Zach', 'Jaime']
 
