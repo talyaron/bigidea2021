0813aad832a47f6889c1f59d60e8a311bbf31eec
robertUfberg <robertufberg@gmail.com>
2021-10-10 13:17:25 +0300

    mj

--- JS/DOM manipulations/tal/Robby/01-simple.js
+++ JS/DOM manipulations/tal/Robby/01-simple.js
@@ -1,4 +1,18 @@
 function handleClick(ev){
-    alert("someone clicked me:)");
+    //alert(`I was clicked by ${ev.target.id} dawg and the class on this button is ${ev.target.className} broski`);
+    console.log(ev)
+    console.log(ev.target.outerText)
+    const buttonText=ev.target.outerText
+    if(buttonText === 'Push Me'){
+        ev.target.innerText = "Pushed";
+    }
+    else{
+        ev.target.innerText =  "Push Me";
+    }
+}
+
+function handleTextChange(ev)
+{
+console.log(ev.target.value);
 
 }
