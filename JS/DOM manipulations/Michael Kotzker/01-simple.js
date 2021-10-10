2cb051790d58d0870e3ba58c0b12569d139e0e45
Mnkjango <mkotzker493@gmail.com>
2021-10-10 15:12:06 +0300

    fgf

--- JS/DOM manipulations/Michael Kotzker/01-simple.js
+++ JS/DOM manipulations/Michael Kotzker/01-simple.js
@@ -1,21 +1,8 @@
-function handleClick(ev){
-    // alert(`I was clicked by ${ev.target.id} and the class on this button is ${ev.target.className}`);
-    console.log(ev)
-    console.log(ev.target.outerText);
-    const buttonText = ev.target.outerText
-    if(buttonText === 'Push Me'){
-
-        //change to Pushed
-        ev.target.innerText ="Pushed";
-    } else {
-        ev.target.innerText ="Push Me";
-    }
-}
 function handleTextChange(ev){
     console.log(ev.target.value)
 }
 let userTurn = 'O'
-function handleClick2(ev){
+function handleClick(ev){
     console.log(ev)
     console.log(ev.target.outerText);
     const Text = ev.target.outerText
