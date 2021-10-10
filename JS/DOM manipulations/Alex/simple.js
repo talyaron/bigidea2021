401af959ef711bd1998e5e3ec738f2082a9edf56
Tal Yaron <tal.yaron@gmail.com>
2021-10-10 13:37:04 +0300

    ready

--- JS/DOM manipulations/Alex/simple.js
+++ JS/DOM manipulations/Alex/simple.js
@@ -2,11 +2,12 @@ function handleClick(ev){
     //alert("I was clicked by " + ev.target.id + " and the classes on this button are " + ev.target.className);
     console.log(ev);
     const buttonText = ev.target.innerText;
+    console.log(buttonText);
     if(buttonText === 'Push Me'){
         
         ev.target.innerText = 'Pushed';
     } else {
-        ev.target.innerText = 'Push me';
+        ev.target.innerText = 'Push Me';
     }
 }
 
