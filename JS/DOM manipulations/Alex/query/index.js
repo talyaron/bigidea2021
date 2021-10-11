769b52de0872e78d0c21ccaafc4a81359a29eb4b
Alex Rothenberg <alex37roth@gmail.com>
2021-10-11 11:34:45 +0300

    working

--- JS/DOM manipulations/Alex/query/index.js
+++ JS/DOM manipulations/Alex/query/index.js
@@ -1,2 +1,15 @@
 const myDiv = document.querySelector('#myDiv');
 console.dir(myDiv);
+myDiv.innerText = "Hello";
+myDiv.style.fontSize= "40px";
+myDiv.style.color="blue";
+myDiv.addEventListener('mouseenter', handleMouseEnter);
+myDiv.addEventListener('mouseleave', handleMouseLeave);
+
+function handleMouseEnter(ev){
+    console.log('Mouse entered');
+}
+
+function handleMouseLeave(ev){
+    console.log('Mouse left');
+}
