d094428ae449793145c0bfdf0705faf5329e7a25
jaimemesq <jaimemesq@outlook.com>
2021-10-11 13:26:44 +0300

    Update javascript.js

--- JS/DOM manipulations/jaime/query/javascript.js
+++ JS/DOM manipulations/jaime/query/javascript.js
@@ -5,18 +5,18 @@ myImage.addEventListener('mouseenter', handleMouseEnter);
 myImage.addEventListener('mouseleave', handleMouseLeave);
 myImage.src = './fallingwithstyle.jpg';
 myImage.style.maxWidth = '200px';
-myImage.style.display = 'none';
+myImage.style.display = 'block';
 
 const fallingAudio = new Audio('falling.mp3');
 
 function handleMouseEnter(ev){
     console.log("Mouse entered!!!!!!!!!!!!!!!!!!!!!!!")
-    myImage.style.display = 'block';
+    myImage.style.top = '0vh';
     fallingAudio.play();
    
 }
 
 function handleMouseLeave(ev){
     console.log("Mouse left!!!!!!!!!!!!!!!!");
-    myImage.style.display = 'none';
+    myImage.style.top = '100vh';
 }
