2f03fa9fe3ebc825750525e2cc0c5bbf385d7bcc
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-10-11 11:48:36 +0300

    cont

--- JS/DOM manipulations/becky/query/index.js
+++ JS/DOM manipulations/becky/query/index.js
@@ -6,9 +6,16 @@ myDiv.style.color = "purple";
 myDiv.addEventListener('mouseenter', handleMouseEnter);
 myDiv.addEventListener('mouseleave', handleMouseLeave);
 
+const myImage = document.querySelector('#myImage');
+myImage.src = ''
+myImage.style.maxWidth = '200px';
+myImage.style.display = 'none';
+
 function handleMouseEnter(ev){
     console.log("mouse entered!!!!!")
+    myImage.style.display = 'block'
 }
 function handleMouseLeave(ev){
     console.log("mouse left")
+    myImage.style.display = 'none'
 }
