290470ba9a9a20432aaa475f66c02845bcad07ef
Tal <tal.yaron@gmail.com>
2021-10-11 11:35:08 +0300

    query 2

--- JS/DOM manipulations/tal/query/index.js
+++ JS/DOM manipulations/tal/query/index.js
@@ -1,2 +1,14 @@
 const myDiv = document.querySelector('#myDiv');
 console.dir(myDiv);
+myDiv.innerText = "Hello";
+myDiv.style.fontSize = "40px";
+myDiv.style.color = "royalblue";
+myDiv.addEventListener('mouseenter', handleMouseEnter);
+myDiv.addEventListener('mouseleave', handleMouseLeave);
+function handleMouseEnter(ev){
+    console.log("Mouse enterd!!!!!!!!!!!!!!!!!!!!!!!")
+}
+
+function handleMouseLeave(ev){
+    console.log("Mouse leaved!!!!!!!!!!!!!!!!");
+}
