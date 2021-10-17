9b3187ccebf373e94c45b7e3dbb529cd4b56ca66
Tal <tal.yaron@gmail.com>
2021-10-17 13:00:13 +0300

    cont

--- JS/DOM manipulations/tal/spinnigWheel/index.js
+++ JS/DOM manipulations/tal/spinnigWheel/index.js
@@ -7,20 +7,25 @@ function spinTheWheel(minRotate, maxRotate) {
     if (number > 6) number = 1;
     console.log(Math.ceil(number))
 
-    return -1* numberOfRotationsInDeg;
+    return {
+        rotate: -1 * numberOfRotationsInDeg,
+        result: Math.ceil(number)
+    }
 
 }
 
 // for(let i = 0; i <50;i++){
 //     console.log((spinTheWheel(4, 6)>=1440 && spinTheWheel(4,6)<=2160)); //=1440- 1960
 // }
-setTimeout(()=>{
-    const wheel = document.getElementById('wheel');
-    const rotation = spinTheWheel(40, 60)
-    wheel.style.transform = `rotate(${-rotation}deg)`;
-},2000)
 
-    const wheel = document.getElementById('wheel');
-    const rotation = spinTheWheel(40, 60)
-    wheel.style.transform = `rotate(${rotation}deg)`;
+
+const wheel = document.getElementById('wheel');
+wheel.addEventListener('transitionend', ev => {
+    console.log('animationend')
+    console.log(ev.target.dataset.result);
+    alert(`The result is ${ev.target.dataset.result}`);
+})
+const rotation = spinTheWheel(4, 6);
+wheel.style.transform = `rotate(${rotation.rotate}deg)`;
+wheel.dataset.result = rotation.result;
 
