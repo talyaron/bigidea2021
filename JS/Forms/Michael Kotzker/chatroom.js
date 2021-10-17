a4ee4f29c1d9c45de58a03b9dabf0c5a98967085
Mnkjango <mkotzker493@gmail.com>
2021-10-17 22:18:12 +0300

    m

--- JS/Forms/Michael Kotzker/chatroom.js
+++ JS/Forms/Michael Kotzker/chatroom.js
@@ -3,6 +3,9 @@ function handleSubmit(ev){
     console.log(ev);
 
     let text = ev.target.elements.text.value;
-    console.log(text);
-
+    console.log('>', text);
+    
+    let chatbox = ev.target.elements.text.value;
+    var chat = document.getElementById('chatbox');
+    chat.innerText = chatbox;
 }
