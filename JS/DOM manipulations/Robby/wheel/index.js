4d53f077f87d6a81ca48592da303638b75c9b1ec
robertUfberg <robertufberg@gmail.com>
2021-10-17 13:05:36 +0300

    n

--- JS/DOM manipulations/Robby/wheel/index.js
+++ JS/DOM manipulations/Robby/wheel/index.js
@@ -1,13 +1,25 @@
 function spinTheWheel(minRotate, maxRotate){
     const numberOfRotationsInDeg = minRotate*360 + (maxRotate-minRotate)*(Math.random()*360);
-    return numberOfRotationsInDeg;
+    const finalDifference = numberOfRotationsInDeg%360;
+    let number= (finalDifference/60)+1 ;
+    if(number>6) number= 1;
+    const finalNum= Math.ceil(number)
+    console.log(finalNum);
+    return {numberOfRotationsInDeg: -1*numberOfRotationsInDeg,
+        number: number}
+    
     
 }
 
+function getSpinNum(number){
+    alert(`You got ${number}`)
+}
 // for(let i =0; i<20; i++){
 //     console.log((spinTheWheel(4,6)>= 1440 && spinTheWheel(4,6) <=2160));
 // }
-
+setTimeout(() =>{
 const wheel= document.getElementById('wheel');
 const rotation= spinTheWheel(4,6);
-wheel.style.transform= `rotate(${rotation}deg)`;
+wheel.style.transform= `rotate(${rotation.numberOfRotationsInDeg}deg)`;
+wheel.onanimationend=getSpinNum(rotation.number);
+}, 2000)
