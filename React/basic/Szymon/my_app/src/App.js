2c6c372e60dd772ca45504014b59f1f7d8160d4c
SzymontheDestroyer <shadowbrzezinksi@gmail.com>
2021-10-18 04:15:50 -0700

    A
    A

--- React/basic/Szymon/my_app/src/App.js
+++ React/basic/Szymon/my_app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Szymon from './Game.jpg'
 function Blog(){
   return(
     <div className="Blog">
@@ -10,6 +11,7 @@ function Blog(){
       <b>
         My favorite video game is Monster Hunter World. This is my favorite because I enjoy hunting massive beasts and showing them whose boss. I most enjoy obtaining their materials to make better gear and then hunt other mosnters.
       </b>
+      <img src={Szymon}/>
     </div>
   )
 }
