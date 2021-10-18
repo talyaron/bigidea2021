521b4f3eb93c7b543df073a774d30e3a7d2ac858
Alex Rothenberg <alex37roth@gmail.com>
2021-10-18 13:43:27 +0300

    blog post

--- React/basic/Alex/my_basic_app/src/App.js
+++ React/basic/Alex/my_basic_app/src/App.js
@@ -7,9 +7,13 @@ const abc = 123;
 function App() {
   return (
    <div className='gold'>
-     <img src={scuba}/>
+     <img className='image' src={scuba}/>
      <h1>My wifi wont let me download a new image, but I really like scuba diving :)</h1>
-    <div>Boo {abc}</div>
+    <div className='paragraph'>I learned how to scuba dive after my sister got her certification and began scuba diving with our dad. it sounded like so much fun and there were so many interesting animals they were able to see I wanted to join them</div>
+    <br></br>
+    <div className='paragraph'>I love scuba diving for a lot of reasons, its a lot of fun to feel almost weightless and swim around with my friends, sometimes not knowing whats up and whats down</div>
+    <br></br>
+    <div className='paragraph'>It also feels like an entirely different world, swimming with schools of fish, huge groper fish, sea turtles, and even once sharks were some of the most unique experiences of my life</div>
    </div>
 
   );
