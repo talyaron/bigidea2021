2f067c63f21829d9cecbc9c28c3890192b6fcf7e
Mnkjango <mkotzker493@gmail.com>
2021-10-18 14:29:50 +0300

    m

--- React/basic/Michael Kotzker/my_basic_app/src/App.js
+++ React/basic/Michael Kotzker/my_basic_app/src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
-import tal from './tal.png';
+import D2 from './D2logo.jpg';
 import './App.css';
 
 const abc = 123;
 
 function App() {
   return (
-    <div className="box">
-      <img src={tal} />
-      <h1>Boo {abc}</h1>
-      <div>Hi all</div>
-    </div>
+   <div className='gold'>
+     <img className='image' src={D2}/>
+     <h1>Test</h1>
+    <div className='paragraph'>Test</div>
+    <br></br>
+    <div className='paragraph'>Test</div>
+    <br></br>
+    <div className='paragraph'>Test</div>
+   </div>
+
   );
 }
 
