35509ac7b4dc4950fa8d4029b4e5853608f150dc
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-25 00:19:30 +0300

    :)

--- React/basic2/becky/src/App.js
+++ React/basic2/becky/src/App.js
@@ -4,19 +4,20 @@ import lego from './lego.jpg'
 import thomas from './thomas.jpeg'
 import rebecca from './ribecca.jpeg'
 import nerf from './nerf.jpg'
-
+import max from './max.png'
+import becky from './me.jpeg'
 import './App.css';
 
 
 function App(){
   return(
     <div className="cont">
-      <div className = 'pic' id = 'pic1'  alt='' style={{ backgroundImage: `url(${hotweel})` }}></div>
-      <div className = 'pic'  id = 'pic2'  alt=''style={{ backgroundImage: `url(${kitchen})` }}></div>
-      <div className = 'pic'  id = 'pic3'  alt=''style={{ backgroundImage: `url(${lego})` }}></div>
-      <div className = 'pic'  id = 'pic4'  alt=''style={{ backgroundImage: `url(${thomas})` }}></div>
-      <div className = 'pic'  id = 'pic5'  alt=''style={{ backgroundImage: `url(${rebecca})` }}></div>
-      <div className = 'pic'  id = 'pic6' alt=''style={{ backgroundImage: `url(${nerf})` }}></div>
+      <div className = 'pic' id = 'pic1'  alt=''></div>
+      <div className = 'pic'  id = 'pic2'  alt=''></div>
+      <div className = 'pic'  id = 'pic3'  alt=''></div>
+      <div className = 'pic'  id = 'pic4'  alt=''></div>
+      <div className = 'pic'  id = 'pic5'  alt=''></div>
+      <div className = 'pic'  id = 'pic6' alt=''></div>
       
     </div>
   );
