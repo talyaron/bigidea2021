55e9093a8c9744da3c741ea0303520ad929014b7
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-24 11:39:31 +0300

    :)

--- React/basic2/Max/src/App.js
+++ React/basic2/Max/src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import shark from './shark.jpg';
-import csgo from './csgo.jpg';
+import greta from './madGreta.jpg';
+import nap from './napolian.jpg';
+import singa from './singapore.jpg';
 
 
 
 function App() {
   return (
-    <div className="box">
-        <img src={shark}/>
-        <h1>Hi my name is Max Blog post </h1>
-        <img src={csgo} className='CSGO'/>
-        <div className ="bodypar">this picture is not of me, however look how SWAG he looks.<br/>
-        this has nothing to do with my favorate game but what it is, is my profile pic <br/>
-        when I play csgo. Csgo is my favorate game because im good at it :)
-        </div>
+    <div className="cont">
+       
+       <img src = {greta} className = 'photo'></img>
+       <img src = {nap} className = 'photo'></img>
+       <img src ={singa} className = 'photo'></img>
     </div>
   );
 }
