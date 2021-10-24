6e10aa78c40ee6f53b4d4f86780f6c1a5b91648a
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-10-24 11:57:36 +0300

    cont

--- React/basic2/Max/src/App.js
+++ React/basic2/Max/src/App.js
@@ -12,9 +12,11 @@ function App() {
        <img src = {greta} className = 'photo' id = "photo1"></img>
        <img src = {nap} className = 'photo' id = "photo2"></img>
        <img src = {singa} className = 'photo' id = "photo3"></img>
+       
        <div className = 'text' id = "text1">Greta is Back at it again tweeting "In the German Rhineland, six further villages are planned to 
        be destroyed to expand the largest coal mine of Europe. This is absurd. Today we visited those defending the villages and 
        fighting for the coal to stay in the ground. We are standing with you. #AlleDörferBleiben" </div>
+
        <div className = 'text' id = "text2">par about napolian</div>
        <div className = 'text' id = "text3">par about singa</div>
        <a className = 'link' id = "link1">link 1</a>
