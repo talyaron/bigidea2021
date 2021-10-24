87f61b1c453ed9b2c06b72eede51f0479e22dd02
Mnkjango <mkotzker493@gmail.com>
2021-10-24 11:34:36 +0300

    Update App.js

--- React/basic2/Michael Kotzker/my_basic_app/src/App.js
+++ React/basic2/Michael Kotzker/my_basic_app/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>News</h1>
-      <div className='Article1'>Test</div>
-      <div className="Article2">Test</div>
-      <div className="Article3">Test</div>
+      <div className="Images"></div>
+      <div className='Article1'>Bitcoin</div>
+      <div className="Article2">Marijuana</div>
+      <div className="Article3">Queen Elizabeth</div>
     </div>
   );
 }
