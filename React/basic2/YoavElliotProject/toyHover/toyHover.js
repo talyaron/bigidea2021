9074b45687ce7889be1e174ae4b9c3b562fdfd76
ykonsta <yoav.konsta@gmail.com>
2021-10-24 11:33:15 -0700

    khbhjnh

--- React/basic2/YoavElliotProject/toyHover/toyHover.js
+++ React/basic2/YoavElliotProject/toyHover/toyHover.js
@@ -2,7 +2,12 @@
 import imageA from './geoGuess.png';
 import imageB from './geoGuess.png';
 import imageC from './geoGuess.png';
+<<<<<<< Updated upstream:React/basic2/YoavElliotProject/toyHover/toyHover.js
 import './toyHover.css';
+=======
+import imageA from './pokerpic.jpg';
+import './App.css';
+>>>>>>> Stashed changes:React/basic2/YoavElliotProject/toyHover/App.js
 
 const abc = 123;
 
@@ -16,6 +21,16 @@ function App() {
 
       
       <div className="para1">I am here today to talk to you about GeoGuesser. I love GeoGuesser. It is a fun and engaging game that teaches people about local regions on other parts of the planet, alongside how cultural differences can change the look and feel of society. Trying to identifty which region of the world you are in, though difficult, gradually broadens your understanding of the global landscape.</div>
+
+      <div className = "box"></div>
+      <div className = "box2"></div>
+      <div className = "box3"></div>
+      <div className = "box4"></div>
+      <div className = "box5"></div>
+      <div className = "box6"></div>
+      <div className = "box7"></div>
+      <div className = "box8"></div>
+      <div className = "box9"></div>
     </div>
   );
 }
