0371e50be8f216bfd629228efc890060cd02a6f6
Mnkjango <mkotzker493@gmail.com>
2021-10-24 11:19:44 +0300

    Update App.js

--- React/basic2/Michael Kotzker/my_basic_app/src/App.js
+++ React/basic2/Michael Kotzker/my_basic_app/src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import D2 from './D2logo.jpg';
 import './App.css';
 
-const abc = 123;
-
 function App() {
   return (
-   <div className=''>
-     Hi
-   </div>
-
+    <div>
+      <div className=''>
+      
+      </div>
+    </div>
   );
 }
 
