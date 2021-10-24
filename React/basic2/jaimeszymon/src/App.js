b149215da493201efba4a86ae813065a28f7a376
jaimemesq <jaimemesq@outlook.com>
2021-10-24 16:14:50 +0300

    s

--- React/basic2/jaimeszymon/src/App.js
+++ React/basic2/jaimeszymon/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import beyblade from './beyblade.jpeg';
 import catan from './catan.jpeg';
 import cube from './cube.jpg';
-import dnd from './dnd.png';
+import dnd from './dnd.jpg';
 import ds from './ds.png';
 import legos from './legos.jpg';
 
@@ -11,36 +11,30 @@ function App() {
   return (
     <div>
     <div className="body">
-    <p className="header">Favorite Toys/Games</p>
+    <h1 className="header">Favorite Toys/Games</h1>
 
       <div className="box one">
       <h3>Catan</h3>
-      <img url=""></img>
       </div>
 
       <div className="box two">
       <h3>DS</h3>
-      <img></img>
       </div>
 
       <div className="box three">
       <h3>Rubik's Cube</h3>
-      <img></img>
       </div>
 
       <div className="box four">
       <h3>Legos</h3>
-      <img></img>
       </div>
       
       <div className="box five">
       <h3>Dungeons and Dragons</h3>
-      <img></img>
       </div>
       
       <div className="box six">
       <h3>Beyblade</h3>
-      <img></img>
       </div>
 
     </div>
