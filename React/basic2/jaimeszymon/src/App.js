a742c9efa50677a6c55537d6e566f1dc1577a1c9
jaimemesq <jaimemesq@outlook.com>
2021-10-24 15:40:19 +0300

    a

--- React/basic2/jaimeszymon/src/App.js
+++ React/basic2/jaimeszymon/src/App.js
@@ -1,16 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import beyblade from './beyblade.jpeg';
+import catan from './catan.jpeg';
+import cube from './cube.jpg';
+import dnd from './dnd.png';
+import ds from './ds.png';
+import legos from './legos.jpg';
 
 function App() {
   return (
-    <div className="main">
-            <div className="box">Catan</div>
-            <div className="box">DS</div>
-            <div className="box"></div>
-            <div className="box">Legos</div>
-            <div className="box">Beyblade</div>
-            <div className="box">Dungeons and Dragons</div>
-        </div>
+    <div>
+    <div className="body">
+    <p className="header">Favorite Toys/Games</p>
+
+      <div className="box one">
+      <h3>Catan</h3>
+      <img url=""></img>
+      </div>
+
+      <div className="box two">
+      <h3>DS</h3>
+      <img></img>
+      </div>
+
+      <div className="box three">
+      <h3>Rubik's Cube</h3>
+      <img></img>
+      </div>
+
+      <div className="box four">
+      <h3>Legos</h3>
+      <img></img>
+      </div>
+      
+      <div className="box five">
+      <h3>Dungeons and Dragons</h3>
+      <img></img>
+      </div>
+      
+      <div className="box six">
+      <h3>Beyblade</h3>
+      <img></img>
+      </div>
+
+    </div>
+    </div>
   );
 }
 
