56c56aca42214a64f870b6975f7f18b4e4ab984b
SzymontheDestroyer <91409086+SzymontheDestroyer@users.noreply.github.com>
2021-10-24 14:23:37 +0300

    [p

--- React/basic2/Robby/src/App.js
+++ React/basic2/Robby/src/App.js
@@ -8,19 +8,17 @@ const game = 'World Of Warcraft III';
 function App() {
   return (
     <div className="box">
-      <div className="image" style={{backgroundImage: `url(${cat})`}} />
-      <div className="image" style={{backgroundImage: `url(${hi})`}} />
-      <div className="image"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image1"  />
+      <div className="image2" style={{backgroundImage: `url(${hi})`}} />
+      <div className="image3"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image4"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image5"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image6"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image7"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image8"style={{backgroundImage: `url(${minecraft})`}} />
+      <div className="image9"style={{backgroundImage: `url(${minecraft})`}} />
 
-      <h1>Robby and Eytan create first news article</h1>
 
-      <h1>Second news article created, scientists baffled.</h1>
-      <h1>Another headline created, science is deemed complete worldwide</h1>
-      <div>"we did it" says comp sci majors, confused to what they were being asked</div>
-      <div>
-        "they're just too advanced for us" Pentagon head scientist says
-      </div>
-      <div>"Now that's just crazy" Bill Nye exclaimed in jealousy.</div>
     </div>
   );
 }
