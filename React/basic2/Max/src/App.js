effefb9a2a42e460241c776f6f7cb99624f4b7ae
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-24 11:50:11 +0300

    name ele

--- React/basic2/Max/src/App.js
+++ React/basic2/Max/src/App.js
@@ -12,12 +12,14 @@ function App() {
        <img src = {greta} className = 'photo'></img>
        <img src = {nap} className = 'photo'></img>
        <img src = {singa} className = 'photo'></img>
-       <div className = 'text'></div>
-       <div className = 'text'></div>
-       <div className = 'text'></div>
-       <a className = 'link'></a>
-       <a className = 'link'></a>
-       <a className = 'link'></a>
+       <div className = 'text'>Greta is Back at it again tweeting "In the German Rhineland, six further villages are planned to 
+       be destroyed to expand the largest coal mine of Europe. This is absurd. Today we visited those defending the villages and 
+       fighting for the coal to stay in the ground. We are standing with you. #AlleDörferBleiben" </div>
+       <div className = 'text'>par about napolian</div>
+       <div className = 'text'>par about singa</div>
+       <a className = 'link'>link 1</a>
+       <a className = 'link'>link 2</a>
+       <a className = 'link'>link 3</a>
     </div>
   );
 }
