d70edbc84077ffb8ba120628f7e731ed40fdea4a
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-24 11:42:22 +0300

    header

--- React/basic2/Max/src/App.js
+++ React/basic2/Max/src/App.js
@@ -8,7 +8,7 @@ import singa from './singapore.jpg';
 function App() {
   return (
     <div className="cont">
-       
+       <h1 className = 'Header'>News</h1>
        <img src = {greta} className = 'photo'></img>
        <img src = {nap} className = 'photo'></img>
        <img src ={singa} className = 'photo'></img>
