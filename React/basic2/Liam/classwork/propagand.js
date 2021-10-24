b57921856e7acd265a55d4d74903f83eddb164b7
Tacoman611 <91414112+Tacoman611@users.noreply.github.com>
2021-10-24 11:57:28 +0300

    hi

--- React/basic2/Liam/classwork/propagand.js
+++ React/basic2/Liam/classwork/propagand.js
@@ -1,18 +1,19 @@
-import '';
-import smash from '';
+import './propaganda.css';
+import wheel from './img/wheel.jpg';
+import greta from './img/greta.jpg';
+import balence from './img/balence.jpg';
 function prop() {
     return (
         <div>
-            <h2 className='h2'>Liam's favorite video games</h2>
-            <img src={smash} alt="smash" />
+            <>
             <div>
-                <div></div>
+                <div><img src={wheel} alt="wheel"></img> </div>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
             <div>
-                <div></div>
+                <div><img src={greta} alt="greta"></div>
                 <div></div>
                 <div></div>
                 <div></div>
