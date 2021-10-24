e12b4d03ec3bf6f7cc1b86744bc1daecaa65d57d
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-24 21:53:11 +0300

    e

--- React/basic2/becky/src/App.js
+++ React/basic2/becky/src/App.js
@@ -11,12 +11,12 @@ import './App.css';
 function App(){
   return(
     <div className="cont">
-      <img className = 'pic' src ={hotweel} />
-      <img className = 'pic' src ={kitchen} />
-      <img className = 'pic' src ={lego} />
-      <img className = 'pic' src ={thomas} />
-      <img className = 'pic' src ={ribecca} />
-      <img className = 'pic' src ={nerf} />
+      <img className = 'pic' src ={hotweel} id = 'pic1' />
+      <img className = 'pic' src ={kitchen} id = 'pic2' />
+      <img className = 'pic' src ={lego} id = 'pic3' />
+      <img className = 'pic' src ={thomas} id = 'pic4' />
+      <img className = 'pic' src ={ribecca} id = 'pic5' />
+      <img className = 'pic' src ={nerf} id = 'pic6' />
       
     </div>
   );
