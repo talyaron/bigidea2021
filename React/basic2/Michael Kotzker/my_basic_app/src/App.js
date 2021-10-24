a36ae13fa3d1a84e9ba29043102dc7cac7ab12f1
Mnkjango <mkotzker493@gmail.com>
2021-10-24 11:29:03 +0300

    m

--- React/basic2/Michael Kotzker/my_basic_app/src/App.js
+++ React/basic2/Michael Kotzker/my_basic_app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import D2 from './D2logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>News</h1>
-      <div className='Article1'></div>
-      <div className="Article2"></div>
-      <div className="Article3"></div>
+      <div className='Article1'>Test</div>
+      <div className="Article2">Test</div>
+      <div className="Article3">Test</div>
     </div>
   );
 }
