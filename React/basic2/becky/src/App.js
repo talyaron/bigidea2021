25f1c035ec95b94c1c4db8d683f7b4bd030a7762
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-10-24 11:40:05 +0300

    cont

---
+++ React/basic2/becky/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import me from './me.jpeg';
+import cards from './cards.jpeg'
+
+const abc = 123
+
+function App(){
+  return(
+    <div className=" ">
+      hi
+    </div>
+  );
+}
+
+export default App;
