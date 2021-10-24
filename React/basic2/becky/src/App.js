c6597c760ba008860e289df2796990308bef89b2
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-10-24 21:48:07 +0300

    pics

--- React/basic2/becky/src/App.js
+++ React/basic2/becky/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import me from './me.jpeg';
-import cards from './cards.jpeg'
+import hotweel from './hotweels.jpeg'
+import kitchen from './kitchen.png'
+import lego from './lego.jpg'
+import thomas from './thomas.jpeg'
+import ribecca from './ribecca.jpeg'
+import nerf from './nerf.jpg'
 
-const abc = 123
+import './App.css';
+
 
 function App(){
   return(
-    <div className="container">
-      hi
+    <div className="cont">
+      <img className = 'pic' src ={hotweel} />
+      <img className = 'pic' src ={kitchen} />
+      <img className = 'pic' src ={lego} />
+      <img className = 'pic' src ={thomas} />
+      <img className = 'pic' src ={ribecca} />
+      <img className = 'pic' src ={nerf} />
+      
     </div>
   );
 }
