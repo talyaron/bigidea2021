38fe12d6a4e509bc5816525e931b7b433b063078
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-10-25 13:03:44 +0300

    uu
    kk

---
+++ React/02-components/sam/src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css'
+import image from './download.jpg'
+function Card(){
+    return (
+    <div className='card'>Card
+         <img src={image} alt='bla' />
+
+         </div>
+    )
+}
+
+export default Card;
