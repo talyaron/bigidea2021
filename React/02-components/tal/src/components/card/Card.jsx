6ad19aa3b950b75d532d13f36fc989a93c693b94
Tal <tal.yaron@gmail.com>
2021-10-25 14:15:04 +0300

    Update Card.jsx

--- React/02-components/tal/src/components/card/Card.jsx
+++ React/02-components/tal/src/components/card/Card.jsx
@@ -3,6 +3,9 @@ import tal from '../../img/tal.png'
 
 //Becky
 //Robby
+//Tal 
+// https://www.google.com/search?q=tal+yaron&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiuzYOrt-XzAhVL_rsIHV76AZIQ_AUoAXoECAEQAw&biw=1152&bih=753&dpr=1#imgrc=m_lRqzeT_YUBRM
+
 
 function Card(props) {
     console.log(props)
