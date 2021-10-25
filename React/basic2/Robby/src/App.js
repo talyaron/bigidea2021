e09d57f3f8bce4437f83d670fe89b2a921edaeb5
Eytan75 <47122145+Eytan75@users.noreply.github.com>
2021-10-25 09:25:20 +0200

    Update App.js

--- React/basic2/Robby/src/App.js
+++ React/basic2/Robby/src/App.js
@@ -9,13 +9,13 @@ function App() {
   return (
     <div className="box">
       <div className="image1"  />
-      <div className="image2"/>
+      <div className="image6"/>
       <div className="image3" />
-      <div className="image4" />
-      <div className="image5" />
-      <div className="image6" />
-      <div className="image7" />
       <div className="image8" />
+      <div className="image5" />
+      <div className="image2" />
+      <div className="image4" />
+      <div className="image7" />
       <div className="image9" />
 
 
