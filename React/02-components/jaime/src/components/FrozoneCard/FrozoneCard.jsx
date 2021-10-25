1c732437bebbfffeba5b56d0ebd4155059eed5be
jaimemesq <jaimemesq@outlook.com>
2021-10-25 14:23:24 +0300

    kjb

--- React/02-components/jaime/src/components/FrozoneCard/FrozoneCard.jsx
+++ React/02-components/jaime/src/components/FrozoneCard/FrozoneCard.jsx
@@ -1,9 +1,10 @@
 import './FrozoneCard.css';
 
-function FrozoneCard(){
+function FrozoneCard(props){
     return(
         <div>
             <div className="card"><br></br><br></br><br></br>Samuel L. Jackson</div>
+            <p>Gender: {props.gender}</p>
         </div>
     )
 }
--- React/02-components/jaime/src/components/MovieCard/MovieCard.jsx
+++ React/02-components/jaime/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,13 @@
 import './MovieCard.css';
 
-function MovieCard(){
+function MovieCard(props){
+    console.log(props);
     return(
         <div>
-            <div className="card"><br></br><br></br><br></br></div>
+            <div className="card"><br></br><br></br><br></br>
+            <p>Name: {props.name}</p>
+            <p>Gender: {props.gender}</p>
+            </div>
         </div>
     )
 }
