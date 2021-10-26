6fe8a99d6b5dc1f11c9183080018d24d916c9087
Mnkjango <mkotzker493@gmail.com>
2021-10-26 19:56:52 +0300

    Update App.js

--- React/02-components/michael/src/App.js
+++ React/02-components/michael/src/App.js
@@ -15,11 +15,11 @@ function App() {
         </p>
         <br></br>
         <p>
-          I met Ella in December 2018, on a Discord server about a book series we both liked at the time. We quickly became friends and have talked to each other and playedg games together often since then. Last Summer, we made plans to meet up with our families - my Dad lives in Florida, and so does Ella, so my Dad would take me to see her - , but due to scheduling differences we could not.
+          ELLA: I met Ella in December 2018, on a Discord server about a book series we both liked at the time. We quickly became friends and have talked to each other and playedg games together often since then. Last Summer, we made plans to meet up with our families - my Dad lives in Florida, and so does Ella, so my Dad would take me to see her - , but due to scheduling differences we could not.
         </p>
         <br></br>
         <p>
-          I met Hope in October of last year. I was discussing a world I had created in a server I was on, and requested anyone interested in reading about it DM me. She did, and after reading it we found we had many things in common. We have been friends since then, and she has been kind enough to draw pictures of things I request - she is an excellent artist (an example of her art is provided below). Hope lives in Alabama, so visiting her is unfortunately unlikely.
+          HOPE: I met Hope in October of last year. I was discussing a world I had created in a server I was on, and requested anyone interested in reading about it DM me. She did, and after reading it we found we had many things in common. We have been friends since then, and she has been kind enough to draw pictures of things I request - she is an excellent artist (an example of her art is provided below). Hope lives in Alabama, so visiting her is unfortunately unlikely.
         </p>
         <img src={Tartarus} alt="bla" />
       </header>
