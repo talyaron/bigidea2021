8be038c76937281b2d8e1b48a514d8e3a5068880
ElliotKupchik <elliotkupchik@gmail.com>
2021-10-26 21:04:04 +0300

    eee

--- React/02-components/elliot/src/App.js
+++ React/02-components/elliot/src/App.js
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        <Card name='Elliot' gender='Male'/>
-        <Card name='Elon' gender='Male'/>
-        <Card name='Jessica' gender='Female'/>
+        <Card name='Chris' gender='Male' image='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg/440px-Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg'/>
+        <Card name='Elon' gender='Male' image='https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg/440px-Elon_Musk_Royal_Society_%28crop1%29.jpg'/>
+        <Card name='Harry' gender='Male' image='https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg'/>
       </header>
 
       <header className='App-actorDisplay'>
@@ -21,7 +21,7 @@ function App() {
         <ActorLine />
         <ActorMovie />
       </header>
-      
+
     </div>
   );
 }
--- React/02-components/elliot/src/components/card/Card.jsx
+++ React/02-components/elliot/src/components/card/Card.jsx
@@ -5,8 +5,8 @@ function Card(props) {
     console.log(props.name)
     return (
         <div className = 'card'>
-            <img src={elliot} alt = 'avg' />
-            <p>{props.name}</p>
+            <img src={props.image} alt = 'avg' />
+            <p>Name: {props.name}</p>
             <p>Gender: {props.gender}</p>
         </div>
     )
