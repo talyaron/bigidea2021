62f8764a04748cbf4e85ccda649291a2c19781b4
Mnkjango <mkotzker493@gmail.com>
2021-10-27 14:12:41 +0300

    m

--- Recap/DomManipulations/Michael/Game.js
+++ Recap/DomManipulations/Michael/Game.js
@@ -1,5 +1,32 @@
 const OddEven = document.getElementById("OddEven");
+const Clicker = document.getElementById("Clicker");
 
-OddEven.addEventListener("click", ()=>{
-    
-})
+console.dir(OddEven);
+console.dir(Clicker);
+
+Clicker.addEventListener("click", changeTexttoEvenorOdd);
+
+function changeTexttoEvenorOdd(ev){
+    //change text of header
+    OddEven.innerText = getOddorEven();
+}
+
+function getOddorEven(){
+    const number = Math.ceil(Math.random()*2);
+
+    if(number == 1){
+        return "Odd"
+    } else{
+        return "Even"
+    }
+}
+
+//Checking
+//let Odd = 0, Even = 0;
+//for(let i = 0; i<100; i++){
+//    let answer = getOddorEven();
+//    if(answer == "Odd") Odd++;
+//    else if (answer == "Even") even++
+//}
+
+//console.log(Odd, Even);
