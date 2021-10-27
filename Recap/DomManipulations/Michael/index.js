7c5290f3a58c8759653191e91f54ed808da5400b
Mnkjango <mkotzker493@gmail.com>
2021-10-27 11:26:13 +0300

    m

--- Recap/DomManipulations/Michael/index.js
+++ Recap/DomManipulations/Michael/index.js
@@ -8,3 +8,13 @@ console.log(root)
 setInterval(() => {
     root.innerHTML++
 }, 1000);
+
+function handleSubmit(ev){
+    ev.preventDefault();
+    console.log(ev);
+
+    const text= ev.target.elements.text.value;
+    let newText = document.getElementsByTagName('div')[0];
+    
+    newText.innerHTML += text + '<br>';
+}
