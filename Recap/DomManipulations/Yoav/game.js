66a8276d8c20fda310d1c1e595a14e918b7ca023
Tal <tal.yaron@gmail.com>
2021-10-27 13:43:43 +0300

    sdfg

--- Recap/DomManipulations/Yoav/game.js
+++ Recap/DomManipulations/Yoav/game.js
@@ -1,7 +1,7 @@
 const clicker = document.getElementById('clicker');
-const oddEven = document.getElementById('oddEven');
+const oddEven = document.getElementById('oddEven')
 
-oddEven.addEventListener('click',()=>{
+clicker.addEventListener('click',()=>{
     //const random = Math.floor(Math.random()*2);
     console.log("hi");
     
