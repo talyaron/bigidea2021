1fb26cc29044104a8e2f0eb61c6d4a7b8ca87273
Tal <tal.yaron@gmail.com>
2021-10-27 10:40:28 +0300

    recap

--- React/02-components/tal/src/App.js
+++ React/02-components/tal/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
        
         <Card name='tal' gender='male' image='https://www.google.com/search?q=tal+yaron&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiuzYOrt-XzAhVL_rsIHV76AZIQ_AUoAXoECAEQAw&biw=1152&bih=753&dpr=1#imgrc=m_lRqzeT_YUBRM'/>
-        <Card name='robbert' gender='male' />
+        <Card name='robbert' gender='male' image='file:///Users/robert/Desktop/pictures/5d71018a-8a92-4d86-917e-00e5657ce208.jpg'/>
         <Card name='becky' gender='female' image='https://farm66.static.flickr.com/65535/51266845914_37d6047f37.jpg' />
        
 
