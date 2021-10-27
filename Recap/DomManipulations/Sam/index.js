f7ab5f6ab8b4a65a9ee53fae9bffb5d99ba9a7cb
SamuelSpiz <samuelspiz@yahoo.it>
2021-10-27 10:58:02 +0300

    cc

---
+++ Recap/DomManipulations/Sam/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('mouseenter', () => { document.body.style.background = 'red'})
+document.addEventListener('mouseleave', () => {document.body.style.background = 'white'})
+
+
+//timer
+setInterval(() => {
+
+}, 1000)
