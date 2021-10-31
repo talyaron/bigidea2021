5bdbcdd14c42f3fe3ac71f4c7dd48a742ff56750
robertUfberg <robertufberg@gmail.com>
2021-10-31 12:10:08 +0200

    sda

---
+++ React/03-state/robby/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Counter from './components/Counter/Counter'
+import TextInput from './components/TextInput/TextInput'
+
+function App() {
+  return (
+    <div className="App">
+
+        <Counter />
+        <TextInput />
+
+
+    </div>
+  );
+}
+
+export default App;
