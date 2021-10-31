b9bc2ea46d7d798b7710088b94f87d198eeadb47
Mnkjango <mkotzker493@gmail.com>
2021-10-31 11:44:57 +0200

    m

--- React/03-state/michael/src/App.js
+++ React/03-state/michael/src/App.js
@@ -9,15 +9,17 @@ import Counter from './components/Counter/Counter'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
 
         <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
 
-      </header>
+
     </div>
   );
 }
