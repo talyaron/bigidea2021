2e4ec3c606a59a01c44d19d8c81dcb411e325e68
Tal <tal.yaron@gmail.com>
2021-10-31 11:33:57 +0200

    cont

--- React/03-state/tals/src/components/Counter/Counter.jsx
+++ React/03-state/tals/src/components/Counter/Counter.jsx
@@ -1,7 +1,21 @@
-function Counter(){
-    return(
-        <div>Counter</div>
+import { useState } from 'react';
+
+//we will use useState hook
+
+function Counter() {
+    //[state, setterOfState] = useState (initial value)
+    const [counter, setCounter] = useState(1);
+    function handleClick() {
+        setCounter(counter + 1)
+    }
+    
+
+    return (
+        <div onClick={handleClick}>Counter: {counter}</div>
     )
 }
+
+
+
 
 export default Counter;
