2393c0161c130a239bddf37f7aff9c784b7c91f7
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-10-31 20:32:23 +0200

    cont

--- React/03-state/becky/src/components/Hangman/Hangman.jsx
+++ React/03-state/becky/src/components/Hangman/Hangman.jsx
@@ -1,9 +1,49 @@
-import { useState } from "react/cjs/react.production.min"
+import React, {useState} from 'react';
+import './Hangman.css';
 
-let word = 'tiger'
-function checkWord(ev){
-    const [text, checkText] = useState()
-function handleWord(ev){
+let randomWord = 'tiger'; 
 
+function Hangman() {
+    const [text, setText] = useState();
+    const [color, setColor] = useState('red');
+    let userInput;
+    
+
+    function handleWriting(ev){
+        setText(ev.target.value)
+        const lastChar = ev.target.value.slice(-1)
+
+        if (randomWord.includes(lastChar)) {
+            console.log('YES');
+            setColor('green');
+        }
+        else {
+            setColor('red');
+        }
+            
+    }
+
+
+
+    return (
+
+        <div>
+            <div className='hangman'></div>
+            <div className = 'box' style={{background:color}}></div>
+
+            Type your guess: 
+            <input type = 'text'
+                    placeholder = 'Input guess here'
+                    onKeyUp = {handleWriting} />
+            <div 
+            className = 'textContainer'
+            id = 'textbox'
+            />
+            {text}
+        </div>
+    )
 }
-}
+
+
+export default Hangman;
+
