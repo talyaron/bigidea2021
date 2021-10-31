2b9b820c25b211df19aaf591460957a4018610fc
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-10-31 12:05:44 +0200

    r

--- React/03-state/zacharyl/src/App.js
+++ React/03-state/zacharyl/src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function input() {
+      return (
+        <input className="input" type="text">
+        </input>
+      );
 }
-
+  )
 export default App;
+
