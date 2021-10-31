7803bf16b61a58e2191418537e283d5fa86a56d9
Tal <tal.yaron@gmail.com>
2021-10-31 14:07:59 +0200

    con

--- React/03-state/tals/src/components/Counter/Counter.jsx
+++ React/03-state/tals/src/components/Counter/Counter.jsx
@@ -7,13 +7,14 @@ function Counter() {
     //[state, setterOfState] = useState (initial value)
     const [counter, setCounter] = useState(1);
     const [text, setText] = useState('');
-    const [color, setColor] = useState('pink')
+    const [color, setColor] = ('pink')
 
     function handleClick() {
         setCounter(counter + 1)
     }
     
     function handleKeyUp(ev){
+        console.dir(ev.target.value)
         setText(ev.target.value)
     }
 
