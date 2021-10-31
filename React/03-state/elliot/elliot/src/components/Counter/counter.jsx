39911b97744d67d78a48bc4064ec388ffaf3d9e9
ElliotKupchik <elliotkupchik@gmail.com>
2021-10-31 12:16:18 +0200

    bb

--- React/03-state/elliot/elliot/src/components/Counter/counter.jsx
+++ React/03-state/elliot/elliot/src/components/Counter/counter.jsx
@@ -1,16 +1,40 @@
 import {useState} from 'react';
+import './counter.css'
+
 
 function Counter(){
     
 //[state, setterOfState] = useState(initialVal)
     const [counter, setCounter] = useState(1);
-    
+    const [text, setText] = useState();
+
     function handleClick(){
         setCounter(counter+1)
     }
 
+    function handleWriting(ev){
+        setText(ev.target.value)
+    }
+
     return(
-        <div onClick={handleClick}>Counter: {counter}</div>
+        <div className = 'container'>
+            <div 
+                className = 'counter' 
+                onClick={handleClick}>
+                Counter: {counter}
+            </div>
+            <input 
+                type='text'
+                placeholder='Input Words Here'
+                onKeyUp={handleWriting}>
+            </input>
+            <div 
+                className = 'textContainer'
+                id='textbox'>
+                {text}
+            </div>
+        </div>
+        
     )
 }
 
