671fbd92ef71923b3de6de8ac96e0d53ded8b326
Mnkjango <mkotzker493@gmail.com>
2021-10-31 11:33:22 +0200

    m

--- React/03-state/michael/src/App.js
+++ React/03-state/michael/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+//components
+import Counter from './components/Counter/Counter'
+
+//npx create-react-app <nameOfApp>
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +14,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Counter />
+
       </header>
     </div>
   );
