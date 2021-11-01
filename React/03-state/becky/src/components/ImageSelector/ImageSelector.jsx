e190cd229d83bc71bc6c77e068142cbbb7789da0
Tal <tal.yaron@gmail.com>
2021-11-01 11:27:33 +0200

    fix

--- React/03-state/becky/src/components/ImageSelector/ImageSelector.jsx
+++ React/03-state/becky/src/components/ImageSelector/ImageSelector.jsx
@@ -1,21 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './ImageSelector.css'
 import Ace from './Ace.jpg'
 import emma from './emma.jpg'
 import Israel from './Israel.jpg'
 
-function ImageSelector(){
+function ImageSelector() {
     const [backgroundImage, setBackground] = useState('pink');
-    function onClick(ev){
+    function onClick(ev) {
         setBackground(ev.target.value);
-           
+
     }
-    return(
-        <div className = 'imageSelector'> 
-            <div className = 'box' id = 'box1' style={{background:backgroundImage}} onClick = {onClick}></div>
-            <div className = 'box' id = 'box2' style={{background:backgroundImage}} onClick = {onClick}></div>
-            <div className = 'box' id = 'box3' style={{background:backgroundImage}} onClick = {onClick}></div>
-            <div className = 'box' id = 'box4' style={{background:backgroundImage}} ></div>
+    return (
+        <div className='imageSelector'>
+            <div className='box' id='box1' style={{ background: backgroundImage }} onClick={onClick}></div>
+            <div className='box' id='box2' style={{ background: backgroundImage }} onClick={onClick}></div>
+            <div className='box' id='box3' style={{ background: backgroundImage }} onClick={onClick}></div>
+            <div className='box' id='box4' style={{ background: backgroundImage }} ></div>
         </div>
     )
 }
