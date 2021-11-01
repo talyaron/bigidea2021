02dc6000b7bcfb7436aef29bc4e563d8058819c3
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-11-01 11:23:41 +0200

    cont

---
+++ React/03-state/becky/src/components/ImageSelector/ImageSelector.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './ImageSelector.css'
+import Ace from './Ace.jpg'
+import emma from './emma.jpg'
+import Israel from './Israel.jpg'
+
+function ImageSelector(){
+    const [backgroundImage, setBackground] = useState('pink');
+    function onClick(ev){
+        setBackground(ev.target.value);
+           
+    }
+    return(
+        <div className = 'imageSelector'> 
+            <div className = 'box' id = 'box1' style={{background:backgroundImage}} onClick = {onClick}></div>
+            <div className = 'box' id = 'box2' style={{background:backgroundImage}} onClick = {onClick}></div>
+            <div className = 'box' id = 'box3' style={{background:backgroundImage}} onClick = {onClick}></div>
+            <div className = 'box' id = 'box4' style={{background:backgroundImage}} ></div>
+        </div>
+    )
+}
+export default ImageSelector;
