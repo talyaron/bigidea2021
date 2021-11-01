eab1fb7c011c6244bd189cbde108068c617be164
Mnkjango <mkotzker493@gmail.com>
2021-11-01 13:33:05 +0200

    m

--- React/03-state/michael/src/components/Hangman/Hangman.jsx
+++ React/03-state/michael/src/components/Hangman/Hangman.jsx
@@ -1,48 +1,43 @@
-import React, {useState} from 'react';
+import { useState } from "react";
 import './Hangman.css';
 
-let randomWord = 'tiger'; 
-
 function Hangman() {
-    const [text, setText] = useState();
-    const [color, setColor] = useState('red');
-    let userInput;
-    
 
-    function handleWriting(ev){
-        setText(ev.target.value)
-        const lastChar = ev.target.value.slice(-1)
+    const[color, setColor] = useState('white');
+    const [width, setWidth] = useState(400);
+    const word = "Destiny";
 
-        if (randomWord.includes(lastChar)) {
-            console.log('YES');
-            setColor('green');
-        }
-        else {
-            setColor('red');
-        }
+    function checkText(letter) {
+
+        letter = letter.toLowerCase();
+        var format = letter;
+        
+
+        if(word.match(format)) {
+            console.log(true);
+          
+            setColor('green');   
+            setWidth(200)         
+        } else {
+            console.log(false);
             
+            setColor('red');
+            setWidth(500)
+        }
     }
 
+    function handleCheckText(ev) {
+        let value = ev.target.value, char = value.slice(-1);
+        console.log(char);
+        checkText(char);
+    }
 
-
-    return (
-
-        <div>
-            <div className='hangman'></div>
-            <div className = 'box' style={{background:color}}></div>
-
-            Type your guess: 
-            <input type = 'text'
-                    placeholder = 'Input guess here'
-                    onKeyUp = {handleWriting} />
-            <div 
-            className = 'textContainer'
-            id = 'textbox'
-            />
-            {text}
+    return(
+        <div className="guessBox" style={{background:color, width:`${width}px`, height:`100px`}}>
+            <p>{color}</p>
+            <input type="text" name="textBox" onChange={handleCheckText}></input>
         </div>
     )
 }
-
 
 export default Hangman;
