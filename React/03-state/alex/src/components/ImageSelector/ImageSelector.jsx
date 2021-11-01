9dfa239018f85af30677c6579b12b4ce7beb64ce
Alex Rothenberg <alex37roth@gmail.com>
2021-11-01 13:05:12 +0200

    pushy p

--- React/03-state/alex/src/components/ImageSelector/ImageSelector.jsx
+++ React/03-state/alex/src/components/ImageSelector/ImageSelector.jsx
@@ -6,7 +6,6 @@ import boat from '../../boat.jpg';
 
 function ImageSelector(){
 
-    const[color, setColor] = useState('white');
     const[variable, setVariable] = useState();
 
     function changeSRC(ev){
@@ -18,17 +17,17 @@ function ImageSelector(){
     return(
         <div>
             <div className='box1'  onClick={changeSRC}>
-                <img style={{width:"100px", height:"100px"}} src={shark}></img>
+                <img style={{width:"100px", height:"100px"}} src={shark} alt='Error'></img>
             </div>
             <div className='box2' onClick={changeSRC}>
-            <img src={boat} style={{width:"100px", height:"100px"}}></img>
+                <img src={boat} style={{width:"100px", height:"100px"}} alt='Error'></img>
             </div>
                 
             <div className='box3'  onClick={changeSRC}>
-            <img src={alaska} style={{width:"100px", height:"100px"}}></img>
+                <img src={alaska} style={{width:"100px", height:"100px"}} alt='Error'></img>
             </div>
-            <div className='changeBox' style={{width:"100px", height:"100px"}} >
-                <img src={variable} style={{width:"100px", height:"100px"}}></img>
+            <div className='changeBox'>
+                <img src={variable} style={{width:"100px", height:"100px"}} alt='No Image'></img>
             </div>
         </div>
     )
