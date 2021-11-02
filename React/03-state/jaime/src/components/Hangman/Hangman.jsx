ea31d66880ea318dc3c1473aec7233b75f95614e
jaimemesq <jaimemesq@outlook.com>
2021-11-02 23:59:46 +0200

    s

--- React/03-state/jaime/src/components/Hangman/Hangman.jsx
+++ React/03-state/jaime/src/components/Hangman/Hangman.jsx
@@ -1,37 +1,31 @@
 import { useState } from "react";
 import './Hangman.css';
 
+let guessWord, dashes = [], wordArr =[];
+
 function Hangman() {
 
-    const[color, setColor] = useState('white');
-    const [width, setWidth] = useState(400);
+    const[color, setColor] = useState('orange');
     const [showResults, setShowResults] = useState('block');
     const [guessWordArray, setGuessWordArray] = useState([]);
-    let guessWord, dashes = [];
-
-    function checkText(letter) {
-
-        letter = letter.toLowerCase();
-
-        if(guessWord.includes(letter)) {
-            setColor('green');   
-            setWidth(300)         
-        } else {
-            setColor('red');
-            setWidth(500)
-        }
-    }
 
     function handleCheckText(ev) {
         let value = ev.target.value, char = value.slice(-1);
         console.log(char);
-        checkText(char);
+
+        char = char.toLowerCase();
+
+        if(wordArr.includes(char)) {
+            setColor('green');         
+        } else {
+            setColor('red');
+        }
     }
 
     function handleSubmit(ev) {
         ev.preventDefault();
         guessWord = ev.target.elements.word.value;
-        let wordArr = guessWord.split("");
+        wordArr = guessWord.split("");
         console.log(guessWord, wordArr);
         
         for(let i=0; i<wordArr.length; i++) {
@@ -44,19 +38,20 @@ function Hangman() {
     }
 
     return(
-        <div className="guessBox" style={{background:color, width:`${width}px`, height:`150px`}}>
-            <p>{color}</p>
+        <div className="guessBox" style={{background:color, width:`500px`, height:`150px`}}>
+            <p>Hangman</p>
             <form onSubmit={handleSubmit} style={{display:showResults}}>
                 <input type="password" name="word" placeholder="Type your secret word"/>
                 <input type="submit" value="Hide" />
             </form>
             <p></p>
             <input type="text" name="guessBox" placeholder="Guess a letter" onKeyUp={handleCheckText} maxLength="1"/>
-            {
-            dashes.map((letter, index) => {
+            <div className="wrapper">
+            {dashes.map((letter, index) => {
                 return(<div key={index}>"{letter}"</div>)
-            })
-        }
+            }
+            )}
+            </div>
         </div>
     )
 }
