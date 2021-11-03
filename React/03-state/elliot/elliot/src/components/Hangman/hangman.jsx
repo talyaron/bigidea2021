a408f23d9dae40c30d4ee6884c6454d9a2b7f4b5
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-03 09:55:24 +0200

    hh

--- React/03-state/elliot/elliot/src/components/Hangman/hangman.jsx
+++ React/03-state/elliot/elliot/src/components/Hangman/hangman.jsx
@@ -1,49 +1,26 @@
 import {useState} from 'react';
 import './hangman.css'
 
-/*
-                <input 
-                    type='text'
-                    id = 'inputActualWord'
-                    placeholder='Input The Secret Word Here:'
-                    onKeyUp={handleTargetWord}> 
-                </input>
-
-                <input
-                    type='button'
-                    id = 'secretWordConfirm'
-                    onClick={handleTargetWordConfirmation}> Confirm Secret Word
-                </input>
-*/
+//let word = [a, c, t, u, a, l, l, y]
+//<div className = "wrapper"> {word.map(letter,index) => return( <p key={index}> {letter} </p> )} </div>
+//This creates a list of words
 
 function HangmanGame(props){
     
-    //[state, setterOfState] = useState(initialVal)   
         const [color, setColor] = useState('gray');
         let fullSecretWord = props.guessWord.toLowerCase();
-        //let inputSecretWord = '';
         let inputGuess = '';
-        //let blurredWord = '';
-
-        /*function handleTargetWord(ev){
-            inputSecretWord = ev.target.value;
-        }
-
-        function handleTargetWordConfirmation(){
-            fullSecretWord = props.inputSecretWord.toLowerCase();
-            blurredWord = 'X'.repeat(fullSecretWord.length());
-            document.getElementById('inputActualWord').style.display = blurredWord;
-        }*/
+        let inputChar = '';
 
         function handleNewGuess(ev){
-            inputGuess = ev.target.value;
-            let inputChar = inputGuess.slice(-1)
+            inputGuess = ev.target.value; //Stores the inserted guess
+            inputChar = inputGuess.slice(-1) //Gets the last letter of the inserted guess
             compareGuess(inputChar)
         }
     
         function compareGuess(letter){
             letter = letter.toLowerCase();
-            console.log(letter);
+            
             if (fullSecretWord.match(letter)){
                 setColor('green');
             } else {
@@ -69,4 +46,4 @@ function HangmanGame(props){
         )
     }
     
-    export default HangmanGame;
+export default HangmanGame;
