60f66fb064ef7a03021a4886c0d91bfbc498e351
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-07 12:44:24 +0200

    f

---
+++ React/03-state/zachary/src/components/Hangman/hangman.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import './hangman.css';
+
+let SetWord;
+
+function hangman(){
+const word=
+
+    return (
+
+        <div className="list">
+            {words.map((word, index) => {
+                return <div key={word.id} 
+                className="word">
+                    {word.id}{word.word}
+                </div>;
+            })}
+        </div>
+}
+
+export default hangman;
