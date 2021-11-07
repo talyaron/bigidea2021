4e6185b87ef9e7defa472df785b4b208e783f54f
robertUfberg <robertufberg@gmail.com>
2021-11-07 13:02:22 +0200

    yw5er6

---
+++ React/04-conditinalRendering/robbyeitan/src/components/pictures.jsx
@@ -0,0 +1,6 @@
+/* 
+16 boxes - 1/2 pictures half names but start as blank
+- when clicked, box turns and stays until next box is clicked 
+    - if box matches, screen turns green and boxes stay flipped
+    - if not, boxes go back to normal
+*/
