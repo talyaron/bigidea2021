4c7345706860bcb88d2d907bc5e0e7c43801adb3
Tal <tal.yaron@gmail.com>
2021-11-07 13:32:17 +0200

    memory 1

--- React/04-conditinalRendering/tal/src/App.js
+++ React/04-conditinalRendering/tal/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Box from './components/If/If';
-import Switch from './components/Switch/Switch';
-import TernaryOperator from './components/TernaryOperator/TernaryOperator';
+// import Box from './components/If/If';
+// import Switch from './components/Switch/Switch';
+// import TernaryOperator from './components/TernaryOperator/TernaryOperator';
+import Memory from './pages/Memory/Memory';
 
 function App() {
   return (
     <div className="App">
-      <TernaryOperator isGood={false} />
+      <Memory />
+      {/* <TernaryOperator isGood={false} /> */}
       {/* <Box isOne={true} dkfjghdjkfhgjk='aaa' sdfhsdfg='23' />
       <Box isOne={false} dkfjghdjkfhgjk='bbb' skjdh={true} /> */}
-      <Switch img='hangman1' />
+      {/* <Switch img='hangman1' />
       <Switch img='hangman2' />
       <Switch img='dragon' />
-      <Switch img='fish' />
+      <Switch img='fish' /> */}
     </div>
   );
 }
