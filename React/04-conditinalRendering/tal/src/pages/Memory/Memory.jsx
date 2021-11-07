c14416646be9ee45671f91d7298bf6acef4a784e
Tal <tal.yaron@gmail.com>
2021-11-07 14:25:34 +0200

    solution to memory game

--- React/04-conditinalRendering/tal/src/pages/Memory/Memory.jsx
+++ React/04-conditinalRendering/tal/src/pages/Memory/Memory.jsx
@@ -6,22 +6,77 @@ import telAviv from '../../img/tel-aviv.jpg'
 import jerusalem from '../../img/jerusalem.jpg'
 
 const memory = [
-    { text: 'Tel-Aviv', id: 1 }, { img: telAviv, id: 1 },
-    { text: 'Jerusalem', id: 2 }, { img: jerusalem, id: 2 },
+    { text: 'Tel-Aviv', id: 1, number: 1 }, { img: telAviv, id: 1, number: 2 },
+    { text: 'Jerusalem', id: 2, number: 3 }, { img: jerusalem, id: 2, number: 4 },
 ]
+let twoCards = [];
 
 function Memory() {
     const [cards, setCards] = useState(shuffle(memory));
 
+    function handleCardCheck(card, i) {
+        console.log(card)
+
+        if (twoCards.length < 2) {
+            twoCards.push(card);
+
+            const tempCards = [...cards];
+            tempCards[i].show = true;
+            tempCards[i].key = i;
+            setCards(tempCards)
+        }
+        if (twoCards.length === 2) {
+            if (twoCards[0].id !== twoCards[1].id) {
+                //flip them back
+                const tempCards = [...cards];
+
+                const index1 = twoCards[0].key;
+                const index2 = twoCards[1].key;
+
+                tempCards[index1].show = false;
+                tempCards[index2].show = false;
+                console.log(tempCards)
+                setCards(tempCards)
+
+                //clean two cards
+                
+            }
+            twoCards = [];
+        }
+        console.log(twoCards)
+    }
 
     return (
         <div className='cards'>
             {cards.map((card, i) => {
-            console.log(card.img)
+
                 return (card.img ?
-                    <div key={i} className='card' style={{backgroundImage:`url(${card.img})`}}></div>
+                    card.show ?
+                        <div
+                            key={i}
+                            onClick={() => handleCardCheck(card, i)}
+                            className='card'
+                            style={{ backgroundImage: `url(${card.img})` }}
+                        ></div>
+                        :
+                        <div
+                            key={i}
+                            onClick={() => handleCardCheck(card, i)}
+                            className="card--hide">Flip</div>
                     :
-                    <div key={i} className='card'>{card.text}</div>
+                    card.show ?
+                        <div
+                            onClick={() => handleCardCheck(card, i)}
+                            key={i}
+                            className='card'
+                        >
+                            {card.text}
+                        </div>
+                        :
+                        <div
+                            key={i}
+                            onClick={() => handleCardCheck(card, i)}
+                            className="card--hide">Flip</div>
                 )
 
             })
