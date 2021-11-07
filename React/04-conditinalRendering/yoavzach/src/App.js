76c8597fc69e2e6ed260ddd36e06628f5af66ed5
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-07 13:39:42 +0200

    cf

--- React/04-conditinalRendering/yoavzach/src/App.js
+++ React/04-conditinalRendering/yoavzach/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 
 //components
 import Card from './components/Card/Card';
-
+import source1 from './images/funny.jpg'
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card source={source1}/>
       
     </div>
   );
