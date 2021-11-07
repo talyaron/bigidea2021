664234a9fe47644c071356d051511f023722bb9d
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-07 11:27:34 +0200

    e
    ee

--- React/03-state/sam/src/App.js
+++ React/03-state/sam/src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
-//components
-import List from './components/List/List';
-import Counter from './components/Counter/Counter';
-import Hangman from './components/Hangman/Hangman';
-
 //npx create-react-app <nameOfApp>
 
 function App() {
   return (
     <div className="App">
-      <List />
+      <Ternaryoperator isGood={false} />
+      <switch img='hangman1' />
 {/* <Hangman /> */}
         
 
---
+++ React/03-state/sam/src/components/TernaryOperator/ternaryOperator.jsx
@@ -0,0 +1,1 @@
+import './ternaryOperator.css'
