2dd4ae6d0fd44bdd410c1906b2d3f30ccca3ae1d
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-07 11:23:02 +0200

    hh

---
+++ React/03-state/sam/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import List from './components/List/List';
+import Counter from './components/Counter/Counter';
+import Hangman from './components/Hangman/Hangman';
+
+//npx create-react-app <nameOfApp>
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+{/* <Hangman /> */}
+        
+
+
+    </div>
+  );
+}
+
+export default App;
