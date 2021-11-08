ad4c70b6ba08f47952469ff2e2b1f04695a6494a
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-08 13:35:53 +0200

    jkjk

--- React/05-router/elliot/src/views/pages/SubCatcherTwo/SubCatcherTwo.jsx
+++ React/05-router/elliot/src/views/pages/SubCatcherTwo/SubCatcherTwo.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-//<li><Link to="/">Home</Link></li>
+import FortThree from './pages/FortThree/FortThree';
+import FortFour from './pages/FortFour/FortFour';
 
 const SubCatcherTwo = () => {
     return <div>
-        <h1>1</h1>
+        <Router>
+            <u1>
+                <li><Link to="/fortThree">3</Link></li>
+                <li><Link to="/fortFour">4</Link></li>
+            </u1>
+            <Routes>
+                <Route path="/fortThree" element={<FortThree />} />
+                <Route path="/fortFour" element={<FortFour />} />
+            </Routes>
+        </Router>
     </div>;
 };
   
