f41630466072f976279b6ef1e246a052a8370b7f
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-08 12:55:30 +0200

    jkjnkinj

---
+++ React/05-router/elliot/src/views/pages/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+    return <div>
+        <h1>Contact Me</h1>
+        <li><Link to="/">Home</Link></li>
+    </div>;
+};
+  
+export default Contact;
