c44da65012e0537e85637dfdf4285e8a673661a4
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-11-08 13:42:22 +0200

    Update Blogs.js

--- React/05-router/zachary/src/views/Blogs.js
+++ React/05-router/zachary/src/views/Blogs.js
@@ -1,3 +1,8 @@
+import { Link } from 'react-router-dom';
+import Pizza from '../images/pizza.jpg';
+import Fish from '../images/fish.jpg';
+import Images from '../components/Images';
+
 const Blogs = () => {
     return <h1>Blog Articles</h1>;
   };
