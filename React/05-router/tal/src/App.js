01eec1c05a94f9b77bfbe006beed470f95345a83
Tal <tal.yaron@gmail.com>
2021-11-08 14:27:56 +0200

    jhv

--- React/05-router/tal/src/App.js
+++ React/05-router/tal/src/App.js
@@ -5,13 +5,31 @@ import './App.css';
 import Home from './views/pages/Home/Home';
 import Blogs from './views/pages/Blogs/Blogs';
 import Contact from './views/pages/Contact/Contact';
+import MarketingBlog from './views/pages/Blogs/MarketingBlog';
+import TechBlog from './views/pages/Blogs/TechBlog';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li>
+            <Link to='/blogs'>Blogs</Link>
+            <ul>
+              <li><Link to='/blogs/tech'>Tech Blog</Link></li>
+              <li><Link to='/blogs/marketing'>Marketing Blog</Link></li>
+            </ul>
+          </li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} >
+          <Route path="marketing" element={<MarketingBlog />} />
+          <Route path="tech" element={<TechBlog />} />
+        </Route>
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
---
+++ React/05-router/tal/src/views/pages/Blogs/TechBlog.jsx
@@ -0,0 +1,6 @@
+
+const TechBlog = () => {
+    return <h1>Blog Articles on Tech</h1>;
+  };
+  
+  export default TechBlog;
