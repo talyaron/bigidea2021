236b96152d1b89c747f1c7b23ddc9c13438e4633
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-08 13:08:43 +0200

    Ejeejh

---
+++ React/05-router/elliot/src/views/pages/Five/Five.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+//<li><Link to="/">Home</Link></li>
+
+const Five = () => {
+    return <div>
+        <h1>1</h1>
+    </div>;
+};
+  
+export default Five;
---
+++ React/05-router/elliot/src/views/pages/Six/Six.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+//<li><Link to="/">Home</Link></li>
+
+const Six = () => {
+    return <div>
+        <h1>1</h1>
+    </div>;
+};
+  
+export default Six;
