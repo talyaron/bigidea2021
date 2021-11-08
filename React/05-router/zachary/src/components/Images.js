4582d8318fc5e922c498cfe64cbfa2deedd0c3ff
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-08 13:32:30 +0200

    asd

--- React/05-router/zachary/src/components/Images.js
+++ React/05-router/zachary/src/components/Images.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-const Images = ({source,next}) => {
+const Images = ({source,next,name}) => {
     return (
         <div>
             <img src={source}></img>
-            <Link to={next}></Link>
+            <Link to={next}>{name}</Link>
         </div>
     )
 }
--- React/05-router/zachary/src/views/Contact.js
+++ React/05-router/zachary/src/views/Contact.js
@@ -1,11 +1,14 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
-    return <div>
-      <h1>Contact Me</h1>
-      <Link to="/">Link into home</Link>
-    </div>;
+  return <div>
+    <h1>Which one is better?</h1>
+    <div className="Parent">
+      <Images source={Dog} next='/contact.js' name="Dog" />
+      <Images source={Cat} next='/blogs.js' name="Cat" />
+    </div>
+  </div>;
 
-  };
-  
-  export default Contact;
+};
+
+export default Contact;
