234e436c6532cc98606b0f651c792826d1bdd643
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-08 13:42:22 +0200

    wer

--- React/05-router/zachary/src/views/Blogs.js
+++ React/05-router/zachary/src/views/Blogs.js
@@ -1,5 +1,11 @@
 const Blogs = () => {
-    return <h1>Blog Articles</h1>;
+    return  <div>
+    <h1>What's your favourite animal?</h1>
+    <div className="Parent">
+      <Images source={Dog} next='/contact.js' name="Dog" />
+      <Images source={Cat} next='/blogs.js' name="Cat"/>
+    </div>
+    </div>;
   };
   
   export default Blogs;
