aad46fbc5c7c2bac29e8eef35dc8445c1110dee3
Tacoman611 <liamharir2003@gmail.com>
2021-11-08 13:57:58 +0200

    fortune

---
+++ React/05-router/Liam/src/LiamSzymon/Final1.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import './Fortune.css';
+const Final1 = () => {
+    return( <div>
+        <h1>Good Health!</h1>;
+        <Link to='/' > Home Page </Link>
+    </div>
+        
+    ) 
+  }
+  
+  export default Final1;
---
+++ React/05-router/Liam/src/LiamSzymon/Final4.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import './Fortune.css';
+const Final4 = () => {
+    return( <div>
+        <h1>Succesful in crypto!</h1>;
+        <Link to='/' > Home Page </Link>
+    </div>
+        
+    ) 
+  };
+  
+  export default Final4;
