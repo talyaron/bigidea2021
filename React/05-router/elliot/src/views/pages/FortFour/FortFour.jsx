f4983bbfc5704412b2a51a5ccd4e20ef5cf9e983
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-08 13:18:34 +0200

    nnn

--- React/05-router/elliot/src/views/pages/FortFour/FortFour.jsx
+++ React/05-router/elliot/src/views/pages/FortFour/FortFour.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 //<li><Link to="/">Home</Link></li>
 
-const Six = () => {
+const FortFOur = () => {
     return <div>
         <h1>1</h1>
     </div>;
 };
   
-export default Six;
+export default FortFour;
