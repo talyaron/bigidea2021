84eea7d15e118a9efb77da6a055616dc6cca501d
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-08 13:15:18 +0200

    d

---
+++ React/05-router/zachary/src/components/Images.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Images = ({source}) => {
+    return (
+        <div>
+            <img src={source}></img>
+        </div>
+    )
+}
+
+export default Images
--- React/05-router/zachary/src/views/Home.js
+++ React/05-router/zachary/src/views/Home.js
@@ -1,5 +1,14 @@
+import Dog from '../images/dog.jpg'
+import Cat from '../images/cat.jpg'
+import Images from '../components/Images';
 const Home = () => {
-    return <h1>Home</h1>;
+    return <div>
+      <h1>What's your favourite animal?</h1>
+      <div className="Parent">
+        <Images source={Dog} />;
+        <Images source={Cat} />;
+      </div>
+      </div>;
   };
   
   export default Home;
