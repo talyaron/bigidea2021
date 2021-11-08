f6a9c45238f46e1bd81e143b793866b581ac94b1
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-11-08 13:48:29 +0200

    push push

--- React/05-router/elliot/src/views/pages/FortOne/FortOne.jsx
+++ React/05-router/elliot/src/views/pages/FortOne/FortOne.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const FortOne = () => {
-    return <div>
-        <h1>You will be an astronaut on the ISS!</h1>
-        <li><Link to="/">Restart The Game.</Link></li>
-    </div>;
+	return (
+		<div>
+			<h1>You will be an astronaut on the ISS!</h1>
+			<li>
+				<Link to='/'>Restart The Game.</Link>
+			</li>
+		</div>
+	);
 };
-  
+
 export default FortOne;
