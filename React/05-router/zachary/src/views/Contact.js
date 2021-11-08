433149333429967a9f931ba5c83515d9084689ff
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-11-08 13:34:56 +0200

    cont

--- React/05-router/zachary/src/views/Contact.js
+++ React/05-router/zachary/src/views/Contact.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import Pizza from '../images/pizza.jpg'
+import Fish from '../images/fish.jpg'
 
 const Contact = () => {
   return <div>
     <h1>Which one is better?</h1>
     <div className="Parent">
-      <Images source={Dog} next='/contact.js' name="Dog" />
-      <Images source={Cat} next='/blogs.js' name="Cat" />
+      <Images source={Pizza} next='/contact.js' name="Pizza" />
+      <Images source={Fish} next='/blogs.js' name="Fish" />
     </div>
   </div>;
 
