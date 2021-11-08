c06f363cbd8f0b732d30ed42b0380e26e2fd79bd
ykonsta <yoav.konsta@gmail.com>
2021-11-08 04:17:26 -0800

    2rt

--- React/05-router/yoav/src/views/pages/Result4/Result4.jsx
+++ React/05-router/yoav/src/views/pages/Result4/Result4.jsx
@@ -1,7 +1,9 @@
+import bitcoin from './bitcoin.jpg';
+
 const R4 = () => {
     return <div>
       <h1> You will win a bitcoin </h1>
-
+      <img src = {bitcoin} />
       </div>;
   };
   
--- React/05-router/yoav/src/views/pages/Result7/Result7.jsx
+++ React/05-router/yoav/src/views/pages/Result7/Result7.jsx
@@ -1,7 +1,9 @@
+import nothing from './nothing.jpg';
+
 const R7 = () => {
     return <div>
       <h1> Literally nothing will happen </h1>
-
+      <img src = {nothing} />
       </div>;
   };
   
