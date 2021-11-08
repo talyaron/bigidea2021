647e9761a8bbee957cc792b14638337071cc0a4a
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-11-08 13:44:34 +0200

    cont

--- React/05-router/zachary/src/views/Blogs.js
+++ React/05-router/zachary/src/views/Blogs.js
@@ -5,7 +5,7 @@ import Images from '../components/Images';
 
 const Blogs = () => {
     return  <div>
-    <h1>What's your favourite animal?</h1>
+    <h1>Do you prefer watching or playing sports?</h1>
     <div className="Parent">
       <Images source={Dog} next='/contact.js' name="Dog" />
       <Images source={Cat} next='/blogs.js' name="Cat"/>
