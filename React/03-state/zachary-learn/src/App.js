5859947d8c52f51a0462f13abbad848ba4e784d7
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-13 14:17:30 +0200

    d

---
+++ React/03-state/zachary-learn/src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <Header  />
+    </div>
+    
+  );
+}
+
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello from a class</h1>
+//   }
+// }
+
+export default App;
