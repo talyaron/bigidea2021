f3b00112a3eb5b164cd50de86c5c7661b99e8306
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-15 14:01:29 +0200

    t

--- React/07-firebaseCRUD/madlibs/src/components/UserPhoto/UserPhoto.jsx
+++ React/07-firebaseCRUD/madlibs/src/components/UserPhoto/UserPhoto.jsx
@@ -1,17 +1,17 @@
 import {useState,useEffect} from 'react';
 import {db} from '../../functions/firebase/config'
 import {collection,getDocs,addDoc}from 'firebase/firestore'
-import{doc, setDoc,add} from 'firebase/firestore'
+import{doc, setDoc} from 'firebase/firestore'
 
 function UserPhoto(){
     
     function handleSubmit(ev){
-
         ev.preventDefault()
+        const usersCollectionRef=collection(db, "Images");
         let userImage = ev.target.elements.imageURL.value
         console.log(userImage)
         // put into firebase
-        
+        addDoc(usersCollectionRef,{ImageUrl:userImage});
     }
 
     
@@ -19,7 +19,9 @@ function UserPhoto(){
     return(
         <div>
             <form onSubmit={handleSubmit}>
-                <input type='text' name='imageURL'/>
+            <input> </input>
+                <input type='text' name='imageURL'>URL</input>
+                
                 <input type="submit"/>
 
             </form>
