f38058533855526ceb8d2ecaa11f634bbfe7f972
ykonsta <yoav.konsta@gmail.com>
2021-11-15 01:29:31 -0800

    feg

--- React/06-firebaseBasic/yoav/src/App.js
+++ React/06-firebaseBasic/yoav/src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { db } from './functions/firebase/config';
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
-
-function App() {
-
-  useEffect(() => {
-    const catRef = doc(db, "cats", "nw03YaQ8CV4Zu6VS1sMy");
-
-    getDoc(catRef).then(catDB => {
-      console.log(catDB.data());
-    })
-
-    onSnapshot(catRef, catDB=>{
-     console.log(catDB.data())
-    })
-  }, [])
-
-=======
 import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -84,7 +62,6 @@ function App() {
   }
 
   
->>>>>>> Stashed changes
   return (
     <div className="App">
      
