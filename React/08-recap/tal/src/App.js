c8757350242197ebb36ed2daee333b5d5fe8f809
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-17 13:03:48 +0200

    Update App.js

--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -1,54 +1,36 @@
 import { useEffect, useState } from 'react'
 import './App.css';
 import { db } from './functions/firebase/config';
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot, collection, getDocs, getDoc } from "firebase/firestore";
 
 function App() {
-  // const [userObj, setUserObj] = useState({ name: '', image: '' })
-  const [answerObj, setAnswerObj] = useState({ name: '', answer: 0 })
+  
   useEffect(() => {
-    // const userRef = doc(db, 'users', 'me')
-    // setDoc(userRef,{name:'Tal', image:'https://dailygazette.com/wp-content/uploads/fly-images/126428/shutterstock_245726512-scaled-940x940.jpg'})
-    // onSnapshot(userRef, userDB => {
-    //   if (userDB.exists()) {
-    //     console.log(userDB.data())
-    //     const { name, image } = userDB.data();
-    //     if (!name) console.error('No user in DB');
-    //     if (!image) console.error('No image in DB');
-
-    //     if (typeof image === 'string' && typeof name === 'string') {
-    //       setUserObj({ name, image })
-    //     }
-    //   }
-    // })
-
-    const answersRef = doc(db, 'answers', 'answer')
-    onSnapshot(answersRef, (answerDB) => {
-      try {
-      
-        if (answerDB.exists()) {
-          console.log(answerDB.data());
-          const { name, answer } = answerDB.data();
-          if (typeof name !== 'string') throw new Error('Name is not a string');
-          if (typeof answer !== 'number') throw new Error(`answer is not a number (${name})`)
-          setAnswerObj({ name, answer });
-        }
-      } catch (err) {
-        console.error(err)
-      }
+    //const usRef = collection(db, "us");
+    const michaelRef = doc(db, 'us', "noeAEfNicuKOI0ndT4Nl");
+    getDoc(michaelRef).then(michaelDB => {
+      console.log(michaelDB.data())
     })
-  }, []);
+
+      const unsub = onSnapshot(doc(db, "us", "noeAEfNicuKOI0ndT4Nl"), (doc) => {
+        console.log("Current data: ", doc.data());
+    })
+      
+   /* getDocs(usRef).then(usDB=>{
+      usDB.forEach(personDB=>{
+        console.log(personDB.id)
+        console.log(personDB.data())
+      })
+    })
+*/
+    //get only Michael from the database
+    
+  }, [])
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className='userCard'>
-          {/* <img src={userObj.image}></img>
-          <h1>{userObj.name}</h1> */}
-          <h1>{answerObj.name}: {answerObj.answer}</h1>
-        </div>
-      </header>
+      hi
     </div>
   );
 }
