8279e1a1587adad4bb23cc62f23fc7edf94cc633
Mnkjango <mkotzker493@gmail.com>
2021-11-17 11:44:25 +0200

    Update App.js

--- React/08-recap/Michael/src/App.js
+++ React/08-recap/Michael/src/App.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react'
 import './App.css';
 import { db } from './functions/firebase/config';
-import { collection, getDocs } from "firebase/firestore";
+import { collection,doc, getDocs, getDoc, onSnapshot } from "firebase/firestore";
 
 function App() {
   //get all of us
 
   //run only on start
   useEffect(() => {
-    const Ref = collection(db, "us");
-    getDocs(Ref).then(dataDB =>{
-      dataDB.forEach(datum =>{
-        console.log(datum.data());
-      })
+    const Ref = doc(db, "us", "noeAEfNicuKOI0ndT4Nl");
+    getDoc(Ref).then(dataDB =>{
+      console.log(dataDB.data())
     })
-  })
+
+    //get me  
+
+    const unsub = onSnapshot(doc(db, "us", "noeAEfNicuKOI0ndT4Nl"), (doc) => {
+      console.log("Current data: ", doc.data());
+    })
+
+  },[])
 
   return(
     <div className="App">
