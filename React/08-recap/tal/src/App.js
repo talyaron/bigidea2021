46283c538786dbd634a7b13d46e4adc0c341dd0e
SzymontheDestroyer <91409086+SzymontheDestroyer@users.noreply.github.com>
2021-11-17 14:28:38 +0200

    a

--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -43,6 +43,8 @@ function App() {
     <div className="App">
       {name}
       {surprise}
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/J---aiyznGQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <input type="submit"></input>
     </div>
   );
 }
