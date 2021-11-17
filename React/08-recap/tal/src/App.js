264783c0eff8f88e3a6310ad2e01fc39ac574ca0
Tal Yaron <tal.yaron@gmail.com>
2021-11-17 14:28:13 +0200

    fix

--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -28,13 +28,13 @@ function App() {
 
   return (
     <div className="App">
-      <form >
+      <form onSubmit={handleURLSubmit} >
         <input type='text' placeholder='Enter your link here' name="link" />
         <input type='submit' value='Submit' />
       </form>
     
-      
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${link}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h1>{link}</h1>
+      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${link}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   );
 }
