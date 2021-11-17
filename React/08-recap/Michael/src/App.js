08612f592ff8e6ee6471205cf470ce602ae29567
Mnkjango <mkotzker493@gmail.com>
2021-11-17 14:12:25 +0200

    Update App.js

--- React/08-recap/Michael/src/App.js
+++ React/08-recap/Michael/src/App.js
@@ -7,24 +7,24 @@ function App() {
   //get all of us
 
   //run only on start
-  useEffect(() => {
-    const Ref = doc(db, "us", "noeAEfNicuKOI0ndT4Nl");
-    getDoc(Ref).then(dataDB =>{
-      console.log(dataDB.data())
-    })
+  //useEffect(() => {
+    //const Ref = doc(db, "us", "noeAEfNicuKOI0ndT4Nl");
+    //getDoc(Ref).then(dataDB =>{
+    //  console.log(dataDB.data())
+    //})
 
     //get me  
 
-    const unsub = onSnapshot(doc(db, "us", "noeAEfNicuKOI0ndT4Nl"), (doc) => {
-      console.log("Current data: ", doc.data());
-    })
+    //const unsub = onSnapshot(doc(db, "us", "noeAEfNicuKOI0ndT4Nl"), (doc) => {
+    //  console.log("Current data: ", doc.data());
+    //})
 
-  },[])
+  //},[])
 
-  return(
-    <div className="App">
-      Hi
-    </div>
-  )
+  //return(
+    //<div className="App">
+     // Hi
+    //</div>
+  //)
 }
 export default App;
