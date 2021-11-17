0d8663cb43e434a9242ceefb6987f0e79010f1c5
ElliotKupchik <elliotkupchik@gmail.com>
2021-11-17 17:24:10 +0200

    why ISNT THIS WORKING AHHHHHHHH

--- React/08-recap/elliot/src/App.js
+++ React/08-recap/elliot/src/App.js
@@ -1,55 +1,64 @@
 import './App.css';
-import { useEffect} from 'react'
+import { useEffect, useState } from 'react'
 import { db } from './functions/firebase/config';
-import { doc, onSnapshot, getDoc, getDocs, collection, setState} from "firebase/firestore";
+import { doc, onSnapshot, addDoc, getDoc, getDocs, collection } from "firebase/firestore";
 
 
 function App() {
-  const [submitVisible, setSubmit] = setState('block');
-  const [videoVisible, setVideo] = setState('none');
-  const [videoSRC, setSRC] = setState('none');
+  const [submitVisible, setSubmit] = useState('block');
+  const [videoVisible, setVideo] = useState('none');
+  const [videoSRC, setSRC] = useState('');
+  const ourDatabase = collection(db, 'movies');
+  let ourDBArray = [];
   
   useEffect(() => {
-    /*let ourDBArray = [];
 
     getDocs(ourDatabase).then(dataDB => {
       dataDB.forEach(datum => {
         ourDBArray.push(datum.data())
-        console.log(datum.id)
-        console.log(datum.data())
       })
-      console.log(ourDBArray)
-      console.log("stage 1")
+    })
+
+    onSnapshot(ourDatabase, (snapshot) => {
+      snapshot.forEach(doc => {
+        if(ourDBArray.indexOf(doc.data()) === -1){
+          ourDBArray.push(doc.data());
+          console.log(ourDBArray);
+        }
+      })
     })
     
-    getDoc(ourElement).then(docDB => {
-      console.log(docDB.data())
-      console.log("stage 2")
-    })
-    
-    const unsubscribe = onSnapshot(ourElement, (doc) => {
-      console.log('Current Data: ' + doc.data());
-      console.log("stage 3")
-    })*/
   },[])
   
   function addVideo(ev){
+    let addedVideoLink = ev.target.elements.link.value;
     ev.preventDefault();
+    console.log("activatedAddVideo");
     if(ev.target.elements.link.value == ''){
       alert("You must input a link into the text box. Please input a link and try again.");
     } else {
-
+      addDoc(doc(db, "movies", "movie"),{url: addedVideoLink});
+      setSubmit('none');
+      changeVideo();
+      setVideo('block');
     }
+  }
+
+  function changeVideo(){
+    let randomVideo = ourDBArray[Math.floor(Math.random() * ourDBArray.length)];
+    getDoc(randomVideo).then(docDB => {
+      setSRC(docDB.src.value);
+    })
   }
 
   return (
     <div className="App">
-      <form className="submitPersonalVideo" display={submitVisible} onSubmit = {addVideo}>
+      <form className="submitPersonalVideo" display={submitVisible} onSubmit={addVideo}>
         <input type="text" name="link" placeholder="Please input the link of your video here:"></input>
         <input type="button" placeholder="Confirm"></input>
       </form>
       <form className="newVideoRequest" display = {videoVisible} onSubmit = {changeVideo}>
-        <iframe width = "300" height = "300" max-width = "750" max-height = "750" src={videoSRC} frameborder="2" autoplay> </iframe>
+        <iframe width = "300" height = "300" max-width = "750" max-height = "750" src={videoSRC}> </iframe>
         <input type="button" placeholder="Generate New Video"></input>
       </form>
     </div>
