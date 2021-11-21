6ad07be15e663cc084d40181e2e130dfaa0fd59f
Tal Yaron <tal.yaron@gmail.com>
2021-11-21 10:20:56 +0200

    Update App.js

--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -4,17 +4,23 @@ import { db } from './functions/firebase/config';
 import { doc, onSnapshot, setDoc, collection } from 'firebase/firestore';
 
 function App() {
-	const [userVideo1, setUserVideo] = useState('');
+	const [userVideo1, setUserVideo] = useState('DEV372Kof0g');
+
 	function handleSubmit(ev) {
 		ev.preventDefault();
-
+		const videoId = new RegExp('/^[A-Za-z0-9_-]{11}$/')
 		let userVideo = ev.target.elements.videoURL.value;
 		let userName = ev.target.elements.name.value;
-		setUserVideo(userVideo);
-		console.log(userVideo, userName);
-		setDoc(doc(db, 'us', userName), {
-			videoURL: userVideo,
-		});
+		if (typeof userVideo === 'string' && userVideo.match(videoId)) {
+			setUserVideo(userVideo);
+			console.log(userVideo, userName);
+			setDoc(doc(db, 'us', 'movie'), {
+				videoURL: userVideo,
+				userName
+			});
+		} else {
+			alert('This is not a valid youtube Id')
+		}
 	}
 
 	return (
@@ -24,7 +30,7 @@ function App() {
 				<input type='text' name='videoURL' placeholder='Video URl'></input>
 				<input type='submit' />
 			</form>
-      <iframe width="560" height="315" src={userVideo1} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			<iframe width="560" height="315" src={`https://www.youtube.com/embed/${userVideo1}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	);
 }
