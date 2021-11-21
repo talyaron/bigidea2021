e4083648f091bd09f7bcb6ed1a6e3df0d0b3992d
Mnkjango <mkotzker493@gmail.com>
2021-11-21 14:30:19 +0200

    lkijh

--- React/09-mouseEvents/Michael/src/App.js
+++ React/09-mouseEvents/Michael/src/App.js
@@ -1,53 +1,72 @@
-import { useRef } from 'react';
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import { useRef, useState } from 'react';
 import seaMP3 from './words/sea.mp3';
 import landMP3 from './words/land.mp3';
 
 function App() {
+	const circle = useRef(null);
+	const [circleLocation, setCircleLocation] = useState('Sea');
+	const [landSea, setlandSea] = useState('Sea');
+	const [color, setColor] = useState('green');
+  const seaSound = new Audio(seaMP3);
+  const landSound = new Audio(landMP3);
 
-  const circle = useRef(null);
-  const sea = new Audio(seaMP3);
-  const land = new Audio(landMP3);
-
-  console.dir(circle);
-
-
-  function handleClick(ev) {
-    console.log(ev)
-    console.log(ev.target.id)
-   
-    //get x and y of the click point
-    const x = ev.clientX;
-    const y = ev.clientY;
-
-    circle.current.style.top = `${y - 5}px`;
-    circle.current.style.left = `${x - 5}px`;
-
-    sea.play();
-
-  }
-  function handleClick2(ev) {
-    console.log(ev)
-    console.log(ev.target.id)
-   
-    //get x and y of the click point
-    const x = ev.clientX;
-    const y = ev.clientY;
-
-    circle.current.style.top = `${y - 5}px`;
-    circle.current.style.left = `${x - 5}px`;
-
-    land.play();
-
+	function gameMec() {
+    let tempLandSea;
+		let i = Math.floor(Math.random() * 2);
+			if (i === 1) {
+				tempLandSea = 'Sea';
+				console.log('Sea');
+        seaSound.play()
+			} else {
+				tempLandSea = 'Land'
+				console.log('Land');
+        landSound.play()
+			} 
+    setlandSea(tempLandSea)
+	}
+  function checkAnswer(id){
+    if(id === landSea){
+      console.log('true')
+      return(true)
+      
+    }
+    else{
+      alert('dumb ass mf')
+      return(false)
+    }
   }
 
-  return (
-    <div>
-      <div id='sea' className='box blue' onClick={handleClick}></div>
-      <div id='land' className='box brown' onClick={handleClick2}></div>
-      <div ref={circle} className='circle'></div>
-    </div>
-  );
+
+
+
+	function handleClick(ev) {
+		let location = ev.target.id;
+		const x = ev.clientX;
+		const y = ev.clientY;
+		circle.current.style.top = `${y - 5}px`;
+		circle.current.style.left = `${x - 5}px`;
+    setCircleLocation(location);
+		if (checkAnswer(location) === true){
+      gameMec()
+    }
+    
+
+	}
+
+	return (
+		<div>
+			<div id='start' className='start' onClick={gameMec}>
+				start game
+			</div>
+			<div id='Sea' className='box blue' onClick={handleClick}></div>
+			<div id='Land' className='box brown' onClick={handleClick}></div>
+			<div id='redC' ref={circle} className='circle'></div>
+			<div className='landSea'>{landSea}</div>
+			<div className='row' style={{ background: color }}></div>
+		</div>
+	);
 }
 
 export default App;
