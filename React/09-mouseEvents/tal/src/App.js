e639422fc99f9f10c837bfd8daf18a1156f98f99
Tal Yaron <tal.yaron@gmail.com>
2021-11-21 11:26:04 +0200

    init mouse events

--- React/09-mouseEvents/tal/src/App.js
+++ React/09-mouseEvents/tal/src/App.js
@@ -1,21 +1,9 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Liam from './pages/Liam';
-import Michael from './pages/Michael';
-
 
 function App() {
   return (
-    <BrowserRouter>
-    <ul>
-      <li><Link to='/michael'>Michael</Link></li>
-      <li><Link to='/liam'>Liam</Link></li>
-    </ul>
-      <Routes>
-        <Route exact={true} path="/" element={<Liam />} />
-        <Route path="/liam" element={<Liam />} />
-        <Route path="/michael" element={<Michael />} />
-      </Routes>
-    </BrowserRouter>
+   <div>
+     App
+   </div>
   );
 }
 
