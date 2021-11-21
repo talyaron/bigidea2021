967e6a1ad3cf401063350a909aec040c18710c07
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-21 13:51:42 +0200

    f

--- React/09-mouseEvents/zachary-szymon/src/App.js
+++ React/09-mouseEvents/zachary-szymon/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import landMP3 from './words/land.mp3'
+import seaMP3 from './words/sea.mp3'
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const sea = new Audio(seaMP3);
+  const land = new Audio(landMP3);
+ 
+
+
+  const circle=useRef(null);
+  console.dir(circle);
+  function sayLocation (){
+    setTimeout( function (ev){ 
+     
+     },Math.random()*10000+2000);
+  }
+function handleClick (ev) {
+  
+ console.log(ev)
+ console.log(ev.target.id)
+ land.play();
+
+ //get x and y of click point
+ const x=ev.clientX-10;
+ const y=ev.clientY-10;
+ 
+
+ //move circle
+circle.current.style.top=`${y}px`;
+circle.current.style.left=`${x}px`;
+}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='sea' className='box blue' onClick={handleClick}>
+        
+      </div>
+
+      <div id='land' className='box brown' onClick={handleClick}>
+        
+        </div>
+    <div ref={circle} className='circle'></div>
+
+    <button onClick={sayLocation}>Start Game</button>
+
+    
+
+
     </div>
   );
 }
