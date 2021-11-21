1f44357ff7256d92c2278b55c4d91f751f226c94
Tal Yaron <tal.yaron@gmail.com>
2021-11-21 10:25:27 +0200

    tal 1

--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -33,6 +33,6 @@ function App() {
 			<iframe width="560" height="315" src={`https://www.youtube.com/embed/${userVideo1}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	);
-}
+};
 
 export default App;
