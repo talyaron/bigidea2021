48366a81a6a41cb2eb079c4a807ce0295f263816
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-11-21 10:09:02 +0200

    pp

--- React/07-firebaseCRUD/madlibs/src/App.js
+++ React/07-firebaseCRUD/madlibs/src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import {db} from './functions/firebase/config'
+import UserPhoto from './components/UserPhoto/UserPhoto'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        BLA BLA
-      </header>
+     <UserPhoto />
+        
     </div>
   );
 }
--- React/08-recap/tal/src/App.js
+++ React/08-recap/tal/src/App.js
@@ -1,56 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import './App.css';
 import { db } from './functions/firebase/config';
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot, setDoc, collection } from 'firebase/firestore';
 
 function App() {
-  // const [userObj, setUserObj] = useState({ name: '', image: '' })
-  const [answerObj, setAnswerObj] = useState({ name: '', answer: 0 })
-  useEffect(() => {
-    // const userRef = doc(db, 'users', 'me')
-    // setDoc(userRef,{name:'Tal', image:'https://dailygazette.com/wp-content/uploads/fly-images/126428/shutterstock_245726512-scaled-940x940.jpg'})
-    // onSnapshot(userRef, userDB => {
-    //   if (userDB.exists()) {
-    //     console.log(userDB.data())
-    //     const { name, image } = userDB.data();
-    //     if (!name) console.error('No user in DB');
-    //     if (!image) console.error('No image in DB');
+	const [userVideo1, setUserVideo] = useState('');
+	function handleSubmit(ev) {
+		ev.preventDefault();
 
-    //     if (typeof image === 'string' && typeof name === 'string') {
-    //       setUserObj({ name, image })
-    //     }
-    //   }
-    // })
+		let userVideo = ev.target.elements.videoURL.value;
+		let userName = ev.target.elements.name.value;
+		setUserVideo(userVideo);
+		console.log(userVideo, userName);
+		setDoc(doc(db, 'us', userName), {
+			videoURL: userVideo,
+		});
+	}
 
-    const answersRef = doc(db, 'answers', 'answer')
-    onSnapshot(answersRef, (answerDB) => {
-      try {
-      
-        if (answerDB.exists()) {
-          console.log(answerDB.data());
-          const { name, answer } = answerDB.data();
-          if (typeof name !== 'string') throw new Error('Name is not a string');
-          if (typeof answer !== 'number') throw new Error(`answer is not a number (${name})`)
-          setAnswerObj({ name, answer });
-        }
-      } catch (err) {
-        console.error(err)
-      }
-    })
-  }, []);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className='userCard'>
-          {/* <img src={userObj.image}></img>
-          <h1>{userObj.name}</h1> */}
-          <h1>{answerObj.name}: {answerObj.answer}</h1>
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input type='text' name='name' placeholder='Name'></input>
+				<input type='text' name='videoURL' placeholder='Video URl'></input>
+				<input type='submit' />
+			</form>
+      <iframe width="560" height="315" src={userVideo1} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+		</div>
+	);
 }
 
 export default App;
