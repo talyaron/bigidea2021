bcf3982a12e2ec3e7e8bb16c5c541e08213bbd6b
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-22 14:23:48 +0200

    fvr

---
+++ React/09-mouseEvents/zachary-samuel/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'addDoc, changeDoc' from firebase;
+import db from './configfirebase/config';
+
+function App() {
+  const 
+db
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
