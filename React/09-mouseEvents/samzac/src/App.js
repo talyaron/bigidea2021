50f820c6931f4d479435bc02e0fcf7772f8ef66d
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-24 12:00:37 +0200

    cg

--- React/09-mouseEvents/samzac/src/App.js
+++ React/09-mouseEvents/samzac/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { db } from './functions/firebase/config';
+import { db } from './firebase/config.js';
 import { useEffect, useState } from 'react';
-import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 function App() {
-
+  function ResetGame (){
+    
+  }
   function handleSet(ev) {
     ev.preventDefault();
     const setName = ev.target.elements.nameBox.value;
@@ -20,6 +22,7 @@ function App() {
       <input type='text' placeholder='Enter your name' name='nameBox'/>
       <input type='text' placeholder='Enter your image url' name='imgBox'/>
       </form>
+      <button onClick={ResetGame}></button>
     </div>
   );
 }
