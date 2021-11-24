188e8026fb7897876e34afba4ab8986434d9df71
robertUfberg <robertufberg@gmail.com>
2021-11-24 11:52:13 +0200

    h

--- React/09-mouseEvents/robby/src/App.js
+++ React/09-mouseEvents/robby/src/App.js
@@ -16,6 +16,9 @@ function App() {
 	const [color, setColor] = useState('green');
   const [playing, setPlaying ]= useState(false);
   const [score, setScore ]= useState(0);
+  const [player, setPlayer]= useState("");
+  const [highScore, setHighScore]= useState(0);
+  const [highPlayer, setHighPlayer]= useState("");
 
 
   const seaSound = new Audio(seaMP3);
@@ -26,6 +29,7 @@ function App() {
 
     ev.preventDefault();
     const name= ev.target.elements.name.value;
+    setPlayer(name);
 
     if(name === ''){
       alert('Cannot display an empty name! Please fill the name and try again.')
@@ -48,7 +52,7 @@ function App() {
     var anyPlayersPlaying = false;
     FirebaseError.database().ref("landOrSea/players").on('value', function(snap){
       snap.forEach(function(childNodes){
-        if(childNodes.val().isPlaying == true){
+        if(childNodes.val().isPlaying === true){
           anyPlayersPlaying = true;
         }
       });
@@ -76,7 +80,7 @@ function App() {
   function checkAnswer(id){
     if(id === landSea){
       console.log('true')
-      setScore(score++);
+      setScore(score+1);
 
       return(true)
       
@@ -104,28 +108,37 @@ function App() {
     else if( checkPlayers() === true){
       
     } else {
-      //end game with final score
+    FirebaseError.database().ref("landOrSea/players").on('value', function(snap){
+        snap.forEach(function(childNodes){
+          if(score>highScore){
+            setHighScore(score);
+            setHighPlayer(player);
+          }
+        });
+      });
+      }
+      alert(`you all suck, but ${highPlayer} sucks the least because they got ${highScore} points. Be Better.`)
     }
 
 
+    return (
+      <div>
+        <form onSubmit={handleNameSubmit}>
+          <input name= "name" type= "text"/>
+          <input name="submit" type= "submit" value= "Confirm"/> 
+        </form>
+        <div id='Sea' className='box blue' onClick={handleClick}></div>
+        <div id='Land' className='box brown' onClick={handleClick}></div>
+        <div id='redC' ref={circle} className='circle'></div>
+        <div className='landSea'>{landSea}</div>
+        <div className='row' style={{ background: color }}></div>
+      </div>
+    );
     
 
 	}
 
-	return (
-		<div>
-      <form onSubmit={handleNameSubmit}>
-        <input name= "name" type= "text"/>
-        <input name="submit" type= "submit" value= "Confirm"/> 
-      </form>
-			<div id='Sea' className='box blue' onClick={handleClick}></div>
-			<div id='Land' className='box brown' onClick={handleClick}></div>
-			<div id='redC' ref={circle} className='circle'></div>
-			<div className='landSea'>{landSea}</div>
-			<div className='row' style={{ background: color }}></div>
-		</div>
-	);
-}
+
 
 export default App;
 /* 
