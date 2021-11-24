1dcc360f398a31fe749ba9b5cf2a8f5036b231e4
Becky Geisberg <21rgeisberg@gannacademy.org>
2021-11-24 10:23:20 +0200

    c

--- React/09-mouseEvents/becky/src/App.js
+++ React/09-mouseEvents/becky/src/App.js
@@ -1,63 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
-import {useRef} from 'react';
+import { useRef } from 'react';
 import Land from './words/land.mp3'
 import Sea from './words/sea.mp3'
+import { db } from './functions/firebase/config';
+
 
 function App() {
-  const circle =useRef(null);
+  const circle = useRef(null);
   console.dir(circle);
   const seaSound = new Audio(Sea);
   const landSound = new Audio(Land);
   let instruction;
 
-  function seaOrLand(){
-   if ((Math.floor(Math.random()*2)+1)== 1){
+  function seaOrLand() {
+    if ((Math.floor(Math.random() * 2) + 1) == 1) {
       seaSound.play()
       instruction = 'sea';
-   }
-   else {
-     landSound.play()
-     instruction = 'land';
-     
-   }
+    }
+    else {
+      landSound.play()
+      instruction = 'land';
+
+    }
   }
 
-  function checkAnswer(id){
-    if(id == instruction){
+  function checkAnswer(id) {
+    if (id == instruction) {
       console.log('true')
-      return(true)
+      return (true)
     }
     else {
       alert('You lost');
-      return(false)
+      return (false)
     }
   }
 
- 
 
-  function handleClick(ev){
+
+  function handleClick(ev) {
     console.log(ev)
     console.log(ev.target.id)
     const x = ev.clientX;
-    const y =ev.clientY;
-    circle.current.style.top =`${y - 50}px`;
+    const y = ev.clientY;
+    circle.current.style.top = `${y - 50}px`;
     circle.current.style.left = `${x - 50}px`;
-    if (checkAnswer(ev.target.id)  == true){
-    setTimeout(function(){seaOrLand()}, 1000)
-  }}
-  function handleStart(){
-    setTimeout(function(){seaOrLand()}, 1000)
+    if (checkAnswer(ev.target.id) == true) {
+      setTimeout(function () { seaOrLand() }, 1000)
+    }
+  }
+  function handleStart() {
+    setTimeout(function () { seaOrLand() }, 1000)
+  }
+
+  function handleName(ev){
+    
   }
 
 
   return (
     <div className="App">
-      
-      <div id = "sea" className=  'boxblue' onClick={handleClick}></div>
-      <div id = "land" className = 'boxgreen' onClick={handleClick}></div>
-      <div ref = {circle} className = 'circle'></div>
-      <input type = 'submit' value ='start' onClick = {handleStart}/>
+
+      <div id="sea" className='boxblue' onClick={handleClick}></div>
+      <div id="land" className='boxgreen' onClick={handleClick}></div>
+      <div ref={circle} className='circle'></div>
+      <input type='text' placeHolder='name' id = 'nameBox'/>
+      <input type='submit' value='submit' onClick={handleName}/>
+      <input type='submit' value='start' onClick={handleStart} />
+
     </div>
   );
 }
