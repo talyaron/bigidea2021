82035af159e46d70e5188d2f3af461629d18b391
Tal Yaron <tal.yaron@gmail.com>
2021-11-24 11:35:38 +0200

    Update App.js

--- React/09-mouseEvents/becky/src/App.js
+++ React/09-mouseEvents/becky/src/App.js
@@ -4,77 +4,77 @@ import { useRef } from 'react';
 import Land from './words/land.mp3'
 import Sea from './words/sea.mp3'
 import { db } from './functions/firebase/config';
-import {doc} from 'firebase/firestore'
+import { doc } from 'firebase/firestore'
 
 
 function App() {
-  const circle = useRef(null);
-  console.dir(circle);
-  const seaSound = new Audio(Sea);
-  const landSound = new Audio(Land);
-  let instruction;
-  let nameRef = useRef(null)
+    const circle = useRef(null);
+    console.dir(circle);
+    const seaSound = new Audio(Sea);
+    const landSound = new Audio(Land);
+    let instruction;
+    let nameRef = useRef(null)
 
-  function seaOrLand() {
-    if ((Math.floor(Math.random() * 2) + 1) == 1) {
-      seaSound.play()
-      instruction = 'sea';
+    function seaOrLand() {
+        if ((Math.floor(Math.random() * 2) + 1) == 1) {
+            seaSound.play()
+            instruction = 'sea';
+        }
+        else {
+            landSound.play()
+            instruction = 'land';
+
+        }
     }
-    else {
-      landSound.play()
-      instruction = 'land';
 
+    function checkAnswer(id) {
+        if (id == instruction) {
+            console.log('true')
+            return (true)
+        }
+        else {
+            alert('You lost');
+            return (false)
+        }
     }
-  }
 
-  function checkAnswer(id) {
-    if (id == instruction) {
-      console.log('true')
-      return (true)
+
+
+    function handleClick(ev) {
+        console.log(ev)
+        console.log(ev.target.id)
+        const x = ev.clientX;
+        const y = ev.clientY;
+        circle.current.style.top = `${y - 50}px`;
+        circle.current.style.left = `${x - 50}px`;
+        if (checkAnswer(ev.target.id) == true) {
+            setTimeout(function () { seaOrLand() }, 1000)
+        }
     }
-    else {
-      alert('You lost');
-      return (false)
+    // function handleStart() {
+    //     setTimeout(function () { seaOrLand() }, 1000)
+    // }
+
+    function handleName(event) {
+        event.preventDefault();
+        nameRef = doc(db, 'YB', 'playerList')
+        console.log(event.target.elements.nameBox.value)
     }
-  }
 
 
+    return (
+        <div className="App">
 
-  function handleClick(ev) {
-    console.log(ev)
-    console.log(ev.target.id)
-    const x = ev.clientX;
-    const y = ev.clientY;
-    circle.current.style.top = `${y - 50}px`;
-    circle.current.style.left = `${x - 50}px`;
-    if (checkAnswer(ev.target.id) == true) {
-      setTimeout(function () { seaOrLand() }, 1000)
-    }
-  }
-  function handleStart() {
-    setTimeout(function () { seaOrLand() }, 1000)
-  }
-
-  function handleName(event){
-    event.preventDefault();
-    nameRef = doc(db , 'YB', 'playerList') 
-    console.log(event.target.elements.nameBox.value)
-  }
-
-
-  return (
-    <div className="App">
-
-      <div id="sea" className='boxblue' onClick={handleClick}></div>
-      <div id="land" className='boxgreen' onClick={handleClick}></div>
-      <div ref={circle} className='circle'></div>
-      <form>
-      <input type='text' placeholder='name' id = 'nameBox'/>
-      <input type='submit' value='submit' onClick={handleName}/>
-      <input type='submit' value='start' onClick={handleStart} />
-      </form>
-    </div>
-  );
+            <div id="sea" className='boxblue' onClick={handleClick}></div>
+            <div id="land" className='boxgreen' onClick={handleClick}></div>
+            <div ref={circle} className='circle'></div>
+            <form onSubmit={handleName}>
+                <input type='text' placeholder='name' name='nameBox' />
+                <input type='submit' value='submit' />
+            
+            </form>
+        </div>
+    );
 }
 
 export default App;
