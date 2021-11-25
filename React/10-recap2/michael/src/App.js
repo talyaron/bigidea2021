a34ce7ace655121ec7c85d2653d8f67f85a97c1b
Mnkjango <mkotzker493@gmail.com>
2021-11-25 14:53:56 +0200

    n

---
+++ React/10-recap2/michael/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">My Time in Israel</header>
+      <div className="Body">
+        So far in Israel, I have had a very positive experience. While there have been issues, overall I have enjoyed my time here.
+        The land is very interesting, and it is fun to see all the history scattered around the country.
+        In Jerusalem, particularly the Old City, I feel like there is a spiritual uniqueness about the place.
+        The Western Wall specifically is a very spiritual experience to be near for me.
+        Aside from sprituality, Jerusalem overall has a very unique aesthetic and atmosphere when compared to other cities,
+        and it is cool to experience it firsthand and see what the place has to offer.
+      </div>
+    </div>
+  );
+}
+
+export default App;
