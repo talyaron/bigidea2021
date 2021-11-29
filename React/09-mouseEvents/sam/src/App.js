11fb8b1c4ed5e8a956ea3130f46cfcf47dfac5d1
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-11-29 11:35:11 +0200

    ;.;;

--- React/09-mouseEvents/sam/src/App.js
+++ React/09-mouseEvents/sam/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 
-var username = prompt("What is your name?");
+var year_born = prompt("Please enter your date of birth:", "Type here");
+var d = new Date();
+var n = d.getFullYear();
+function getAge(birthYear){
+	var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    age = currentYear - birthYear;
+    return age;
+}
+calculatedAge = getAge(year_born);
+alert("Hello, " + "you are " + calculatedAge + " years old!");
 
 function handleSet(ev) {
   ev.preventDefault();
