fc10566bc0561eef95342e1fdd12664382422c54
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-11-29 10:19:49 +0200

    pushy push

--- React/09-mouseEvents/max/src/App.js
+++ React/09-mouseEvents/max/src/App.js
@@ -1,72 +1,174 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import { useRef, useState } from 'react';
+import { db } from './functions/firebase/config';
+import {
+	doc,
+	setDoc,
+	addDoc,
+	collection,
+	updateDoc,
+	onSnapshot,
+	query,
+	getDoc,
+} from 'firebase/firestore';
+import { useRef, useState, useEffect } from 'react';
 import seaMP3 from './words/sea.mp3';
 import landMP3 from './words/land.mp3';
 
 function App() {
 	const circle = useRef(null);
-	const [circleLocation, setCircleLocation] = useState('Sea');
-	const [landSea, setlandSea] = useState('Sea');
-	const [color, setColor] = useState('green');
-  const seaSound = new Audio(seaMP3);
-  const landSound = new Audio(landMP3);
+	const [playerName, setPlayerName] = useState('');
+	const [playerColor, setPlayerColor] = useState('gray');
+	const [landSea, setLandSea] = useState('Sea');
+	const [playerList, setPlayerList] = useState([]);
+	const seaSound = new Audio(seaMP3);
+	const landSound = new Audio(landMP3);
+	let GameStart = false;
+	let enteredName = false;
+	let stop = true;
+	let playerListRef;
+	let gameDataRef = doc(db, 'SoL', 'game');
+	let playerIn = true; // onSnapshot the players isAlive value
+
+	useEffect(() => {
+		// attempting to snapshot game start and round time
+		// db.collection('SoL', 'players', 'playerList').onSnapshot(snapshot =>{
+		// 	setPlayerList(snapshot.docs.map(doc => doc.data()))
+		// })
+	}, []);
+
+	function handleSubmit(ev) {
+		ev.preventDefault();
+		let playerColorTemp = ev.target.elements.playerColor.value;
+		setPlayerColor(playerColorTemp);
+		let playerNameTemp = ev.target.elements.playerName.value;
+		setPlayerName(playerNameTemp);
+		playerListRef = doc(
+			db,
+			'SoL',
+			'players',
+			'playerList',
+			`${playerNameTemp}`
+		);
+		setDoc(playerListRef, {
+			playerName: `${playerNameTemp}`,
+			userColor: `${playerColorTemp}`,
+			playerScore: 0,
+			isAlive: true,
+			isStarter: false,
+		});
+		enteredName = true;
+
+		console.log('HSub ping', 'enters name ',enteredName);
+	}
+
+	function handleStart() {
+		if (enteredName == true) {
+			GameStart = true;
+			console.log(GameStart, 'game start')
+			updateDoc(playerListRef, {
+				isStarter: true,
+			}).then(
+				updateDoc(gameDataRef, {
+					gameStart: true,
+				})
+			);
+		}
+		console.log('Hstart ping');
+		gameMec();
+	}
 
 	function gameMec() {
-    let tempLandSea;
+		console.log('ping');
+		setTimeout(youLose, 2000); // make the time connected to the server
+		let tempLandSea;
 		let i = Math.floor(Math.random() * 2);
-			if (i === 1) {
-				tempLandSea = 'Sea';
-				console.log('Sea');
-        seaSound.play()
-			} else {
-				tempLandSea = 'Land'
-				console.log('Land');
-        landSound.play()
-			} 
-    setlandSea(tempLandSea)
+
+		if (i === 1) {
+			tempLandSea = 'Sea';
+			seaSound.play();
+		} else {
+			tempLandSea = 'Land';
+			landSound.play();
+		}
+		setLandSea(tempLandSea);
 	}
-  function checkAnswer(id){
-    if(id === landSea){
-      console.log('true')
-      return(true)
-      
-    }
-    else{
-      alert('dumb ass mf')
-      return(false)
-    }
-  }
-
-
-
+	function checkAnswer(id) {
+		if (id === landSea) {
+			stop = false;
+			updateDoc(playerListRef, {
+				playerScore: +1, // make this work properly
+			});
+			return true;
+		} else {
+			stop = true;
+			return false;
+		}
+	}
 
 	function handleClick(ev) {
-		let location = ev.target.id;
-		const x = ev.clientX;
-		const y = ev.clientY;
-		circle.current.style.top = `${y - 5}px`;
-		circle.current.style.left = `${x - 5}px`;
-    setCircleLocation(location);
-		if (checkAnswer(location) === true){
-      gameMec()
-    }
-    
-
+		console.log('click', GameStart, 'game start = ')
+		if (GameStart !== false) {
+			let location = ev.target.id;
+			const x = ev.clientX;
+			const y = ev.clientY;
+			circle.current.style.top = `${y - 5}px`;
+			circle.current.style.left = `${x - 5}px`; // is i want make all the players show up and locations updated on server
+			if (checkAnswer(location) === true) {
+				gameMec();
+			}
+		}
 	}
+	function youLose() {
+		if (stop === true) {
+			debugger
+			updateDoc(playerListRef, {
+				isAlive: false,
+			});
+			alert('You lost :(');
+		}
+		stop = true;
+	}
+	if (playerIn != true) {
+		// conditional render so once you lose all you see is score board
+		return (
+			<div>You are out, your score was #scoreHere#</div>
 
-	return (
-		<div>
-			<div id='start' className='start' onClick={gameMec}>
-				start game
+			//.map all the players
+		);
+	} else {
+		return (
+			<div>
+				<form onSubmit={handleSubmit}>
+					<input type='text' name='playerName' placeholder='Enter your name' />
+					<input type='text' name='playerColor' placeholder='Pick a color' />
+					<input type='submit' />
+				</form>
+
+				<div id='start' className='start' onClick={handleStart}>
+					start game
+				</div>
+				<div id='Sea' className='box blue' onClick={handleClick}></div>
+				<div id='Land' className='box brown' onClick={handleClick}></div>
+				<div
+					id='redC'
+					ref={circle}
+					className='circle'
+					style={{ background: playerColor }}>
+					{playerName}
+				</div>
+				<div className='landSea'>{landSea}</div>
+
+				{playerList.map(({ playerName, userColor, isAlive, playerScore }) => (
+					<div className='nameList'>
+						{playerName} Playing as the color:{userColor}
+						<br />
+						Score:{playerScore}
+						<br />
+						is still playing: {isAlive}
+					</div>
+				))}
 			</div>
-			<div id='Sea' className='box blue' onClick={handleClick}></div>
-			<div id='Land' className='box brown' onClick={handleClick}></div>
-			<div id='redC' ref={circle} className='circle'></div>
-			<div className='landSea'>{landSea}</div>
-			<div className='row' style={{ background: color }}></div>
-		</div>
-	);
+		);
+	}
 }
-
 export default App;
