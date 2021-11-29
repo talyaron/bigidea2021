65c1c3cc8937daa5daf9092e34476be929c350e1
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-11-29 12:54:28 +0200

    wftr4

--- React/09-mouseEvents/szymon/src/App.js
+++ React/09-mouseEvents/szymon/src/App.js
@@ -5,38 +5,42 @@ import landMP3 from './words/land.mp3'
 
 function App() {
 
-  // const circle = useRef(null);
-  // const sea = new Audio(seaMP3);
-  // const land=new Audio(landMP3)
+  const circle = useRef(null);
+  const sea = new Audio(seaMP3);
+  const land = new Audio(landMP3)
 
-  // console.dir(circle);
-
-
-  // function handleClick(ev) {
-  //   console.log(ev)
-  //   console.log(ev.target.id)
-   
-  //   //get x and y of the click point
-  //   const x = ev.clientX;
-  //   const y = ev.clientY;
-
-  //   circle.current.style.top = `${y - 5}px`;
-  //   circle.current.style.left = `${x - 5}px`;
-
-  //   sea.play();
-  //   land.play()
+  console.dir(circle);
 
 
+  function handleClick(ev) {
+    console.log(ev)
+    console.log(ev.target.id)
 
-  return (
-    <div>
-      {/* <div id='sea' className='box blue' onClick={handleClick}></div>
-      <div id='land' className='box brown' onClick={handleClick}></div>
-      <div ref={circle} className='circle'></div> */}
-      <div className="sea"></div>
-      <div className="land"></div>
-    </div>
-  );
+    //get x and y of the click point
+    const x = ev.clientX;
+    const y = ev.clientY;
+
+    circle.current.style.top = `${y - 5}px`;
+    circle.current.style.left = `${x - 5}px`;
+    const lanCount = 0;
+    const seaCount = 0;
+    if()
+
+
+    sea.play();
+    land.play();
+  }
+
+
+
+    return (
+      <div>
+        <div ref={circle} className='circle'></div> 
+        <div className="sea" onClick={handleClick}></div>
+        <div className="land" onClick={handleClick}></div>
+      </div>
+    );
+  
 }
 
 export default App;
