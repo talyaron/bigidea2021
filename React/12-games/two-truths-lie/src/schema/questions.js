703ed2ed14faddd89d98c179bdf04aa856759b3d
Tal Yaron <tal.yaron@gmail.com>
2021-12-01 13:16:48 +0200

    cont

--- React/12-games/two-truths-lie/src/schema/questions.js
+++ React/12-games/two-truths-lie/src/schema/questions.js
@@ -3,10 +3,10 @@ const questionList = [
         true1:'a true answer',
         true2:'a true answe',
         untrue:'a lie',
-        score:'0',
         user:{
             name:'the user name',
             image:'a url'
         }
     }
-]
+];
+
