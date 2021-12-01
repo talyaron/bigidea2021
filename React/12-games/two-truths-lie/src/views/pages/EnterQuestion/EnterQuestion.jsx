e83183dd21c79e7a8243743dd786a93e0d46280a
jaimemesq <jaimemesq@outlook.com>
2021-12-01 13:28:52 +0200

    Update EnterQuestion.jsx

--- React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
+++ React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
@@ -1,21 +1,27 @@
 import './EnterQuestion.css';
+import { useState } from 'react';
 
 function EnterQuestions() {
+
+    const [questionAnswered, setquestionAnswered] = useState(true);
+    const [answers, setAnswers] = useState("");
 
     function handleSubmit(ev) {
         ev.preventDefault();
         console.log(ev);
         let truth1 = ev.target.elements.truthOne.value, truth2 = ev.target.elements.truthTwo.value, lie = ev.target.elements.lie.value;
 
-        let Question = {truth1: truth1, truth2: truth2, lie: lie};
-        console.log(Question);
+        let Answers = {truth1: truth1, truth2: truth2, lie: lie};
+        console.log(Answers);
+        setquestionAnswered(false);
+        setAnswers(Answers);
     }
 
 
 
     return (
         <div className='Container'>
-            <form className = 'form' onSubmit={handleSubmit}>
+            {questionAnswered? <form className = 'form' onSubmit={handleSubmit}>
                 <label className = 'label1' for = 'truthOne'> Enter first truth</label>
                 <input type='text'name='truth1' id = 'truthOne' placeholder='truth' />
                 <label className = 'label2' for = 'truthTwo'> Enter second truth</label>
@@ -23,7 +29,7 @@ function EnterQuestions() {
                 <label className = 'label3' for = 'lie'> Enter lie</label>
                 <input type='text' name='lie' id = 'lie'placeholder='lie' />
                 <input className = 'submit' type='submit' value='submit' />
-            </form>
+            </form> : <p>Truth 1:{answers.truth1}, Truth 2:{answers.truth2}, Lie:{answers.lie}</p>}
         </div>
     )
 
