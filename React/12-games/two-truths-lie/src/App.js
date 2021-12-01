9fac92c5967cd760726b962fe7f303d326db9ac7
ElliotKupchik <elliotkupchik@gmail.com>
2021-12-01 11:06:25 +0200

    ekje

---
+++ React/12-games/two-truths-lie/src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import './App.css';
+
+//Pages
+import EnterQuestion from './views/pages/EnterQuestion/EnterQuestion';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
