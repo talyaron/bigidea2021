17f38f1bbe40fc297887ee9438ac89096af9b5d3
ykonsta <yoav.konsta@gmail.com>
2021-12-01 03:44:11 -0800

    wf

--- React/12-games/yoavspizzacharino/src/App.js
+++ React/12-games/yoavspizzacharino/src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  let untruePosition;
+  let true1Position;
+  let true2Position
+
+  function shufflePositions() {
+    let randomNumber = Math.ceil(Math.random() * 3)
+    untruePosition = randomNumber;
+
+    let randomNumber2 = Math.ceil(Math.random() * 2)
+    if (randomNumber2 === 1) {
+      if (untruePosition === 1) {
+        true1Position = 2;
+        true2Position = 3;
+      }
+      else if (untruePosition === 2) {
+        true1Position = 1;
+        true2Position = 3;
+      }
+      else if (untruePosition === 3) {
+        true1Position = 1;
+        true2Position = 2;
+      }
+    }
+    if (randomNumber2 === 2) {
+      if (untruePosition === 1) {
+        true2Position = 2;
+        true1Position = 3;
+      }
+      else if (untruePosition === 2) {
+        true2Position = 1;
+        true1Position = 3;
+      }
+      else if (untruePosition === 3) {
+        true2Position = 1;
+        true1Position = 2;
+      }
+    }
+
+    
+  }
+
+  shufflePositions();
+  console.log("True 1: " + true1Position + ",True 2: " + true2Position + ", Untrue: " + untruePosition);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
