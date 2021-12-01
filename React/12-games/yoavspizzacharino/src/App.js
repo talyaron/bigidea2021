07fbd7de101975a80855da25ee9a6976d9768b26
ykonsta <yoav.konsta@gmail.com>
2021-12-01 04:40:07 -0800

    erokfn

--- React/12-games/yoavspizzacharino/src/App.js
+++ React/12-games/yoavspizzacharino/src/App.js
@@ -1,55 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
-let x;
+import React, { useState } from 'react';
+
+
+
 
 function App() {
+  // const [true1, setTrue1] = useState("Testing true 1");
+  // const [true2, setTrue2] = useState("Testing true 2");
+  // const [untrue, setUntrue] = useState("Testing untrue");
+  let true1 = "true1";
+  let true2 = "true2";
+  let untrue = "untrue";
   let untruePosition;
-  let true1Position;
-  let true2Position
+  const [box1, setBox1] = useState("temp");
+  const [box2, setBox2] = useState("temp");
+  const [box3, setBox3] = useState("temp");
 
   function shufflePositions() {
-    let randomNumber = Math.ceil(Math.random() * 3)
-    untruePosition = randomNumber;
-
+    untruePosition = Math.ceil(Math.random() * 3)
     let randomNumber2 = Math.ceil(Math.random() * 2)
     if (randomNumber2 === 1) {
       if (untruePosition === 1) {
-        true1Position = 2;
-        true2Position = 3;
+        setBox1(untrue);
+        setBox2(true1);
+        setBox3(true2);
       }
       else if (untruePosition === 2) {
-        true1Position = 1;
-        true2Position = 3;
+        setBox1(true1);
+        setBox2(untrue);
+        setBox3(true2);
       }
       else if (untruePosition === 3) {
-        true1Position = 1;
-        true2Position = 2;
+        setBox1(true1);
+        setBox2(true2);
+        setBox3(untrue);
       }
     }
     if (randomNumber2 === 2) {
       if (untruePosition === 1) {
-        true2Position = 2;
-        true1Position = 3;
+        setBox1(untrue);
+        setBox2(true2);
+        setBox3(true1);
       }
       else if (untruePosition === 2) {
-        true2Position = 1;
-        true1Position = 3;
+        setBox1(true2);
+        setBox2(untrue);
+        setBox3(true1);
       }
       else if (untruePosition === 3) {
-        true2Position = 1;
-        true1Position = 2;
+        setBox1(true2);
+        setBox2(true1);
+        setBox3(untrue);
       }
     }
 
     
   }
 
-  shufflePositions();
-  console.log("True 1: " + true1Position + ",True 2: " + true2Position + ", Untrue: " + untruePosition);
+  function handleClick(ev){
+    ev.preventDefault();
+    shufflePositions();
+  }
+  
 
 
   return (
     <div className="App">
+      <div className = "container"> 
+        <div className = "box1" onClick = {handleClick}>{box1}</div>
+        <div className = "box2" onClick = {handleClick}>{box2}</div>
+        <div className = "box3" onClick = {handleClick}>{box3}</div>
+
+      </div>
     </div>
   );
 }
