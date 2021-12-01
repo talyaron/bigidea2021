601a56f3d77de3a31fdd8329002c62a6e17700dd
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-12-01 13:20:11 +0200

    ertr

--- React/12-games/yoavspizzacharino/src/App.js
+++ React/12-games/yoavspizzacharino/src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { db } from './firebase/config.js';
+import { useEffect, useState } from 'react';
+import { doc, setDoc, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
 
+let playersRef= doc(db,'true-lie','qocj2PnYZcvmDXOf4mCn','players')
+ 
 function App() {
+ 
   let randomLiePosition;
+
+function nextRound(){
+  randomise
+}
+
 
   function liePosition() {
     let randomNumber = Math.ceil(Math.random() * 3)
