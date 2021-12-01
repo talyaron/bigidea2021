9c1f331fc950dbfd4a6cac711d050cdda1b9e7df
robertUfberg <robertufberg@gmail.com>
2021-12-01 13:34:38 +0200

    h

--- React/12-games/two-truths-lie/src/App.js
+++ React/12-games/two-truths-lie/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import { db } from './functions/firebase/config';
+import { doc, getDoc, setDoc, onSnapshot, collection } from 'firebase/firestore'
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  function setUserInfo() {
-
+    const [isLoggedIn, setIsLoggedIn]= useState(false);
+    const [user, setUser]= useState(
+      {name: "", image: ""}
+    )
+     
     function handleSubmit(ev) {
 
         ev.preventDefault();
@@ -12,22 +18,50 @@ function App() {
         const setPic= ev.target.elements.userPicture.value;
         console.log(ev.target.elements.userName.value)
         console.log(ev.target.elements.userPicture.value)
-        const user = {name:setName, image:setPic};
-        db.ref('/person').push(person);
+          setDoc(doc(db, "true-lie", "qocj2PnYZcvmDXOf4mCn", "players", setName), {
+          image: setPic,
+          score: 0,
+          name: setName
+
+          })
+          setUser({name: setName, image: setPic})
+          setIsLoggedIn(true)
+          
+     
+
+
         
       
         
     }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Input Name Here' name="userName" />
-            <input type="text" placeholder='Input Photo Here' name="userPicture" />
-            <input type="submit" value="Submit" />
-        </form>
-    );
+    if(!isLoggedIn){
+      return (
+        <div>
+          <form onSubmit={handleSubmit}>
+              <input type="text" placeholder='Input Name Here' name="userName" />
+              <input type="text" placeholder='Input Photo Here' name="userPicture" />
+              <input type="submit" value="Submit" />
+          </form>
+  
+          </div>
+      );
+    }
+  else{
+    return(
+      <div> 
+        <div id= "namebox">
+        Your name is: {user.name}
+        </div>
+        <div id= "picture">
+          Here's a fun picture
+        <img src= {user.image}/>
+        </div>
+      </div>
+      
+    )
+  }
 }
 
-}
+
 
 export default App;
--- React/12-games/two-truths-lie/src/functions/firebase/config.js
+++ React/12-games/two-truths-lie/src/functions/firebase/config.js
@@ -1,7 +1,5 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import {getFirestore} from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,18 +7,18 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDapS68qpDP6ABTttkJQsIWFXoXIAdQTkM",
-  authDomain: "landorsea-bde63.firebaseapp.com",
-  projectId: "landorsea-bde63",
-  storageBucket: "landorsea-bde63.appspot.com",
-  messagingSenderId: "461272467675",
-  appId: "1:461272467675:web:c2591ece0443e6f8dee4fe",
-  measurementId: "G-VW89WM6L9E"
+  apiKey: "AIzaSyDBrctixsp9hagB4axdLENPLTXSfzhOUl8",
+  authDomain: "common-tst.firebaseapp.com",
+  projectId: "common-tst",
+  storageBucket: "common-tst.appspot.com",
+  messagingSenderId: "253126688266",
+  appId: "1:253126688266:web:66b65b2bb48a5340e9e9f3",
+  measurementId: "G-0CELWFLDGP"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
  
