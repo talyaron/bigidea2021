961a8d6334feb5d9379aa4d6c3f4f1eba081095b
Tal Yaron <tal.yaron@gmail.com>
2021-12-01 11:02:04 +0200

    questionList schema

---
+++ React/12-games/two-truths-lie/src/schema/questions.js
@@ -0,0 +1,11 @@
+const questionList = [
+    {
+        true1:'a true answer',
+        true2:'a true answe',
+        untrue:'a lie',
+        user:{
+            name:'the user name',
+            image:'a url'
+        }
+    }
+]
