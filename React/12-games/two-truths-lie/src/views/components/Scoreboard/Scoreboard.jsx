3abe85b93245739e495176dc4d0a4b083c398161
SamuelSpiz <91409481+SamuelSpiz@users.noreply.github.com>
2021-12-01 14:29:59 +0200

    a

--- React/12-games/two-truths-lie/src/views/components/Scoreboard/Scoreboard.jsx
+++ React/12-games/two-truths-lie/src/views/components/Scoreboard/Scoreboard.jsx
@@ -1,8 +1,8 @@
 import "./Scoreboard.css"
 import "./config.js"
 import { useEffect, useState } from "react"
-// import {db} from './config'
-// import {collection,query,where,getDocs,} from 'firebase/firestore'
+import {db} from './config'
+import {doc,collection,query,where,getDocs,onSnapshot} from 'firebase/firestore'
 
 function Scoreboard() {
 
@@ -11,9 +11,25 @@ function Scoreboard() {
         { id: 'aaa', name: 'Syzmon', image: '', score: 12 },
         { id: 'bbb', name: 'Max', image: '', score: 10 }
     ])
-
+    const q=query(collection(db,"true-lie","qocj2PnYZcvmDXOf4mCn","players" ))
     useEffect(() => {
         //listen to palyers
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const list = [];
+        querySnapshot.forEach((doc) => {
+            list.push(doc.data());
+
+            for(let i=0; i<= list.length; i++){
+                for(let k=0; k<= list.length; k++){
+                    //if num is higher, move to front
+                    //if lower move to back
+        }
+        }
+        });
+        console.log(list);
+        setPlayers(list)
+      });      
+
     }, [])
 
     return (
@@ -21,6 +37,7 @@ function Scoreboard() {
             {players.map(player => {
                 return (
                     <div key={player.id} className='nametag'>
+                        <img className="img"src={player.image}></img>
                         {player.name}: {player.score}
                     </div>
                 )
--- React/12-games/two-truths-lie/src/views/components/Scoreboard/config.js
+++ React/12-games/two-truths-lie/src/views/components/Scoreboard/config.js
@@ -1,16 +1,22 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from 'firebase/firestore';
+// // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-// apiKey: "AIzaSyDBrctixsp9hagB4axdLENPLTXSfzhOUl8",
-// authDomain: "common-tst.firebaseapp.com",
-// projectId: "common-tst",
-// storageBucket: "common-tst.appspot.com",
-// messagingSenderId: "253126688266",
-// appId: "1:253126688266:web:66b65b2bb48a5340e9e9f3",
-// measurementId: "G-0CELWFLDGP"
-// };
+// Your web app's Firebase configuration
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyDBrctixsp9hagB4axdLENPLTXSfzhOUl8",
+  authDomain: "common-tst.firebaseapp.com",
+  projectId: "common-tst",
+  storageBucket: "common-tst.appspot.com",
+  messagingSenderId: "253126688266",
+  appId: "1:253126688266:web:66b65b2bb48a5340e9e9f3",
+  measurementId: "G-0CELWFLDGP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
