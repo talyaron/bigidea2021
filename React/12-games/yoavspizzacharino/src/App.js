90dc2a7b8ea9c67cc911711a4492cd7aa994b742
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-12-01 13:58:47 +0200

    gt

--- React/12-games/yoavspizzacharino/src/App.js
+++ React/12-games/yoavspizzacharino/src/App.js
@@ -1,18 +1,50 @@
 import './App.css';
 import { db } from './firebase/config.js';
 import { useEffect, useState } from 'react';
-import { doc, setDoc, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
+import { doc, setDoc, updateDoc, deleteDoc, onSnapshot, collection,query,getDocs } from 'firebase/firestore';
 
-let playersRef= doc(db,'true-lie','qocj2PnYZcvmDXOf4mCn','players')
- 
+let questionsRef
+let questionsArr = []
+
 function App() {
- 
+  
+
+  useEffect(() => {
+    
+    async function OnStartup() {
+      //questionsRef = await collection(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions')
+      const q = query(collection(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions'));
+      const querySnapshot = await getDocs(q);
+      let i=1;
+      querySnapshot.forEach((doc) => {
+        let data = doc.data();
+        let tempObj = {
+          // name: data.user.name,
+          true1: data.true1,
+          true2: data.true2,
+          false: data.false,
+          index:i
+        }
+       i++;
+        questionsArr.push(tempObj)
+        console.log(questionsArr);
+      })
+    }
+    OnStartup();
+   
+  }, []);
+
+
   let randomLiePosition;
 
-function nextRound(){
-  randomise
-}
 
+
+  function nextRound() {
+    randomise()
+  }
+  async function randomise() {
+    let random = Math.floor(Math.random() * 6)
+  }
 
   function liePosition() {
     let randomNumber = Math.ceil(Math.random() * 3)
