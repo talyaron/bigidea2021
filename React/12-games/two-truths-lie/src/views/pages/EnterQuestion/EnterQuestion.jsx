c5cd3cf4f38c6a02d05a424566bd64fd7dcdb2c7
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-01 11:15:26 +0200

    push

--- React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
+++ React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
@@ -1,19 +1,20 @@
-import './EnterQuestions.css'
+// import './EnterQuestions.css'
 
 function EnterQuestions() {
     function handleSubmit(ev) {
-        
+        ev.preventDefault()
+        console.log(ev)
     }
 
 
 
     return (
         <div className='Container'>
-            <form onSubmit='handleSubmit'>
+            <form onSubmit={handleSubmit}>
                 <input type='text' name='truth1' placeholder='enter first truth here' />
                 <input type='text' name='truth2' placeholder='enter second truth here' />
                 <input type='text' name='lie' placeholder='enter lie here' />
-                <input type='submit' placeholder='submit' />
+                <input type='submit' value='submit' />
             </form>
         </div>
     )
