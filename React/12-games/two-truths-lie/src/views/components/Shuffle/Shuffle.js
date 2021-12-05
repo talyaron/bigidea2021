b76b0af26c63ab3643a6639ffdd72e3a7b874107
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-12-05 13:18:56 +0200

    dfg

--- React/12-games/two-truths-lie/src/views/components/Shuffle/Shuffle.js
+++ React/12-games/two-truths-lie/src/views/components/Shuffle/Shuffle.js
@@ -7,7 +7,9 @@ function Shuffle({true1, true2, untrue}) {
   // const [true1, setTrue1] = useState("Testing true 1");
   // const [true2, setTrue2] = useState("Testing true 2");
   // const [untrue, setUntrue] = useState("Testing untrue");
-  
+  const [box1Id,setBox1Id]=useState("temp");
+  const [box2Id,setBox2Id]=useState("temp");
+  const [box3Id,setBox3Id]=useState("temp");
   let untruePosition;
   const [box1, setBox1] = useState("temp");
   const [box2, setBox2] = useState("temp");
@@ -21,16 +23,25 @@ function Shuffle({true1, true2, untrue}) {
         setBox1(untrue);
         setBox2(true1);
         setBox3(true2);
+        setBox1Id("untrue")
+        setBox3Id("true")
+        setBox2Id("true")
       }
       else if (untruePosition === 2) {
         setBox1(true1);
         setBox2(untrue);
         setBox3(true2);
+        setBox1Id("true")
+        setBox2Id("untrue")
+        setBox3Id("true")
       }
       else if (untruePosition === 3) {
         setBox1(true1);
         setBox2(true2);
         setBox3(untrue);
+        setBox1Id("true")
+        setBox2Id("true")
+        setBox3Id("untrue")
       }
     }
     if (randomNumber2 === 2) {
@@ -38,16 +49,25 @@ function Shuffle({true1, true2, untrue}) {
         setBox1(untrue);
         setBox2(true2);
         setBox3(true1);
+        setBox1Id("untrue")
+        setBox2Id("true")
+        setBox3Id("true")
       }
       else if (untruePosition === 2) {
         setBox1(true2);
         setBox2(untrue);
         setBox3(true1);
+        setBox1Id("true")
+        setBox2Id("untrue")
+        setBox3Id("true")
       }
       else if (untruePosition === 3) {
         setBox1(true2);
         setBox2(true1);
         setBox3(untrue);
+        setBox1Id("true")
+        setBox2Id("true")
+        setBox3Id("untrue")
       }
     }
 
@@ -64,9 +84,9 @@ function Shuffle({true1, true2, untrue}) {
   return (
     <div className="App">
       <div className = "container"> 
-        <div className = "box1" onClick = {handleClick}>{box1}</div>
-        <div className = "box2" onClick = {handleClick}>{box2}</div>
-        <div className = "box3" onClick = {handleClick}>{box3}</div>
+        <div className = {box1Id} onClick = {handleClick}>{box1}</div>
+        <div className = {box2Id} onClick = {handleClick}>{box2}</div>
+        <div className = {box3Id} onClick = {handleClick}>{box3}</div>
 
       </div>
     </div>
