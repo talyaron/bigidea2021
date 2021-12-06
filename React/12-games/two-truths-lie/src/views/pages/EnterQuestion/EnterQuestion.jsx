42d698c3f852c037b64b852d624047c280c4fd83
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-06 13:54:48 +0200

    push

--- React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
+++ React/12-games/two-truths-lie/src/views/pages/EnterQuestion/EnterQuestion.jsx
@@ -1,40 +1,51 @@
 import './EnterQuestion.css';
 import { useState } from 'react';
 import { db } from '../../../functions/firebase/config';
-import { onSnapshot, doc, getDoc, addDoc, collection } from 'firebase/firestore';
+import { onSnapshot, doc, getDoc, addDoc, collection, deleteDoc, getDocs, query } from 'firebase/firestore';
+import { async } from '@firebase/util';
 
-function EnterQuestions({user, setUser}) {
+function EnterQuestions({ user, setUser }) {
 
-    console.log(user)
-
+    // console.log(user)
+    const questionCollectionRef = collection(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions')
     const [questionAnswered, setquestionAnswered] = useState(true);
     const [answers, setAnswers] = useState("");
 
     function handleSubmit(ev) {
         ev.preventDefault();
-        console.log(ev);
+        // console.log(ev);
         let true1 = ev.target.elements.truthOne.value, true2 = ev.target.elements.truthTwo.value, untrue = ev.target.elements.lie.value;
 
         //addDoc to DB
         const questionsRef = collection(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions');
-        addDoc(questionsRef, {true1, true2, untrue, user});
+        addDoc(questionsRef, { true1, true2, untrue, user });
 
         setquestionAnswered(false);
         setAnswers(`Truth 1: ${true1}, Truth 2: ${true2}, Lie: ${untrue}`);
     }
 
 
+    async function handleClear() {
+        const q = query(collection(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions'));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((docDB) => {
+            deleteDoc(doc(db, 'true-lie', 'qocj2PnYZcvmDXOf4mCn', 'questions', docDB.id))
+        });
+    }
+
+
 
     return (
         <div className='Container'>
-            {questionAnswered? <form className = 'form' onSubmit={handleSubmit}>
-                <label className = 'label1' htmlFor = 'truthOne'> Enter first truth</label>
-                <input type='text'name='truth1' id = 'truthOne' placeholder='truth' />
-                <label className = 'label2' htmlFor = 'truthTwo'> Enter second truth</label>
-                <input type='text' name='truth2' id = 'truthTwo' placeholder='truth' />
-                <label className = 'label3' htmlFor = 'lie'> Enter lie</label>
-                <input type='text' name='lie' id = 'lie'placeholder='lie' />
-                <input className = 'submit' type='submit' value='submit' />
+            <button onClick={handleClear}>Clear All Questions</button>
+            {questionAnswered ? <form className='form' onSubmit={handleSubmit}>
+                <label className='label1' htmlFor='truthOne'> Enter first truth</label>
+                <input type='text' name='truth1' id='truthOne' placeholder='truth' />
+                <label className='label2' htmlFor='truthTwo'> Enter second truth</label>
+                <input type='text' name='truth2' id='truthTwo' placeholder='truth' />
+                <label className='label3' htmlFor='lie'> Enter lie</label>
+                <input type='text' name='lie' id='lie' placeholder='lie' />
+                <input className='submit' type='submit' value='submit' />
             </form> : <p>{answers}</p>}
         </div>
     )
