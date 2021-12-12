70006990bd30f967e984c7764e32a5312c10a176
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-12 13:00:42 +0200

    layout

---
+++ Impact/schema.js
@@ -0,0 +1,35 @@
+const DB =[
+    {users,
+        JoeDoe:{
+            name:'JoeDoe',
+            email:'JoeDoe@gmail.com',
+            userID:'123ABC345daef123',
+            sex:'male/female/nonBinary/other',
+            age:0,
+            location:'jerusalem, Israel',
+            role:'ole/admin/superAdmin',
+            organization:'Soup Kitchen',
+            userPref:{
+                dinner,
+                shabbat,
+                family,
+            }
+        }
+    },
+    {articles,
+        ranNumGen:{
+            eventName: 'Dinner with the Rabbi',
+            organization:'Soup Kitchen',
+            location:'jerusalem, Israel',
+            date:'00/00/00',
+            coverImage:{imgeURLimg123com},
+            maxCapacity:100,
+            article:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+            tags:{
+                dinner,
+                shabbat,
+                family,
+            }
+        }
+    }
+]
