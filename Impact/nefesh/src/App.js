fd3b9c2e9e9b077101e9883aa616cef4fc5dc889
SzymontheDestroyer <91409086+SzymontheDestroyer@users.noreply.github.com>
2021-12-13 11:44:09 +0200

    a
    a

--- Impact/nefesh/src/App.js
+++ Impact/nefesh/src/App.js
@@ -1,10 +1,12 @@
 import SignUp from "./pages/SignUp/SignUp"
+import Pages from "./pages/Login/Login"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Pages/>
     </div>
   );
 }
