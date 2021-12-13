5bdaaf1749e64a0cb8652aaeebfd57e5979c9ed9
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-13 11:05:06 +0200

    admin button

---
+++ Impact/nefesh/src/components/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,16 @@
+
+function ProfilePage(){
+    function adminPageButton(){
+        //render admin popup
+    }
+
+
+
+    return(
+        <div>
+            <h4>userNameHere</h4>
+            <button onClick='adminPageButton' name='adminButton'/>
+
+        </div>
+    )
+}
