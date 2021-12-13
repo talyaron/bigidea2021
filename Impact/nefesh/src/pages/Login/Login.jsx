e337ef9b1dd2b1ef8cc4c46742ae94006e509a1e
Mnkjango <mkotzker493@gmail.com>
2021-12-13 13:20:55 +0200

    m

--- Impact/nefesh/src/pages/Login/Login.jsx
+++ Impact/nefesh/src/pages/Login/Login.jsx
@@ -3,14 +3,14 @@ import './Login.css';
 function Page() {
     return (
       <div className="login">
-          <header>Login</header>
+          <header className='header'>Login</header>
        
-            <form onSubmit='handleSubmit(event)'>
-                <label for="text">Username</label>
-                <input type="text" name="text" id="text" />
-                <label for="text">Password</label>
-                <input type="text" name="text" id="text" />
-                <input type="submit" name="submit" id="submit" />
+            <form className='form' onSubmit='handleSubmit(event)'>
+                <label className='label' for="text">Username</label>
+                <input className='input' type="text" name="text" id="text" />
+                <label className='label' for="text">Password</label>
+                <input className='input' type="text" name="text" id="text" />
+                <input  classname='input' type="submit" name="submit" id="submit" />
             </form>
        
       </div>
