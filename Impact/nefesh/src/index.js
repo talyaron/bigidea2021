0f9a5a30976be1b762e746dadab78cff891fea10
ykonsta <yoav.konsta@gmail.com>
2021-12-13 11:13:07 +0200

    r

--- Impact/nefesh/src/index.js
+++ Impact/nefesh/src/index.js
@@ -5,8 +5,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
-
+import "./index.css"
+import Login from "./pages/login/Login.js"
+import Home from"./pages/home/Home.js"
+import Error from "./pages/404/404.js"
+import Unauthorised from "./pages/401/401.js"
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
@@ -20,21 +23,15 @@ render(
       <Route path="home" element={<Home />} />
       <Route path="login" element={<Login />} />
       <Route path="blank" element={<Blank />}/>
+      <Route path="404" element={<Error />}/>
+      <Route path="401" element={<Unauthorised />}/>
     </Routes>
   </BrowserRouter>,
   rootElement
 );
 
-function Home (){
-  return (<div>
-    Home
-  </div>)
-}
-function Login(){
-  return(<div>
-    Login
-  </div>)
-}
+
+
 function Blank (){
   return(<div>
     Hi
