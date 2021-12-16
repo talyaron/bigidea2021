b32e889ecc7314ef3014cc456959780b7a3cac37
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-16 11:20:55 +0200

    fix error and added change display name function

--- Impact/nefesh/src/components/AdminPage/AdminPage.jsx
+++ Impact/nefesh/src/components/AdminPage/AdminPage.jsx
@@ -1,75 +1,73 @@
 import React, { useEffect } from 'react';
 import './AdminPage.css';
-// import { db } from '../../functions/firebase/config';
-// import { doc, collection, query, where, getDoc, onSnapshot } from 'firebase/firestore'
-// import { async } from '@firebase/util';
+import { db } from '../../functions/firebase/config';
+import {
+	doc,
+	collection,
+	query,
+	where,
+	getDoc,
+	onSnapshot,
+	updateDoc,
+} from 'firebase/firestore';
+import { async } from '@firebase/util';
 
 const AdminPage = (props) => {
-// const userIDRef = 'pvfu0JLfWT8omzrVMPqY' // this will change based on the profile page pulled rn
-// const userDocRef = doc(db, 'users', userIDRef)
+	const userIDRef = 'pvfu0JLfWT8omzrVMPqY'; // this will change based on the profile page pulled rn
+	const userDocRef = doc(db, 'users', 'pvfu0JLfWT8omzrVMPqY');
 
-// useEffect(()=>{
+	useEffect(() => {
+		console.log(getDoc(userDocRef), 'hi');
+	}, []);
 
-// 	console.log(getDoc(userDocRef), 'hi')
-// },[])
-
-	function handleChangeDisplayName(ev){
-
-        /* make sure to check that username is not taken */
-
-        // let username = prompt("Enter your new username.")
-		// console.log(ev , username);
-        // updateDoc(ref, id) {
-        //     displayName: username
-		// 	 }
-		(async () => {
+	function handleChangeDisplayName(ev) {
+		/* make sure to check that username is not taken */
+		let username = prompt('Enter your new username.');
+		console.log(ev, username);
+		updateDoc(userDocRef,{
+				displayName: username
+			})
+		.then(async () => {
 			// create and show the notification
 			const showNotification = () => {
 				// create a new notification
 				const notification = new Notification('UPDATE', {
-					body: 'CHANGES SAVED'
+					body: 'CHANGES SAVED',
 				});
-		
 				// close the notification after 10 seconds
 				setTimeout(() => {
 					notification.close();
 				}, 10 * 1000);
-		
-			}
-		
+			};
 			// show an error message
 			const showError = () => {
 				const error = document.querySelector('.error');
 				error.style.display = 'block';
 				error.textContent = 'You blocked the notifications';
-			}
-		
+			};
 			// check notification permission
 			let granted = false;
-		
 			if (Notification.permission === 'granted') {
 				granted = true;
 			} else if (Notification.permission !== 'denied') {
 				let permission = await Notification.requestPermission();
 				granted = permission === 'granted' ? true : false;
 			}
-		
 			// show notification or error
 			granted ? showNotification() : showError();
-		
-		})();
+		});
 	}
-	function handleSetRoleToOle(ev){
-		console.log(ev ,'handleSetRoleToOle');
+	function handleSetRoleToOle(ev) {
+		console.log(ev, 'handleSetRoleToOle');
 	}
-	function handleSetRoleToOrgAdmin(ev){
-		console.log(ev ,'handleSetRoleToOrgAdmin');
+	function handleSetRoleToOrgAdmin(ev) {
+		console.log(ev, 'handleSetRoleToOrgAdmin');
 	}
-	function handleSuspendUser(ev){
-		console.log(ev ,'handleSuspendUser');
+	function handleSuspendUser(ev) {
+		console.log(ev, 'handleSuspendUser');
 	}
-	function handleBanUser(ev){
-		console.log(ev ,'handleBanUser');
+	function handleBanUser(ev) {
+		console.log(ev, 'handleBanUser');
 	}
 
 	return (
@@ -79,15 +77,23 @@ const AdminPage = (props) => {
 					x
 				</span>
 				{props.content}
-                {/* Buttons for user settings */}
-                <div className='buttonContainer'>
-                    <button id='changeDisplayName' onClick={handleChangeDisplayName}>Change Display Name</button>
-                    <button id='setRoleToOle' onClick={handleSetRoleToOle}>Set Role To Ole</button>
-					<button id='setRoleToOrgAdmin' onClick={handleSetRoleToOrgAdmin}>Set Role To Org Admin</button>
-					<button id='suspendUser' onClick={handleSuspendUser}>Suspend User</button>
-					<button id='banUser' onClick={handleBanUser}>Ban User</button>
-
-                </div>
+				<div className='buttonContainer'>
+					<button id='changeDisplayName' onClick={handleChangeDisplayName}>
+						Change Display Name
+					</button>
+					<button id='setRoleToOle' onClick={handleSetRoleToOle}>
+						Set Role To Ole
+					</button>
+					<button id='setRoleToOrgAdmin' onClick={handleSetRoleToOrgAdmin}>
+						Set Role To Org Admin
+					</button>
+					<button id='suspendUser' onClick={handleSuspendUser}>
+						Suspend User
+					</button>
+					<button id='banUser' onClick={handleBanUser}>
+						Ban User
+					</button>
+				</div>
 			</div>
 		</div>
 	);
