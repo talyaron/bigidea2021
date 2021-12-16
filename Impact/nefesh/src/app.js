dcaa6e5c14e6f19106c38b82679e822f4886fe22
ykonsta <yoav.konsta@gmail.com>
2021-12-16 13:37:29 +0200

    e

--- Impact/nefesh/src/app.js
+++ Impact/nefesh/src/app.js
@@ -1,9 +1,9 @@
 import './app.css';
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
 } from "react-router-dom";
 
 import Login from "./pages/login/Login.js"
@@ -11,30 +11,48 @@ import Home from "./pages/home/Home.js"
 import Error from "./pages/404/404.js"
 import Unauthorised from "./pages/401/401.js"
 import Hi from "./pages/hi/Hi.js"
-import {checkRole} from "./functions/general.js"
-
-
-let role="ole"
+import { checkRole } from "./functions/general.js"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from 'react';
+import { doc, setDoc } from "firebase/firestore"
+import {db} from "./functions/firebase/config"
+let role = "superAdmin"
 let permissionedRole
+const auth = getAuth();
 function App() {
-
-    return (
-        <BrowserRouter>
-            <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/home">Home</Link>
-                <Link to="/hi">Hi</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="home" element={<Home role={role}/>} />
-                <Route path="login" element={<Login />} />
-                <Route path="hi" element={<Hi />} />
-                <Route path="404" element={<Error />} />
-                <Route path="401" element={<Unauthorised />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setDoc(doc(db, "users", user.email), {
+          displayName: user.displayName,
+          email: user.email,
+          userIcon: user.photoURL
+        })
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }, [])
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/hi">Hi</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home role={role} />} />
+        <Route path="login" element={<Login />} />
+        <Route path="hi" element={<Hi />} />
+        <Route path="404" element={<Error />} />
+        <Route path="401" element={<Unauthorised />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- Impact/nefesh/src/pages/login/Login.js
+++ Impact/nefesh/src/pages/login/Login.js
@@ -1,42 +1,36 @@
-import {db} from "../../functions/firebase/config"
+import { db } from "../../functions/firebase/config"
 import React from "react"
-import {authentication} from "../../functions/firebase/config"
-import {signInWithPopup, GoogleAuthProvider} from "firebase/auth";
-import {doc,setDoc} from "firebase/firestore"
-function Login(){
+import { authentication } from "../../functions/firebase/config"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+function Login() {
 
-const SignIn=(ev)=>{
-  ev.preventDefault()
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(authentication, provider)
-  .then((re)=>{
-    console.log(re)
-    console.log(re.user.displayName)
-    console.log(re.user.email)
-    console.log(re.user.photoURL)
-    let userEmail=re.user.email
-    setDoc(doc(db,"users",{userEmail}),{
-      displayName:re.user.displayName,
-      email:re.user.email,
-      userIcon:re.user.photoURL
-    })
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
-}
-
-    return(<div>
-      <h1>
-            login
-      </h1>
-      <form className="Login">
-        <input placeholder="Email Address"></input>
-        <input type="password" placeholder="Password"></input>
-        <input type="submit"></input>
-        <button onClick={SignIn}>Sign In with google</button>
-      </form>
-    </div>)
+  const SignIn = (ev) => {
+    ev.preventDefault()
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((re) => {
+        console.log(re)
+        console.log(re.user.displayName)
+        console.log(re.user.email)
+        console.log(re.user.photoURL)
+        let userEmail = re.user.email
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
-  export default Login;
+  return (<div>
+    <h1>
+      login
+    </h1>
+    <form className="Login">
+      <input placeholder="Email Address"></input>
+      <input type="password" placeholder="Password"></input>
+      <input type="submit"></input>
+      <button onClick={SignIn}>Sign In with google</button>
+    </form>
+  </div>)
+}
+
+export default Login;
