3afaf3d654fa972d480b3690ed6663044c705275
robertUfberg <robertufberg@gmail.com>
2021-12-16 11:43:52 +0200

    jh

--- Impact/nefesh/src/App.js
+++ Impact/nefesh/src/App.js
@@ -1,11 +1,34 @@
-
-import './App.css';
-
+import React, { useState } from 'react';
+import ArticleCreation from './articleCreation/ArticleCreation';
+ 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+ 
+  return <div>
+    <input
+      type="button"
+      value="Click to Create Article"
+      onClick={togglePopup}
+    />
+    
+    {isOpen && <ArticleCreation
+      content={<>
+        <b>Input information here</b>
+        <input type= "text" placeholder= "Enter article title here"/>
+        <br/>
+        <input type= "text" placeholder= "Enter name here"/>
+        <br/>
+        <input type= "text" placeholder= "Enter text here"/>
+        <br/>
+        <button>Create Article</button>
+      </>}
+      handleClose={togglePopup}
+    />}
+  </div>
 }
-
+ 
 export default App;
---
+++ Impact/nefesh/src/articleCreation/ArticleCreation.jsx
@@ -0,0 +1,18 @@
+
+
+import './ArticleCreation.css';
+import React from "react";
+
+
+const Popup = props => {
+    return (
+      <div className="popup-box">
+        <div className="box">
+          <span className="close-icon" onClick={props.handleClose}>x</span>
+          {props.content}
+        </div>
+      </div>
+    );
+  };
+   
+  export default Popup;
