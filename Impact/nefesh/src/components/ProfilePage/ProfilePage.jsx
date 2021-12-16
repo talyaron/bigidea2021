e5fb316ce4666b926be0cf266d988aa189b25fb2
Shark9y <91408516+Shark9y@users.noreply.github.com>
2021-12-16 13:40:13 +0200

    pro

--- Impact/nefesh/src/components/ProfilePage/ProfilePage.jsx
+++ Impact/nefesh/src/components/ProfilePage/ProfilePage.jsx
@@ -1,49 +1,56 @@
 import AdminPage from '../AdminPage/AdminPage';
 import React, { useEffect, useState } from 'react';
 import { db } from '../../functions/firebase/config';
-import { doc, getDoc, onSnapshot} from 'firebase/firestore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 
 var isAdmin = true;
 const userDocRef = doc(db, 'users', 'pvfu0JLfWT8omzrVMPqY');
 
+function ProfilePage() {
+	const [displayName, setDisplayName] = useState('displayName');
+	const [userID, setUserID] = useState('ID');
+	const [isOpen, setIsOpen] = useState(false);
+	const togglePopup = () => {
+		setIsOpen(!isOpen);
+	};
 
-function ProfilePage(){
-    const [displayName, setDisplayName] = useState("displayName");
-	const [userID, setUserID] = useState("ID");
-    const [isOpen, setIsOpen] = useState(false);
-    const togglePopup = () => {setIsOpen(!isOpen);}
+	useEffect(() => {
+		//pull userId of selected user and set for superAdmin page
+		//on snapshot displayName
+		const userData = onSnapshot(userDocRef, (userDB) => {
+			let displayNameTemp = userDB.data().displayName;
+            let userIDTemp = userDB.data().userID;
+			setDisplayName(displayNameTemp);
+            setUserID(userIDTemp)
+		});
+		//get doc user ID
+        
+	}, []);
 
-    useEffect(()=>{
-        //pull userId of selected user and set for superAdmin page
-        //on snapshot displayName
-        const userData = onSnapshot(userDocRef, (userDB) => {
-            console.log("Current data: ", userDB.data().displayName);
-            let displayNameTemp = userDB.data().displayName
-            setDisplayName(displayNameTemp);
-
-        });        
-        //get doc user ID
-        const tempUserID = getDoc(userDocRef,(userIdDB)=>{
-            console.log(userIdDB.data().userID);
-            
-        })
-
-        setUserID(tempUserID);
-    },[])
-
-    return(
-        <div>
-            <h4>{displayName}</h4>
-            {isAdmin ? <input type="button" value="User Settings" onClick={togglePopup} className="adminButton"/> : null}
-            {isOpen && <AdminPage
-            content={<>
-                <b>{displayName}</b>
-                <p>{userID}</p>
-            </>}
-            handleClose={togglePopup}
-            />}
-        </div>
-    )
+	return (
+		<div>
+			<h4>{displayName}</h4>
+			{isAdmin ? (
+				<input
+					type='button'
+					value='User Settings'
+					onClick={togglePopup}
+					className='adminButton'
+				/>
+			) : null}
+			{isOpen && (
+				<AdminPage
+					content={
+						<>
+							<b>{displayName}</b>
+							<p> UserID: '{userID}'</p>
+						</>
+					}
+					handleClose={togglePopup}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default ProfilePage;
