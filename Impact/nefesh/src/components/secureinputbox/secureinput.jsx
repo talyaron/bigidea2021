04d6cd32c7846cc91f4e9f6f9a126569468aea77
Levid0 <89023126+Levid0@users.noreply.github.com>
2021-12-16 14:16:53 +0200

    ww

--- Impact/nefesh/src/components/secureinputbox/secureinput.jsx
+++ Impact/nefesh/src/components/secureinputbox/secureinput.jsx
@@ -1,10 +1,10 @@
 import './secureinputbox.css'
 
-
+import MaterialCommunitiIconslcon from "react-native-vector-icons/dist/MaterialIcons";
 
 function secureinputbox(){
     return(
-        <input type="username" />
+        <input type="Password" />
     )
 }
 
