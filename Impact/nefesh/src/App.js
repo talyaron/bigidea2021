5a52b0573b2bf00fd82e82ab1e740756024e9dda
Tal Yaron <tal.yaron@gmail.com>
2021-12-19 10:27:37 +0200

    Revert "commited!"
    This reverts commit 14db259cff658ee286a9d38252b6d6712302b704.

---
+++ Impact/nefesh/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useState } from 'react';
+import ProfilePage from './components/ProfilePage/ProfilePage';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ProfilePage />
+    </div>
+  );
+}
+
+export default App;
