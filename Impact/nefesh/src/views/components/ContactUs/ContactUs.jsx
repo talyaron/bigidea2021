a2f81c7d3ad9f0fd84b90f66620b3cf024ebc7ba
Tal Yaron <tal.yaron@gmail.com>
2021-12-19 11:42:53 +0200

    das

---
+++ Impact/nefesh/src/views/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,23 @@
+import './ContactUs.css';
+function ContactUs (){
+    function redirect(){ 
+
+      var email = "xyz@something.com"
+      var mailto_link = 'mailto:' + email
+      
+}
+    return(
+        <div>
+      <div>You can contact us at: </div>
+      <div className="contactWrapper">
+      <div className= "phoneNum">
+           Phone number: ###-###-####
+           </div>
+        <div className= "or">OR</div>
+        <a href="mailto:Nefesh@nefesh.com?subject=Contacting Creators!&body=Hi! I wanted to contact you to tell you that (type here)"className="email">Click here to email us</a>
+        </div>
+        </div>
+
+    )
+}
+export default ContactUs;
