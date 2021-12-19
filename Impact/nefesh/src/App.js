bbf0e326e17901ea6d543cbdd0fed06ad9618371
Tal Yaron <tal.yaron@gmail.com>
2021-12-19 10:40:04 +0200

    Revert "done"
    This reverts commit f86214aaa40336df3b7801d73e62d2b85dd153f4.

--- Impact/nefesh/src/App.js
+++ Impact/nefesh/src/App.js
@@ -1,12 +1,9 @@
+
 import './App.css';
-import React, { useState } from 'react';
-import ProfilePage from './components/ProfilePage/ProfilePage';
 
 function App() {
-
   return (
     <div className="App">
-      <ProfilePage />
     </div>
   );
 }
