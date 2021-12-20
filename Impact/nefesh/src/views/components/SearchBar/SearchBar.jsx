e47f6e96105336335ae11794cb226e27fdb5e0aa
robertUfberg <robertufberg@gmail.com>
2021-12-20 13:32:02 +0200

    ghjv

--- Impact/nefesh/src/views/components/SearchBar/SearchBar.jsx
+++ Impact/nefesh/src/views/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,40 @@
 import './SearchBar.css';
 import { useState } from 'react';
+import { getDatabase, ref, onValue, collection, query, where, getDocs } from "firebase/database";
+
 
 const tags = ['newest','popular', 'recent'];
+
 
 
 
 function SearchBar() {
    var searchOption;
    const [articles, setArticles]= useState("")
+   var arr=[];
 
   
-  function getTarget(ev) {
+  async function getTarget(ev) {
+    if (ev.key === 'Enter') {
     ev.preventDefault();
     searchOption= ev.target.value;
     console.log(searchOption);
+    const q = query(collection(db, "cities"), where("capital", "==", true));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+    console.log(arr)
+
+    }
+
+    //insert filters
+    //find articles with word in it, sort by which article has the word appear the most
+    //for each article 
+    // if newest --> check by date
+    // if recent --> check by creation date
+    // if popular --> check by most views
 
 
 
@@ -21,14 +42,14 @@ function SearchBar() {
   }
   return (
     <div>
-      <input type="text" name= "searchBar" list="data" onChange={getTarget} />
+      <input type="text" name= "filterData" list="data" onKeyPress={getTarget} />
 
-      <datalist id="data">
+      {/* <datalist id="data">
         {tags.map((item, key) =>
           <option key={key} value={item} />
         )}
       </datalist>
-          <div name= "articles">{}</div>
+          <div name= "articles"></div> */}
 
     </div>
   )
