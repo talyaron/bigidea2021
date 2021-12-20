e140f1e70569fa50863606aff37b4e0806e1096b
robertUfberg <robertufberg@gmail.com>
2021-12-20 13:02:11 +0200

    ghj

--- Impact/nefesh/src/views/components/SearchBar/SearchBar.jsx
+++ Impact/nefesh/src/views/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,8 @@
 import './SearchBar.css';
-import { useState, useEffect} from 'react';
-import { db } from '../../../functions/firebase/config';
-import { collection, query, where, orderBy, onSnapshot} from 'firebase/firestore';
+import { useState } from 'react';
 
 const tags = ['newest','popular', 'recent'];
-const tempArticles= ["NEWEST", "POPULAR", 'RECENT'];
+
 
 
 function SearchBar() {
@@ -16,32 +14,21 @@ function SearchBar() {
     ev.preventDefault();
     searchOption= ev.target.value;
     console.log(searchOption);
-    if(searchOption=== 'newest'){
-     setArticles(tempArticles[0]);
-      
 
-    }
-    else if(searchOption=== 'popular'){
-      setArticles(tempArticles[1])
-      
-    }
-    else if(searchOption=== 'recent'){
-      setArticles(tempArticles[2])
 
-    }
 
   
   }
   return (
     <div>
-      <input type="text" id= "searchBar" name= "searchBar" list="data" onChange={getTarget} />
+      <input type="text" name= "searchBar" list="data" onChange={getTarget} />
 
       <datalist id="data">
         {tags.map((item, key) =>
           <option key={key} value={item} />
         )}
       </datalist>
-          <div name= "articles">{articles}</div>
+          <div name= "articles">{}</div>
 
     </div>
   )
