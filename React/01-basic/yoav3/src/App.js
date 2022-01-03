30eca43e4533813d6b27de30932bcd26cc3b26b3
ykonsta <yoav.konsta@gmail.com>
2022-01-03 14:04:56 -0800

    wf

--- React/01-basic/yoav3/src/App.js
+++ React/01-basic/yoav3/src/App.js
@@ -9,18 +9,30 @@ import westHamLogo from './logos/113.png';
 
 //components
 
-
+let variable =0
 
 function App() {
-  let variable =0
+  
   const [display, setDisplay] = useState();
-  const [logo, setLogo] = useState();
+  const [logo1, setLogo1] = useState(manCityLogo);
+  const [logo2, setLogo2] = useState(chelseaLogo); 
+  const [gameTime, setGameTime] = useState("04:30");
+  const [option1, setOption1] = useState("Manchester City");
+  const [option2, setOption2] = useState("Draw");
+  const [option3, setOption3] = useState("Chelsea");
+  const [rate1, setRate1] = React.useState(false);
+  const [rate2, setRate2] = React.useState(false);
+  const [rate3, setRate3] = React.useState(false);
+  const [tempRate1, setTempRate1] = useState("");
+  const [tempRate2, setTempRate2] = useState("");
+  const [tempRate3, setTempRate3] = useState("");
   const [game1Display, setGame1Display] = useState();
   const [game2Display, setGame2Display] = useState();
   const [game3Display, setGame3Display] = useState();
   const [baseGameDisplay, setBaseGameDisplay] = useState(game1Display);
-  const [team1,setTeam1]=useState("");
-  const [team2,setTeam2]=useState("")
+  const [team1,setTeam1]=useState("Man City");
+  const [team2,setTeam2]=useState("Chelsea");
+  
 
   //Array of Teams with Data
   let arrayTeams = [];
@@ -29,6 +41,7 @@ function App() {
   
   let manCity = {
     name: "Manchester City",
+    shortName: "Man City",
     id: 110,
     img: {manCityLogo},
     gameID: 1
@@ -41,24 +54,28 @@ function App() {
   }
   let manUtd = {
     name: "Manchester United",
+    shortName: "Man United",
     id: 105,
     img: {manUtdLogo},
     gameID: 2
   }
   let wolves = {
     name: "Wolverhampton Wanderers",
+    shortName: "Wolves",
     id: 15,
     img: {wolvesLogo},
     gameID: 2
   }
   let westHam = {
     name: "West Ham United",
+    shortName: "West Ham",
     id: 113,
     img: {westHamLogo},
     gameID: 3
   }
   let norwich = {
     name: "Norwich City",
+    shortName: "Norwich",
     id: 16,
     img: {norwichLogo},
     gameID: 3
@@ -105,7 +122,7 @@ function App() {
       option1: {
         num: 1,
         name: "Manchester United", 
-        rate: "-120",
+        rate: "-150",
         fanConsensus: "54%"
       },
       option2: {
@@ -117,7 +134,7 @@ function App() {
       option3: {
         num: 3,
         name: "Wolves",
-        rate: "+120",
+        rate: "+410",
         fanConsensus: "35%"
       }
     }
@@ -134,19 +151,19 @@ function App() {
       option1: {
         num: 1,
         name: "West Ham", 
-        rate: "-120",
+        rate: "-100",
         fanConsensus: "54%"
       },
       option2: {
         num: 2,
         name: "Draw",
-        rate: "+195",
+        rate: "+105",
         fanConsensus: "11%"
       },
       option3: {
         num: 3,
         name: "Norwich",
-        rate: "+120",
+        rate: "+210",
         fanConsensus: "35%"
       }
     }
@@ -155,17 +172,111 @@ function App() {
 
 
 
-  function handleClick(){
+  function handleClickNext(){
     console.log("arrived");
     variable+=1
     if (variable%3==0){
-      setTeam1("manUtd")
+      setTeam1("Man City");
+      setTeam2("Chelsea");
+      setLogo1(manCityLogo);
+      setLogo2(chelseaLogo);
+      setGameTime(game1.gameTime);
+      setOption1(game1.options.option1.name);
+      setOption2(game1.options.option2.name);
+      setOption3(game1.options.option3.name);
+      setTempRate1(game1.options.option1.rate);
+      setTempRate2(game1.options.option2.rate);
+      setTempRate3(game1.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
     }
     else if (variable%3==1){
-      setTeam1("man City")
+      setTeam1("Man United")
+      setTeam2("Wolves")
+      setLogo1(manUtdLogo);
+      setLogo2(wolvesLogo);
+      setGameTime(game2.gameTime);
+      setOption1(game2.options.option1.name);
+      setOption2(game2.options.option2.name);
+      setOption3(game2.options.option3.name);
+      setTempRate1(game2.options.option1.rate);
+      setTempRate2(game2.options.option2.rate);
+      setTempRate3(game2.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
     }
     else{
-      setTeam1("wolves")
+      setTeam1("West Ham")
+      setTeam2("Norwich")
+      setLogo1(westHamLogo);
+      setLogo2(norwichLogo);
+      setGameTime(game3.gameTime);
+      setOption1(game3.options.option1.name);
+      setOption2(game3.options.option2.name);
+      setOption3(game3.options.option3.name);
+      setTempRate1(game3.options.option1.rate);
+      setTempRate2(game3.options.option2.rate);
+      setTempRate3(game3.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
+    }
+    
+    
+  }
+
+  function handleClickPrevious(){
+    console.log("arrived");
+    variable-=1
+    if (variable%3==0){
+      setTeam1("Manchester City");
+      setTeam2("Chelsea");
+      setLogo1(manCityLogo);
+      setLogo2(chelseaLogo);
+      setGameTime(game1.gameTime);
+      setOption1(game1.options.option1.name);
+      setOption2(game1.options.option2.name);
+      setOption3(game1.options.option3.name);
+      setTempRate1(game1.options.option1.rate);
+      setTempRate2(game1.options.option2.rate);
+      setTempRate3(game1.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
+    }
+    else if (variable%3==1){
+      setTeam1("Manchester United")
+      setTeam2("Wolves")
+      setLogo1(manUtdLogo);
+      setLogo2(wolvesLogo);
+      setGameTime(game2.gameTime);
+      setOption1(game2.options.option1.name);
+      setOption2(game2.options.option2.name);
+      setOption3(game2.options.option3.name);
+      setTempRate1(game2.options.option1.rate);
+      setTempRate2(game2.options.option2.rate);
+      setTempRate3(game2.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
+    }
+    else{
+      setTeam1("West Ham")
+      setTeam2("Norwich")
+      setLogo1(westHamLogo);
+      setLogo2(norwichLogo);
+      setGameTime(game3.gameTime);
+      setOption1(game3.options.option1.name);
+      setOption2(game3.options.option2.name);
+      setOption3(game3.options.option3.name);
+      setTempRate1(game3.options.option1.rate);
+      setTempRate2(game3.options.option2.rate);
+      setTempRate3(game3.options.option3.rate);
+      setRate1(false);
+      setRate2(false);
+      setRate3(false);
     }
     
     
@@ -173,11 +284,20 @@ function App() {
 
   function handleOptions(){
     console.log("working");
+    setRate1(true);
+    setRate2(true);
+    setRate3(true);
+    if (tempRate1 === "") {
+      setRate1(game1.options.option1.rate);
+      setRate2(game1.options.option2.rate);
+      setRate3(game1.options.option3.rate);
+    }
+    else {
+      setRate1(tempRate1);
+      setRate2(tempRate2);
+      setRate3(tempRate3);
+    }
     
-  }
-
-  function handleBet(){
-    console.log("Bet");
   }
 
 
@@ -190,12 +310,30 @@ function App() {
     <div>
       <div className="box"> Premier League
         <div className="line"></div>
-        
+
+        <div className = "teamLeft">{team2}</div>
+        <div className = "teamRight">{team1}</div>
+        <img className="logoRight" src={logo2}></img>
+        <img className="logoLeft" src={logo1}></img>
+        <div className="time">{gameTime}</div>
         
         <div className="winner">Who Will Win?</div>
-        <div className="next" onClick = {handleClick}>Next</div>
-        <div className="previous">Previous</div>    
-        <div>{team1}</div>
+
+        <div className="option1" onClick = {handleOptions}>{option1}</div>
+        <div className="option2" onClick = {handleOptions}>{option2}</div>
+        <div className="option3" onClick = {handleOptions}>{option3}</div>
+        
+        
+        <div className="rate1">{rate1}</div>
+        <div className="rate2">{rate2}</div>
+        <div className="rate3">{rate3}</div>
+        <a href = "https://www.365scores.com" target = "_blank">Bet Now</a>
+        
+        
+
+        <div className="next" onClick = {handleClickNext}>Next</div>
+        <div className="previous" onClick = {handleClickPrevious}>Previous</div>    
+        
         
         {/* <button onClick={() => setGame2Display((l) => !l)}>Next</button>
 
---
+++ React/01-basic/yoav3/src/views/BetNow/BetNow.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const BetNow = () => {
+    return <div>
+      <h1>Here is where you'd be able to bet</h1>
+
+        <ul> 
+            <li><Link to = '/R1'>Red</Link></li>
+        </ul>
+      
+      </div>;
+  };
+  
+  export default BetNow;
