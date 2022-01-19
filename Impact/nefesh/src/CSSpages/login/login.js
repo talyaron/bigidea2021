02df10b042bac4826257e1601ec1c5cec9986900
SzymontheDestroyer <shadowbrzezinksi@gmail.com>
2022-01-19 03:02:31 -0800

    Create login.js

---
+++ Impact/nefesh/src/CSSpages/login/login.js
@@ -0,0 +1,23 @@
+import './login.css';
+
+function Login() {
+  return (
+    <div className="login">
+      <header className='header'>Login</header>
+       
+      <form className='form' onSubmit='handleSubmit(event)'>
+        <label className='label' for="text">Username</label>
+        <input className='input' type="text" name="text" id="text" />
+        <label className='label' for="text">Password</label>
+        <input className='input' type="text" name="text" id="text" />
+        <br />
+        <button classname= 'button'>Submit</button>
+        <br />
+        <button classname= 'button'>Sign in with Google</button>
+      </form>
+       
+    </div>
+  );
+}
+
+export default Login;
