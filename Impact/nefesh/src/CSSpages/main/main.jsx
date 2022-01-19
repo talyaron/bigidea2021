d39db00382a446c70ca6b8268cade629c8d4a52e
SzymontheDestroyer <shadowbrzezinksi@gmail.com>
2022-01-19 03:06:39 -0800

    Create main.jsx

---
+++ Impact/nefesh/src/CSSpages/main/main.jsx
@@ -0,0 +1,22 @@
+import "./main.css"
+// import userIcon from '@mui/icons-material/AccountCircle';
+
+function Page(){
+    return(
+        <div>
+            <div className="userIcon"></div>
+            <div className="topBar">
+                <button className="selectedbutton">all</button>
+                <button className="unselectedbutton">preferance 1</button>
+                <button className="unselectedbutton">preferance 2</button>
+                <button className="unselectedbutton">preferance 3</button>
+            </div>
+            <div className="middle">
+                <button className="article">add article</button>
+            </div>
+            <button type="submit" className="contact">Contact Us</button>
+        </div>
+    )
+}
+
+export default Page
