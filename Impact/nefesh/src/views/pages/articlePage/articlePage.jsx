6dcac7328cf7ae53bea2fd55b67d404575eadac8
ykonsta <yoav.konsta@gmail.com>
2022-01-20 03:47:29 -0800

    jnjn

--- Impact/nefesh/src/views/pages/articlePage/articlePage.jsx
+++ Impact/nefesh/src/views/pages/articlePage/articlePage.jsx
@@ -3,11 +3,23 @@ import {  useParams } from 'react-router-dom';
 import { useEffect, useState } from "react"
 import { db } from '../../../functions/firebase/config';
 import { collection, query, where, orderBy, onSnapshot } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
+
 
 function ArticlePage(){
     const {id} = useParams();
+    let eventRef = doc(db, 'events', id);
+    console.log(eventRef);
     console.log(id + "hello?");
     //console.log("can print here");
+    useEffect(() => {
+        const unsubscribe = onSnapshot(eventRef, (eventDB) => {
+          console.log(eventDB.data())
+        });
+        return () => {
+          unsubscribe();
+        }
+      }, [])
 
 
 
