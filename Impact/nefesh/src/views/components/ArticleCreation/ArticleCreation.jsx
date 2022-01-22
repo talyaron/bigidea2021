14e08b35cc5647c0e9f4e0302e3baa9981a5a8d7
ElliotKupchik <elliotkupchik@gmail.com>
2022-01-22 18:55:58 +0200

    Update ArticleCreation.jsx

--- Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
+++ Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
@@ -7,7 +7,7 @@ let statesSumbitted = { views: 0, startTime: "", endTime: "" }
 function ArticleCreation(props) {
     const [tagsState, setTagsState] = useState([])
     function submitArticle() {
-        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity,phone, website, email} = statesSumbitted;
+        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity, phone, website, email } = statesSumbitted;
         addDoc(collection(db, "events"), {
             title,
             date,
@@ -19,12 +19,12 @@ function ArticleCreation(props) {
                 houseNumber: houseNumber,
                 city: city
             },
-            contactInfo:{
+            contactInfo: {
                 phone,
                 email,
                 website
             },
-            tags:tagsState,
+            tags: tagsState,
             creatorUID: props.userID,
             creatorOrg: props.userOrg,
             views,
@@ -37,7 +37,7 @@ function ArticleCreation(props) {
         })
     }
     function saveDraft() {
-        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity, phone, website, email} = statesSumbitted;
+        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity, phone, website, email } = statesSumbitted;
 
         addDoc(collection(db, "events"), {
             title,
@@ -50,12 +50,12 @@ function ArticleCreation(props) {
                 houseNumber: houseNumber,
                 city: city
             },
-            contactInfo:{
+            contactInfo: {
                 phone,
                 email,
                 website
             },
-            tags:tagsState,
+            tags: tagsState,
             creatorUID: props.userID,
             creatorOrg: props.userOrg,
             views,
@@ -80,52 +80,41 @@ function ArticleCreation(props) {
         setTagsState([...tagsState, { id: i, tag: ev.target[0].value }])
         i++
     }
-    function deleteTag(ev){
+    function deleteTag(ev) {
         // debugger
         ev.preventDefault()
-        let tempArray=tagsState
+        let tempArray = tagsState
         console.log(ev.currentTarget[0].id)
-        tempArray.splice(ev.currentTarget[0].id,1)
+        tempArray.splice(ev.currentTarget[0].id, 1)
         setTagsState(tempArray)
     }
     return <div>
         <div className='popup-box'>
             <b>Input information here</b>
-            <br />
 
             <input type="text" name="title" onKeyUp={changeState} placeholder="Enter article title here" />
-            <br />
 
             <input type="text" name="name" onKeyUp={changeState} placeholder="Enter host/s name here" />
-            <br />
 
             <input type="text" name="text" onKeyUp={changeState} placeholder="Enter text here" />
-            <br />
 
             <input type="text" name="image" onKeyUp={changeState} placeholder="Enter cover image url here" />
-            <br />
 
             <input type="date" name="date" onChange={changeState} placeholder="Enter date here" />
-            <br />
 
             <input type="text" name="streetName" onChange={changeState} placeholder="Enter street name here" />
-            <br />
 
             <input type="text" name="houseNumber" onChange={changeState} placeholder="Enter building number here" />
-            <br />
 
             <input type="text" name="city" onChange={changeState} placeholder="Enter city here" />
-            <br />
 
 
             <div>Start Time</div>
             <input type="time" name="startTime" onChange={changeState} placeholder="Enter address line 1 here" />
             <div>End Time</div>
             <input type="time" name="endTime" onChange={changeState} placeholder="Enter address line 1 here" />
-            <br />
 
             <input type="number" name="maxCapacity" onChange={changeState} placeholder="Enter maximum capacity here" />
-            <br />
 
             <form onSubmit={addTags}>
                 <input type="text" name="tagsInput" placeholder="Enter tag here" />
@@ -143,12 +132,9 @@ function ArticleCreation(props) {
                 })
                 }
             </div>
-            <br />
 
             <input type="text" name="phone" onChange={changeState} placeholder="Enter phone number here" />
-            <br />
             <input type="text" name="email" onChange={changeState} placeholder="Enter your contact email here" />
-            <br />
             <input type="text" name="website" onChange={changeState} placeholder="Enter your website url here" />
             <button onClick={saveDraft}>Save Draft</button>
             <button onClick={submitArticle}>Submit Article</button>
