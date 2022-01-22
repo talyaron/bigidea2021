b7b25a11412ec3c1e7da0adc73c3e671dec67872
Levid0 <89023126+Levid0@users.noreply.github.com>
2022-01-22 18:04:43 +0200

    gte

--- Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
+++ Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './ArticleCreation.css';
-import { collection, addDoc } from "firebase/firestore"
+import { collection, addDoc, arrayRemove } from "firebase/firestore"
 import { db } from "../../../functions/firebase/config"
-let i=0
+let i = 0
 let statesSumbitted = { views: 0, startTime: "", endTime: "" }
 function ArticleCreation(props) {
     const [tagsState, setTagsState] = useState([])
@@ -31,7 +31,7 @@ function ArticleCreation(props) {
         })
     }
     function saveDraft() {
-        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity } = statesSumbitted;
+        const { title, name, date, text, image, views, streetName, houseNumber, city, startTime, endTime, maxCapacity, phone, website, email} = statesSumbitted;
 
         addDoc(collection(db, "events"), {
             title,
@@ -43,6 +43,11 @@ function ArticleCreation(props) {
                 streetName: streetName,
                 houseNumber: houseNumber,
                 city: city
+            },
+            contactInfo:{
+                phone,
+                email,
+                website
             },
             creatorUID: props.userID,
             creatorOrg: props.userOrg,
@@ -65,8 +70,16 @@ function ArticleCreation(props) {
     }
     function addTags(ev) {
         ev.preventDefault()
-        setTagsState([...tagsState,{id:i,tag:ev.target[0].value}])
+        setTagsState([...tagsState, { id: i, tag: ev.target[0].value }])
         i++
+    }
+    function deleteTag(ev){
+        // debugger
+        ev.preventDefault()
+        let tempArray=tagsState
+        console.log(ev.currentTarget[0].id)
+        tempArray.splice(ev.currentTarget[0].id,1)
+        setTagsState(tempArray)
     }
     return <div>
         <div className='popup-box'>
@@ -103,14 +116,20 @@ function ArticleCreation(props) {
             <div className="tagsMapContainer">
                 {tagsState.map(tag => {
                     return (
-                        <form>
-                        <div key={tag.id}className='nametag'>{tag.tag}
-                        </div>
+                        <form onSubmit={deleteTag} key={tag.id}>
+                            <div className='nametag'>{tag.tag}</div>
+                            <button type="submit" id={tag.id}>X</button>
                         </form>
                     )
                 })
                 }
             </div>
+            <br />
+            <input type="text" name="phone" onChange={changeState} placeholder="Enter phone number here" />
+            <br />
+            <input type="text" name="email" onChange={changeState} placeholder="Enter your contact email here" />
+            <br />
+            <input type="text" name="website" onChange={changeState} placeholder="Enter your website url here" />
             <button onClick={saveDraft}>Save Draft</button>
             <button onClick={ping}>Hi</button>
             <button onClick={submitArticle}>Submit Article</button>
