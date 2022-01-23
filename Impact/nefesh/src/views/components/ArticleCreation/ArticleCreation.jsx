ce1776fda711c7d11c8e13c01a77e544b181522e
Tal Yaron <tal.yaron@gmail.com>
2022-01-23 12:43:08 +0200

    sone fix

--- Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
+++ Impact/nefesh/src/views/components/ArticleCreation/ArticleCreation.jsx
@@ -79,50 +79,50 @@ function ArticleCreation(props) {
         i++
         console.log(ev.target[0].value)
     }
-    function deleteTag(tag){
+    function deleteTag(tag) {
         // debugger
         tag.preventDefault()
-        
-        let tempArray=[...tagsState]
-        tempArray.splice(tag,1)
+
+        let tempArray = [...tagsState]
+        tempArray.splice(tag, 1)
         setTagsState(tempArray)
     }
     return <div>
-        <div className='popup-box'>
+        <div className='createArticle-popup-box'>
 
             <b className='infoTitle'>Input information here</b>
-            <input type="text" name="title" onKeyUp={changeState} placeholder="Enter article title here" className='textBasic inputTitle'/>
+            <input type="text" name="title" onKeyUp={changeState} placeholder="Enter article title here"  />
 
-            <input type="text" name="name" onKeyUp={changeState} placeholder="Enter host/s name here" className='textBasic hostName'/>
+            <input type="text" name="name" onKeyUp={changeState} placeholder="Enter host/s name here"  />
 
-            <input type="text" name="image" onKeyUp={changeState} placeholder="Enter cover image url here" className='textBasic imgUrl'/>
+            <input type="text" name="image" onKeyUp={changeState} placeholder="Enter cover image url here"  />
 
-            <input type="text" name="streetName" onChange={changeState} placeholder="Enter street name here" className='textBasic street'/>
-            <input type="text" name="city" onChange={changeState} placeholder="Enter city here" className='textBasic city'/>
+            <input type="text" name="streetName" onChange={changeState} placeholder="Enter street name here" />
+            <input type="text" name="city" onChange={changeState} placeholder="Enter city here"  />
 
-            <input type="text" name="houseNumber" onChange={changeState} placeholder="Enter building number here" className='textBasic building'/>
-            <input type="number" name="maxCapacity" onChange={changeState} placeholder="Enter maximum capacity here" className='textBasic maxCap'/>
+            <input type="text" name="houseNumber" onChange={changeState} placeholder="Enter building number here" />
+            <input type="number" name="maxCapacity" onChange={changeState} placeholder="Enter maximum capacity here"  />
 
-            <input type="text" name="phone" onChange={changeState} placeholder="Enter phone number here" className='textBasic phoneNum'/>
-            <input type="text" name="email" onChange={changeState} placeholder="Enter your contact email here" className='textBasic email'/>
+            <input type="text" name="phone" onChange={changeState} placeholder="Enter phone number here"  />
+            <input type="text" name="email" onChange={changeState} placeholder="Enter your contact email here"  />
 
-            <input type="text" name="website" onChange={changeState} placeholder="Enter your website url here" className='textBasic webUrl'/>
+            <input type="text" name="website" onChange={changeState} placeholder="Enter your website url here" className='textBasic webUrl' />
 
-            <input type="date" name="date" onChange={changeState} placeholder="Enter date here" className='textBasic date'/>           
-            <div className='timeData'>
-                <div>Start Time</div>
-                <input type="time" name="startTime" onChange={changeState} placeholder="Enter address line 1 here" className='textBasic'/>
-                <div>End Time</div>
-                <input type="time" name="endTime" onChange={changeState} placeholder="Enter address line 1 here" className='textBasic'/>
-            </div>
+            <input type="date" name="date" onChange={changeState} placeholder="Enter date here" className='textBasic date' />
 
-            <input type="text" name="text" onKeyUp={changeState} placeholder="Enter text here" className='textBasic description'/>
+            <div>Start Time</div>
+            <input type="time" name="startTime" onChange={changeState} placeholder="Enter address line 1 here" className='textBasic' />
+            <div>End Time</div>
+            <input type="time" name="endTime" onChange={changeState} placeholder="Enter address line 1 here" className='textBasic' />
+
+
+            <input type="text" name="text" onKeyUp={changeState} placeholder="Enter text here" className='textBasic description' />
 
             <form className='Tags' onSubmit={addTags}>
-                <input type="text" name="tagsInput" placeholder="Enter tag here" className='textBasic'/>
+                <input type="text" name="tagsInput" placeholder="Enter tag here" className='textBasic' />
                 <button className='submitButton' type="submit">Submit</button>
             </form>
-            
+
 
             <div className="tagsMapContainer">
                 {tagsState.map(tag => {
