a9c6b5390620e1e43314efecb26424c956cfb79c
Max Shroyer <Maxshroyer49@gmail.com>
2022-01-24 14:54:12 +0200

    ugg

--- Impact/nefesh/src/views/pages/MainPage/MainPage.jsx
+++ Impact/nefesh/src/views/pages/MainPage/MainPage.jsx
@@ -78,9 +78,5 @@ function App() {
         </div>
     )
 }
-<<<<<<< HEAD
-export default App
-=======
 
 export default App;
->>>>>>> parent of 7468d03... Merge branch 'yoavSpizpreferences' into Dev
