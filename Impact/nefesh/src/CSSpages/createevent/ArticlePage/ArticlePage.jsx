ea8436ce2a20a0e1396560de8891837d3bc8ea66
ElliotKupchik <elliotkupchik@gmail.com>
2022-01-24 12:46:43 +0200

    dgf

--- Impact/nefesh/src/CSSpages/createevent/ArticlePage/ArticlePage.jsx
+++ Impact/nefesh/src/CSSpages/createevent/ArticlePage/ArticlePage.jsx
@@ -4,26 +4,38 @@ function Article(){
         <div className="mainContainer">
             <div className="articleImage">
                 <button className="backButton"> &#60; Back </button>
+                <button className="addEventButton"> Add Event </button>
             </div>
 
             <div className="eventData">
                 <h1 className="eventName"> Event Name </h1>
                 <div className="secondaryInfo">
-                    <p className="eventDate"> DD/MM/YYYY </p>
-                    <p className="eventLocation"> location </p>
+                    <div className="eventTimeContainer">
+                        <p className="eventDate"> Event Date: DD/MM/YYYY </p>
+                        <p className="eventStartTime"> Start Time: 11:00AM </p>
+                        <p className="eventEndTime"> End Time: 4:00PM </p>
+                    </div>
+                    <div className="eventLocationContainer">
+                        <p className="eventStreet"> Street Info </p>
+                        <p className="eventHouse"> House Info </p>
+                        <p className="eventCity"> City Info </p>
+                    </div>
                 </div>
                 <p className="eventDescription"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3 className="username"> Signed / Joe </h3>
             </div>
 
             <div className="userPromptContainer">
-                <button className="signUpButton black"> Sign Up </button>
-                <h3 className="username"> Signed / General Kanobi </h3>
-                <button className="shareButton black"> Share </button>
+                <button className="signUpButton"> Sign Up </button>
+                <div className="capacityValues">
+                    <h3 className="currentCap"> Current Capacity: 43 </h3>
+                    <h3 className="maxCap"> Max Capacity: 120 </h3>
+                </div>
+                <button className="shareButton"> Share </button>
             </div>
 
             <div className="userNavigation">
-                <button className="black"> Home </button>
-                <button className="black"> Share </button>
+                <div>This is a placeholder</div>
             </div>
         </div>
                
