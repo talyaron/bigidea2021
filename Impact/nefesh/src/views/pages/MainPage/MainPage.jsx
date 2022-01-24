ebc5a765a6ce1edb7b8ffa678250ea73e423d0e4
Max Shroyer <Maxshroyer49@gmail.com>
2022-01-24 11:36:18 +0200

    Revert "rats"
    This reverts commit a107e93cb38b2e8f624e5616e474b81601148215.

--- Impact/nefesh/src/views/pages/MainPage/MainPage.jsx
+++ Impact/nefesh/src/views/pages/MainPage/MainPage.jsx
@@ -1,199 +1,190 @@
-import './MainPage.css';
-import { useEffect, useState } from 'react';
+import "./MainPage.css";
+import { useEffect, useState } from "react"
 import { db } from '../../../functions/firebase/config';
-import { getDatabase, ref, onValue, query } from 'firebase/database';
-import {
-	collection,
-	orderBy,
-	onSnapshot,
-	getDocs,
-	where,
-	getFirestore,
-} from 'firebase/firestore';
+import { getDatabase, ref, onValue, query } from "firebase/database";
+import { collection,  orderBy, onSnapshot, getDocs, where, getFirestore} from 'firebase/firestore';
 
-const tags = ['newest', 'popular', 'recent'];
-let eventFilter = '';
-let eventListTemp = [];
+const tags = ['newest','popular', 'recent'];
+let eventFilter = ""
+let eventListTemp = []
 
 function App() {
-	var searchOption;
-	var filterOption;
-	const db = getFirestore();
-	const [articles, setArticles] = useState([]);
-	const [hidden, setHidden] = useState(true);
-	const [tag, setTag] = useState('');
 
-	async function getFilter(ev) {
-		ev.preventDefault();
-		const arr = [];
-		setHidden(false);
-		filterOption = ev.target.value;
-		setTag(filterOption);
-		console.log(ev);
-		console.log(filterOption);
-	}
-	async function getTarget(ev) {
-		let arr2 = [];
-		if (ev.key === 'Enter') {
-			ev.preventDefault();
-			searchOption = ev.target.value;
-			console.log(searchOption);
-			const q = query(
-				collection(db, 'events'),
-				where('Title', '>=', searchOption)
-			);
-			const querySnapshot = await getDocs(q);
-			querySnapshot.forEach((doc) => {
-				// doc.data() is never undefined for query doc snapshots
-				console.log(doc.id, ' => ', doc.data());
-				arr2.push(doc.data());
-			});
-			if (tag === 'popular') {
-				for (var i = 1; i < arr2.length; i++)
-					for (var j = 0; j < i; j++)
-						if (arr2[i].views > arr2[j].views) {
-							var x = arr2[i];
-							arr2[i] = arr2[j];
-							arr2[j] = x;
-						}
-			} else if (tag === 'newest') {
-				for (var i = 1; i < arr2.length; i++)
-					for (var j = 0; j < i; j++)
-						if (arr2[i].Date > arr2[j].Date) {
-							var x = arr2[i];
-							arr2[i] = arr2[j];
-							arr2[j] = x;
-						}
-			} else if (tag === 'recent') {
-				for (var i = 1; i < arr2.length; i++)
-					for (var j = 0; j < i; j++)
-						if (arr2[i].dateAdded > arr2[j].dateAdded) {
-							var x = arr2[i];
-							arr2[i] = arr2[j];
-							arr2[j] = x;
-						}
-			}
-			setArticles(arr2);
-			console.log(arr2);
 
-			//insert filters
-			//find articles with word in it, sort by which article has the word appear the most
-			//for each article
-			// if newest --> check by date
-			// if recent --> check by creation date
-			// if popular --> check by most views
-		}
-	}
-	const [eventListState, setEventListState] = useState([]);
 
-	// const [eventFilterState,setEventFilterState]=useState("")
-	useEffect(() => {
-		const q = query(collection(db, 'events'));
-		const unsub = onSnapshot(q, (querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				const eventTemp = doc.data();
-				eventTemp.id = doc.id;
-				eventListTemp.push(eventTemp);
-			});
-			console.log('ping!');
-			setEventListState(eventListTemp);
-		});
-	}, []);
-	function test() {
-		console.log(eventListTemp);
-	}
-	function changeEventFilter(ev) {
-		// console.log('ping! changeeventfilter')
-		eventListTemp = [...eventListState];
-		eventFilter = ev.target.value;
-		if (eventFilter === 'popular') {
-			eventListTemp.sort(function (a, b) {
-				return b.views - a.views;
-			});
-			setEventListState(eventListTemp);
-			console.log(eventListState);
-		}
-		if (eventFilter === 'newest') {
-			eventListTemp.forEach((doc) => {
-				doc.dateCompare = doc.Date.replace(/\D/g, '');
-			});
-			eventListTemp.sort(function (a, b) {
-				return b.dateAdded.seconds - a.dateAdded.seconds;
-			});
-			setEventListState(eventListTemp);
-			console.log(eventListTemp);
-			console.log(eventListState);
-		}
-	}
-	return (
-		<div>
-			Hi
-			<button onClick={test}>Hi</button>
-			<div className='userInterfaceContainer'>
-				<form className='filterEvents'>
-					<label htmlFor='eventFilterType'>
-						Sort out the events displayed:
-					</label>
-					<select
-						name='eventFilterType'
-						id='eventFilterType'
-						onChange={changeEventFilter}>
-						<option value='newest'>Newest to Oldest</option>
-						<option value='popular'>Most Popular</option>
-						<option value='recent'>Most Recent</option>
-					</select>
-				</form>
-				<div className='eventMapContainer'>
-					{eventListState.map((event) => {
-						return (
-							<div key={event.id} className='nametag'>
-								<h1>{event.Title}</h1>
-								<div>{event.id}</div>
-								<img src={event.Image} style={{ width: '100px' }}></img>
-								<div>This event will take place on: {event.Date}</div>
-								<div>{event.views} many people have viewed this event</div>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-			<div className='container_MainPage'>
-				<div className='sectionOne'>
-					<button onChange={console.log('Hamburger')}>|||</button>
-				</div>
 
-				<input
-					className='filterData'
-					type='text'
-					name='filterData'
-					list='data'
-					onChange={getFilter}
-				/>
-				<datalist className='data' id='data'>
-					{tags.map((item, key) => (
-						<option key={key} value={item} />
-					))}
-				</datalist>
+   var searchOption;
+   var filterOption;
+   const db = getFirestore();
+   const [articles, setArticles]= useState([])
+   const [hidden, setHidden] = useState(true)
+   const [tag, setTag] = useState("")    
 
-				<input
-					className='searchBar'
-					type='text'
-					name='searchBar'
-					hidden={hidden}
-					onKeyPress={getTarget}
-				/>
-				{articles.map((article, i) => (
-					<li className='search_results_list' key={i}>
-						<span className='input-label'>
-							{' '}
-							{i + 1}. {article.Title} Written on {article.Date} by{' '}
-							{article.creator} and currently has {article.views} views
-						</span>{' '}
-						<img src={article.Image} />
-					</li>
-				))}
-			</div>
-		</div>
-	);
+  async function getFilter(ev){
+    ev.preventDefault();
+    const arr= [];
+    setHidden(false)
+    filterOption= ev.target.value;
+    setTag(filterOption)
+    console.log(ev)
+    console.log(filterOption)
+
+ 
+  }
+  async function getTarget(ev) {
+    let arr2= [];
+    if (ev.key === 'Enter') {
+    ev.preventDefault();
+    searchOption= ev.target.value;
+    console.log(searchOption);
+    const q = query(collection(db, "events"), where("Title", ">=", searchOption));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {  
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      arr2.push(doc.data());
+    });
+    if(tag=== "popular"){
+      for (var i = 1; i < arr2.length; i++)
+      for (var j = 0; j < i; j++)
+          if (arr2[i].views > arr2[j].views) {
+            var x = arr2[i];
+            arr2[i] = arr2[j];
+            arr2[j] = x;
+          }
+  
+
+  }
+  else if (tag=== "newest"){
+    for (var i = 1; i < arr2.length; i++)
+    for (var j = 0; j < i; j++)
+        if (arr2[i].Date > arr2[j].Date) {
+          var x = arr2[i];
+          arr2[i] = arr2[j];
+          arr2[j] = x;
+        }
+      }
+  else if(tag=== "recent"){
+    for (var i = 1; i < arr2.length; i++)
+    for (var j = 0; j < i; j++)
+        if (arr2[i].dateAdded > arr2[j].dateAdded) {
+          var x = arr2[i];
+          arr2[i] = arr2[j];
+          arr2[j] = x;
+        }
+  };
+    setArticles(arr2);
+    console.log(arr2)
+
+    
+
+    //insert filters
+    //find articles with word in it, sort by which article has the word appear the most
+    //for each article 
+    // if newest --> check by date
+    // if recent --> check by creation date
+    // if popular --> check by most views
+
+
+
+  
+  }
+
 }
-export default App;
+    const [eventListState, setEventListState] = useState([])
+
+    // const [eventFilterState,setEventFilterState]=useState("")
+    useEffect(() => {
+        const q = query(collection(db, "events"))
+        const unsub = onSnapshot(q, (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const eventTemp = doc.data();
+                eventTemp.id = doc.id;
+                eventListTemp.push(eventTemp)
+            })
+            console.log('ping!')
+            setEventListState(eventListTemp)
+        })
+
+
+    }, [])
+    function test() {
+        console.log(eventListTemp)
+    }
+    function changeEventFilter(ev) {
+        // console.log('ping! changeeventfilter')
+        eventListTemp = [...eventListState];
+        eventFilter = ev.target.value
+        if (eventFilter === "popular") {
+            eventListTemp.sort(function (a, b) { return b.views - a.views })
+            setEventListState(eventListTemp)
+            console.log(eventListState)
+        }
+        if (eventFilter === "newest") {
+            eventListTemp.forEach((doc) => {
+                doc.dateCompare = doc.Date.replace(/\D/g, '')
+            })
+            eventListTemp.sort(function (a, b) { return b.dateAdded.seconds - a.dateAdded.seconds })
+            setEventListState(eventListTemp)
+            console.log(eventListTemp)
+            console.log(eventListState)
+        }
+    }
+    return (
+        <div>
+            Hi
+            <button onClick={test}>Hi</button>
+            <div className="userInterfaceContainer">
+                <form className="filterEvents">
+                    <label htmlFor="eventFilterType">Sort out the events displayed:</label>
+                    <select name="eventFilterType" id="eventFilterType"
+                        onChange={changeEventFilter}
+                    >
+                        <option value="newest">Newest to Oldest</option>
+                        <option value="popular">Most Popular</option>
+                        <option value="recent">Most Recent</option>
+                    </select>
+                </form>
+                <div className="eventMapContainer">
+                    {eventListState.map(event => {
+                        return (
+                            <div key={event.id} className='nametag'>
+                                <h1>{event.Title}</h1>
+                                <div>{event.id}</div>
+                                <img src={event.Image} style={{ width: "100px" }}></img>
+                                <div>This event will take place on: {event.Date}</div>
+                                <div>{event.views} many people have viewed this event</div>
+                            </div>
+                        )
+                    })
+                    }
+                </div>
+            </div>
+       
+
+            <div className='container'>
+              <div className='contianer2'>
+              <div className="sectionOne">
+                <h1>Nefesh B'Nefesh</h1>
+                <button onChange={console.log("does nothing yet")}>|||</button>
+              </div>
+              
+              <input className='filterData' type="text" name= "filterData" list="data" onChange={getFilter} />
+                <datalist className='data' id="data">
+                {tags.map((item, key) =>
+                  <option key={key} value={item} />
+                )}
+              </datalist>
+              <input className='searchBar' type="text" name= "searchBar" hidden= {hidden} onKeyPress={getTarget} />
+              {articles.map((article, i) => (
+            <li className="travelcompany-input" key={i}>
+                <span className="input-label"> {i+1}. {article.Title} Written on {article.Date} by {article.creator} and currently has {article.views} views</span> <img src= {article.Image}/> 
+            </li>
+           ))}
+           </div>
+           <button className='Button2'>Home Page</button>
+            </div>
+            </div>
+          )
+}
+export default App
