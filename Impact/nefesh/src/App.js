8db85b1b5ec484ed188da0ad5718b415267d500c
Max Shroyer <Maxshroyer49@gmail.com>
2022-01-24 22:10:49 +0200

    push move to its own element

--- Impact/nefesh/src/App.js
+++ Impact/nefesh/src/App.js
@@ -15,35 +15,35 @@ import { useEffect, useState } from 'react';
 import { doc, setDoc, getDoc } from 'firebase/firestore';
 import { db } from './functions/firebase/config';
 import AdminPage from './views/pages/AdminPage/AdminPage';
+import StickyBanner from './views/components/StickyBanner/StickyBanner'
 
-//icons
-import HomeButton from './views/components/Images/home.svg'
-import Menu from './views/components/Images/Menu.svg'
 
-//hi
 let role = 'superAdmin';
 let permissionedRole;
 const auth = getAuth();
-let userID = ""
+let userID = '';
 let loggedIn;
 function App() {
-	const [userState, setUserState] = useState({})
-	const navigate = useNavigate();
+	const [userState, setUserState] = useState({});
+	
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
 				console.log('user logged in');
 				const uid = user.uid;
-				userID=uid
+				userID = uid;
 				//get user from db
 				getDoc(doc(db, 'users', uid)).then((userDB) => {
 					if (userDB.exists()) {
 						console.log('user exists');
-						setUserState({ userName: userDB.data().displayName, userOrg:userDB.data().organization});
+						setUserState({
+							userName: userDB.data().displayName,
+							userOrg: userDB.data().organization,
+						});
 						role = userDB.data().role;
-						const uid=userDB.data().userID;
-						userID=uid
+						const uid = userDB.data().userID;
+						userID = uid;
 					} else {
 						//if user exist in db get the user from DB and get the role
 						console.log('user does not exist');
@@ -70,47 +70,29 @@ function App() {
 		});
 	}, []);
 
+
 	return (
 		<div className='container_AppMain'>
 			{loggedIn ? (
 				<div className='container_App'>
-						<nav>
-							<Link to='/ContactUs'>Contact Us</Link>
-							<Link to='/ArticleCreation'>Article Creation</Link>
-							<Link to='/ProfilePage'>Profile Page</Link>
-							<Link to='/AdminPage'>*** Admin Page***</Link>
-						</nav>
-						<Routes>
-							<Route path='/' element={<MainPage role={role} />} />
-							<Route path='404' element={<Error />} />
-							<Route path='401' element={<Unauthorised />} />
-							<Route path='MainPage' element={<MainPage role={role} />} />
-							<Route path='ContactUs' element={<ContactUs />} />
-							<Route path='ArticleCreation' element={<ArticleCreation userID={userID} userOrg={userState.userOrg} />} />
-							<Route path='ProfilePage' element={<ProfilePage  uid={userID} />} />
-							<Route path='AdminPage' element={<AdminPage />} />
-						</Routes>
-					<div id='stickyBanner'>
-						<div className='homeB_container' onClick={()=>{
-							navigate("/MainPage")
-						}} >
-							<img src={HomeButton} alt='Home' className='homeButton'/>
-						</div>
-						<h5>Developed by: Big Idea</h5>
-						<div className='Menu_container' onClick={()=>{
-							navigate("/ProfilePage")
-						}} >
-							<img src={Menu} alt='Home' className='homeButton'/>
-						</div>
-						
-					</div>
+					<Routes>
+						<Route path='/' element={<MainPage role={role} />} />
+						<Route path='404' element={<Error />} />
+						<Route path='401' element={<Unauthorised />} />
+						<Route path='MainPage' element={<MainPage role={role} />} />
+						<Route path='ContactUs' element={<ContactUs />} />
+						<Route path='ArticleCreation' element={<ArticleCreation userID={userID} userOrg={userState.userOrg} />} />
+						<Route path='ProfilePage' element={<ProfilePage uid={userID} />} />
+						<Route path='AdminPage' element={<AdminPage />} />
+					</Routes>
+					<StickyBanner/>
 				</div>
 			) : (
 				<div className='container_App'>
-						<Routes>
-							<Route path='/' element={<Login />} />
-							<Route path='login' element={<Login />} />
-						</Routes>
+					<Routes>
+						<Route path='/' element={<Login />} />
+						<Route path='login' element={<Login />} />
+					</Routes>
 				</div>
 			)}
 		</div>
