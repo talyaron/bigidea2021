5fb36d7aaf678fd15469e84f99a67e1f8df3254f
Max Shroyer <Maxshroyer49@gmail.com>
2022-01-26 11:21:33 +0200

    added component for import Img

---
+++ Impact/nefesh/src/views/components/ImportImgs/ImportImgs.jsx
@@ -0,0 +1,13 @@
+
+import { getStorage, ref } from "firebase/storage";
+
+const storage = getStorage();
+const storageRef = ref(storage, 'EventImgs');
+
+function ImportImg(){
+
+    return(
+        <input type="file" name="articleImg" id="input_ArticleImg" accept="image/png, image/jpeg"/>
+    )
+
+}
