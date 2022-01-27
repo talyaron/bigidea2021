150aa09f4622fd19be9c6f35820987ac4ee5e90f
Max Shroyer <Maxshroyer49@gmail.com>
2022-01-27 14:16:55 +0200

    merge css for article page

--- Impact/nefesh/src/views/pages/Event/Event.jsx
+++ Impact/nefesh/src/views/pages/Event/Event.jsx
@@ -1,59 +1,116 @@
-import React, { useState, useEffect } from "react";
-import "./Event.css";
-import { collection, setDoc, getDoc, doc } from "firebase/firestore";
-import { db } from "../../../functions/firebase/config";
-import { useParams } from "react-router-dom";
-import useScript from "../../../functions/useScript";
+import React, { useState, useEffect } from 'react';
+import './Event.css';
+import { collection, setDoc, getDoc, doc } from 'firebase/firestore';
+import { db } from '../../../functions/firebase/config';
+import { useNavigate, useParams } from 'react-router-dom';
+import useScript from '../../../functions/useScript';
 
 function Event() {
-  const [eventData, setEventData] = useState([]);
-  const [tags, setTags] = useState([]);
-  let { eventID } = useParams();
+	const [eventData, setEventData] = useState([]);
+	const [tags, setTags] = useState([]);
+	const [addressInfo, setAddressInfo] = useState([]);
+	const [contactInfo, setContactInfo] = useState([]);
+	const [orgWebsite, setOrgWebsite] = useState([]);
+	const [websiteValidity, setWebValidity] = useState(false);
+	const [imageValidity, setImgValidity] = useState(false);
+	let { eventID } = useParams();
+	let navigate = useNavigate();
 
-  useEffect(async () => {
-    try {
-      let eventRef = doc(db, "events", eventID);
-      const docSnap = await getDoc(eventRef);
-      setEventData(docSnap.data());
-      setTags(docSnap.data().tags);
-    } catch (err) {
-      console.error(err);
-    }
-  }, []);
+	useEffect(async () => {
+		try {
+			setWebValidity(false);
+			let eventRef = doc(db, 'events', eventID);
+			const docSnap = await getDoc(eventRef);
+			setEventData(docSnap.data());
+			setTags(docSnap.data().tags);
+			setAddressInfo(docSnap.data().address);
+			setContactInfo(docSnap.data().contactInfo);
+			let validState = validURL(docSnap.data().contactInfo.website);
+			setWebValidity(validState);
+			setOrgWebsite(docSnap.data().contactInfo.website);
+		} catch (err) {
+			console.error(err);
+		}
+	}, []);
 
-  function ping() {
-    console.log(eventData);
-  }
-  return (
-    <div>
-      {useScript("https://cdn.addevent.com/libs/atc/1.6.1/atc.min.js")}
+	function validURL(str) {
+		var pattern = new RegExp(
+			'^(https?:\\/\\/)?' + // protocol
+				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+				'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+				'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+				'(\\#[-a-z\\d_]*)?$',
+			'i'
+		); // fragment locator
+		return !!pattern.test(str);
+	}
 
-      <div>Hi {eventID}</div>
-      <button onClick={ping}>Hi</button>
-      <div name="title">{eventData.title}</div>
-      <div name="date">{eventData.date}</div>
-      <div name="startTime">{eventData.startTime}</div>
-      <div name="endTime">{eventData.endTime}</div>
-      <img name="coverImage" src={eventData.coverImage}></img>
-      <div name="maxCapacity">{eventData.maxCapacity}</div>
-      <div name="currentCapacity">{eventData.currentCapacity}</div>
-      <div name="hostName">{eventData.hostName}</div>
-      <div name="organizationName">{eventData.creatorOrg}</div>
-      <div name="views">Views: {eventData.views}</div>
-      {tags.map((tags) => {
-        return <div key={tags.id}>{tags.tag}</div>;
-      })}
-      <div name="text">{eventData.article}</div>
-      <div title="Add to Calendar" className="addeventatc">
-        Add to Calendar
-        <span className="start">{`${eventData.date}+${eventData.startTime}`}</span>
-        <span className="end">{`${eventData.date}+${eventData.endTime}`}</span>
-        <span className="timezone">Asia/Jerusalem</span>
-        <span className="title">{eventData.title}</span>
-        <span className="description">{eventData.article}</span>
-      </div>
-    </div>
-  );
+	function ping() {
+		console.log(eventData);
+	}
+	return (
+		<div className='mainContainer'>
+			  {useScript("https://cdn.addevent.com/libs/atc/1.6.1/atc.min.js")}
+			<div className='articleImage' /*style={divStyle}*/>
+				<div className='articleImageButtons'></div>
+			</div>
+
+			<div className='eventData'>
+				<h1 className='eventName'> {eventData.title} </h1>
+				<h4 className='eventDetails'>
+					<a href={websiteValidity ? orgWebsite : null}>{websiteValidity ? orgWebsite : 'There is no link'}</a>
+				</h4>
+				<div className='secondaryInfo'>
+					<div className='eventTimeContainer'>
+						<p className='eventDate'> Event Date: {eventData.date} </p>
+						<p className='eventStartTime'> Start Time: {eventData.startTime}</p>
+						<p className='eventEndTime'> End Time: {eventData.endTime}</p>
+					</div>
+					<div className='eventLocationContainer'>
+						<p className='eventStreet'> Street Name: {addressInfo.streetName}</p>
+						<p className='eventHouse'> House Number: {addressInfo.houseNumber}</p>
+						<p className='eventCity'> City: {addressInfo.city}</p>
+					</div>
+				</div>
+				<h4 className='eventDetails'> Description: {eventData.article}</h4>
+				<h3 className='username'> Signed / {eventData.hostName} </h3>
+			</div>
+
+			<div className='userPromptContainer'>
+				<h3 className='maxCap'> Max Capacity: {eventData.maxCapacity} </h3>
+				<div title='Add to Calendar' className='addeventatc'>
+						Add to Calendar
+						<span className='start'>{`${eventData.date}+${eventData.startTime}`}</span>
+						<span className='end'>{`${eventData.date}+${eventData.endTime}`}</span>
+						<span className='timezone'>Asia/Jerusalem</span>
+						<span className='title'>{eventData.title}</span>
+						<span className='description'>{eventData.article}</span>
+					</div>
+				<button className='shareButton'> Share </button>
+			</div>
+
+			<div className='contactUsContainer'>
+				<div className='contactUsContent'>
+					{/* <button className="closePopUp" onClick={hideOrganizerContact}>  &times; Close</button> */}
+					<p>Our Website: {websiteValidity ? orgWebsite : 'No Link Available'}</p>
+					<p>Our Phone Number: {contactInfo.phone}</p>
+					<p>Our Email Address: {contactInfo.email}</p>
+				</div>
+			</div>
+
+			<div className='eventTags'>
+				<h3 className='tagTitle'>Event Tags:</h3>
+				{tags.map((tags) => {
+					return (
+						<div className='tagBox' key={tags.id}>
+							{tags.tag}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default Event;
