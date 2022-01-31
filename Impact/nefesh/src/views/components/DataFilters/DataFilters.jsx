cb7ac58562f1deecc84300b80d5e1d25f41ca96a
SamuelSpiz <samuelspiz@yahoo.it>
2022-01-31 14:30:25 +0200

    cc

--- Impact/nefesh/src/views/components/DataFilters/DataFilters.jsx
+++ Impact/nefesh/src/views/components/DataFilters/DataFilters.jsx
@@ -1,4 +1,4 @@
-
+import "./DataFilters.css";
 import { useEffect, useState } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { db } from "../../../functions/firebase/config";
