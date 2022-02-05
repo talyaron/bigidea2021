cd96e6c7392694ffa63f3c37b884d1b414443e27
Max Shroyer <Maxshroyer49@gmail.com>
2022-02-05 22:10:46 +0200

    Half done

--- Impact/nefesh/src/views/pages/Event.jsx
+++ Impact/nefesh/src/views/pages/Event.jsx
@@ -4,6 +4,8 @@ import { getDoc, doc } from 'firebase/firestore';
 import { db } from '../../scripts/firebase/config';
 import { useParams } from 'react-router-dom';
 import useScript from '../../scripts/useScript';
+import Clock from '../../assets/Images/NewIcons/clock.svg'
+import { async } from '@firebase/util';
 // import 'moment-timezone';
 
 function Event() {
@@ -14,17 +16,18 @@ function Event() {
 	const [contactInfo, setContactInfo] = useState([]);
 	const [orgWebsite, setOrgWebsite] = useState([]);
 	const [websiteValidity, setWebValidity] = useState(false);
-	//const [imageValidity, setImgValidity] = useState(false);
+	const [eventDataValid, SetEventDataValid] = useState(false)
+	
 	let { eventID } = useParams();
 	//let navigate = useNavigate();
 	const [image, setImage] = useState();
 	//const [filterObjectData,setfilterData]=useState([]);
-	useEffect(() => {
+	useEffect(async() => {
 		try {
 			setWebValidity(false);
 			let eventRef = doc(db, 'events', eventID);
 			let eventObj;
-			getDoc(eventRef).then((docSnap) => {
+			await getDoc(eventRef).then((docSnap) => {
 				eventObj = docSnap.data();
 
 				let { startTime, endTime, address } = eventObj;
@@ -52,11 +55,13 @@ function Event() {
 				let validState = validURL(eventObj.contactInfo.website);
 				setWebValidity(validState);
 				setOrgWebsite(eventObj.contactInfo.website);
+				SetEventDataValid(true)
 			});
 		} catch (err) {
 			console.error(err);
 		}
 	}, [eventID]);
+
 
 	function filterEntries(data) {
 		if (data[0].length === 0) return [];
@@ -103,65 +108,72 @@ function Event() {
 
 		if (!formatted) formatted = val.data;
 		return formatted;
-	}
+	} 
 
 	return (
-		<div className='mainContainer_Event'>
+		<div className='EventPage'>
+		
+		<div id='mainContainer_Event'>
 			{useScript('https://cdn.addevent.com/libs/atc/1.6.1/atc.min.js')}
-			<img className='coverImage' src={image} alt='Event'></img>
-			<div className='eventData_Event'> 
-			<div key="0">
-				<div key="1" className='title'> { getField(eventData, "title") } </div>
-				<h3 key="2" className='username_Event'> { getField(eventData, "hostName") } </h3>
+			<img id='coverImage_Event' src={image} alt='Event'></img>
+			<div className='eventData_Event'>
+				<div id='title_Event'> {eventData.title} </div>
+				<div id='hostName_Event'>
+					Hosted by: {eventData.hostName} 
 				</div>
-				<div className='dataBox'>
-					{Object.entries(filterEntries([eventData, EventFilter])).map((e) => (
-						<div className='dataEntry' id={e[1][1][1] + 'Entry'}>
-							{e[1][2][1] ? <div className='dataLabel' id={e[1][1][1] + 'Label'}>
-								{e[1][2][1]}
-							</div> : null}
-							<div className='dataField' id={e[1][1][1] + 'Field'}>
-								{formatField(...e)}
+				<div className='eventTimesCont'>
+					{Object.entries(filterEntries([eventData, ['startTime', 'endTime']])).map((e) => (
+						<div className='dataEntry' key={e[0]} id={e[0] + 'Entry'}>
+							<img id='clock' src={Clock} alt="clock" /> 
+							<div className='dataField' id={e[0] + 'Field'}>
+								{e[0]}: {formatField(...e)}
 							</div>
 						</div>
 					))}
 				</div>
-				<div className='eventDetails_Event'>
+				<div id='eventWebsite_Event'>
 					<a href={websiteValidity ? orgWebsite : null}>{websiteValidity ? orgWebsite : 'There is no link'}</a>
 				</div>
-
-				<h4 className='eventDetails_Event'> Description: { getField(eventData, "article") } </h4>
+					{/* {eventDataValid ? 
+					<div id='eventAddress'>
+						<div className='inlineBlock'>{eventData.address[2][1]}</div>
+						<div className='inlineBlock'>{eventData.address[0][1]}</div>
+						<div className='inlineBlock'>{eventData.address[1][1]}</div>
+					</div>
+					: null} */}
+				<div id='eventDescription_Event'>{eventData.article}</div>
+				
 			</div>
-
-			<div className='userPromptContainer_Event'>
-				<h3 className='maxCap'> Max Capacity: { getField(eventData, "maxCapacity") } </h3>
-				<div title='Add to Calendar' className='addeventatc'>
-					Add to Calendar
-					<span className='start'>{`${ getField(eventData, "startTime") }`}</span>
-					<span className='end'>{`${ getField(eventData, "endTime") }`}</span>
-					<span className='timezone'>Asia/Jerusalem</span>
-					<span className='title'>{ getField(eventData, "title") }</span>
-					<span className='description'>{ getField(eventData, "article") }</span>
-				</div>
-				<button className='shareButton button_Event'> Share </button>
+			
+			<div className='maxCap__Event'> Max Capacity: {eventData.maxCapacity} </div>
+			<div title='Add to Calendar' className='addEventToCalender'>
+				Add to Calendar
+				<span className='start'>{`${eventData.startTime}`}</span>
+				<span className='end'>{`${eventData.endTime}`}</span>
+				<span className='timezone'>Asia/Jerusalem</span>
+				<span className='title'>{eventData.title}</span>
+				<span className='description'>{eventData.article}</span>
 			</div>
-
-			<div className='contactUsContainer_Event'>
-				<div className='contactUsContent_Event'>
-					<p>Our Phone Number: {contactInfo.phone}</p>
-					<a rel="noreferrer" href={`mailto: ${contactInfo.email} ?subject=Event!&body=Hi! I wanted to contact you to tell you that (type here)`} target='_blank'>
-						Email Us!
+				<div id='hostPhoneNumber_Event'>
+					{contactInfo.phone}
+					<a href={`mailto: ${contactInfo.email} ?subject=Event!&body=Hi! I wanted to contact you to tell you that (type here)`} target='_blank'>
+						Email {eventData.hostName}!
 					</a>
-				</div>
-			</div>
+					</div>
 			<div className='eventTags_Event'>
-				<h3 className='tagTitle_Event'>Event Tags:</h3>
 				{tags.map((tag, index) => {
 					return <tag key={`tag-${index}`}>{tag}</tag>;
 				})}
 			</div>
 		</div>
+	</div>
 	);
 }
 
 export default Event;
+
+
+
+
+
+
