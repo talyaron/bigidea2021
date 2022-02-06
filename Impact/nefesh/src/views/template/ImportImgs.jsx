15e8a7b8c74a0ec5bdd6000d5b41086cb803086c
Max Shroyer <Maxshroyer49@gmail.com>
2022-02-06 15:22:36 +0200

    push

--- Impact/nefesh/src/views/template/ImportImgs.jsx
+++ Impact/nefesh/src/views/template/ImportImgs.jsx
@@ -42,37 +42,12 @@ function HandleImportImg(props) {
 		console.log('Upload Successful!');
 	}
 
-	return <input type='file' name='articleImg' id='input_ArticleImg' accept='.jpg, .png, .gif, .tif' onChange={onTrigger} className='border-ArticleCreation' />;
+	return(
+		<input type='file' name='articleImg' id='input_ArticleImg' accept='.jpg, .png, .gif, .tif' onChange={onTrigger} className='border-ArticleCreation' />
+	)
+
 }
 
 export default HandleImportImg;
 
-// images.forEach((image, index) => {
-// 	console.log(image, index)
-// 	const imageName = `${uniqueId(2)}${image.name}`;
-// 	console.log(imageName)
-// 	if (!imageName) throw new Error('no name for image')
-// 	console.log(imageName)
-// 	const storageRef = ref(storage, `comparison/${userId}/${comparisonId}/${imageName}`);
-
-// 	const imageRef = doc(db, 'comparison', userId, comparisonId, `${imageName}`);
-
-// 	uploadBytes(storageRef, image).then(snapshot => {
-// 		console.log(snapshot)
-
-// 		getDownloadURL(storageRef).then(httpRef => {
-// 			console.log(httpRef)
-// 			tempObj.images[index] = httpRef;
-
-// 			setDoc(imageRef, { imageSrc: httpRef }).then(() => {
-
-// 				readyArray.push('ready');
-// 				if (readyArray.length === images.length && setIsRedirect) {
-// 					setIsRedirect(true);
-
-// 					console.log(tempObj)
-// 					setDoc(compareRef, tempObj, { merge: true });
-// 				}
-// 			})
-
-// 		})
+// 
