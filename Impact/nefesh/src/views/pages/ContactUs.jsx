582d5ce0f6f83c27f555267ba82c330a05598155
Tacoman611 <91414112+Tacoman611@users.noreply.github.com>
2022-02-06 11:44:37 +0200

    hi

--- Impact/nefesh/src/views/pages/ContactUs.jsx
+++ Impact/nefesh/src/views/pages/ContactUs.jsx
@@ -19,40 +19,24 @@ function ContactUs(props) {
   }, [mailTo, props]);
 
   return (
-    <div>
-      <div className="head">Contact us at:</div>
-      <div className="box-1">
-        <div className="phoneNum">Phone number: ###-###-####</div>
-      </div>
-      <div className="box-1">
-        <a
-          href="mailto:Nefesh@nefesh.com?subject=Contacting Creators!&body=Hi! I wanted to contact you to tell you that (type here)"
-          className="email">
-          Click here to send us an email!
-        </a>
-      </div>
-      {userIsOle? (
-      <div className="apply">
-        Apply for an organizational profile: &nbsp;
-        <div className="apbutton">
-          <div className="box-1">
-            <a href={`${mailTo}`} target="_blank" rel="noreferrer">
-              Click here to apply
-            </a>
-          </div>
-        </div>
-      </div>) : (null) }
+    <div className="back-color">
+      <div className="head">About us</div>
+        <div className="line"> </div>
       <div className="mission">
         <span className="mishead"> Our Mission </span>
-        <br />
+        <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        mollit anim id est laborum.</p>
       </div>
+      <div className="head sp">Contact us</div>
+        <div className="line"> </div>
+
+      
     </div>
   );
 }
