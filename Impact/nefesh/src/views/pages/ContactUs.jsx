9bbd6030e81c15baf573f7cd58bd6532a632e1ac
ykonsta <yoav.konsta@gmail.com>
2022-02-07 05:53:43 -0800

    iuhikjh

--- Impact/nefesh/src/views/pages/ContactUs.jsx
+++ Impact/nefesh/src/views/pages/ContactUs.jsx
@@ -1,13 +1,18 @@
 import '../../styles/page/ContactUs.css';
 import Envelope from '../../assets/Images/NewIcons/email.svg';
 import Phone from '../../assets/Images/NewIcons/phone.svg';
+import www from '../../assets/Images/NewIcons/www.svg';
+import home from '../../assets/Images/NewIcons/home.svg';
+import form from '../../assets/Images/NewIcons/form.svg';
 
 import { useEffect, useState } from 'react';
 
 function ContactUs(props) {
 	const [mailTo, setMailtoLink] = useState('');
 	const [userIsOle, setUserIsOle] = useState();
+	let body = '';
 	useEffect(() => {
+
 		console.log('users', props.uid, props.displayName, props.isOle);
 		//let notName = await getDoc(doc(db, "users", props.uid));
 		let userIDTemp = props.uid;
@@ -15,13 +20,22 @@ function ContactUs(props) {
 		setUserIsOle(props.isOle);
 		var email = 'Nefesh@nefesh.com';
 		var subject = 'Request to be Organization Admin';
-		var body_start = `${props.displayName} with User ID: ${userIDTemp} wants to be an organizational Admin because(input your reason here)`;
+		var body_start = `${props.displayName} with User ID: ${userIDTemp} wants to be an organizational Admin because (input your reason here)`;
+		body = body_start;
 		setMailtoLink(`mailto:` + email + `?subject=` + subject + `&body=` + body_start);
 		console.log(mailTo);
 	}, [mailTo, props]);
 
 	function handleEmail() {
-		window.location = 'mailto:Nefesh@nefesh.com?subject=Contacting Creators!';
+		window.location = `mailto:nbntlv@nbn.org.il?subject=Contact Support &body= Hi Nefesh B' Nefesh, my name is ${props.displayName} and I am contacting you with regards to (input reasoning here)`;
+	}
+
+	function handleOrganization(){
+		window.location = `mailto:nbntlv@nbn.org.il?subject=Request to Become An Organization &body=${body} `
+	}
+
+	function openLink(){
+		window.open('https://www.nbn.org.il/nbn-tlv-hub/')
 	}
 
 	return (
@@ -42,9 +56,9 @@ function ContactUs(props) {
 				</div>
 			) : null}
 			<div className='mission'>
-				<p>The NBN TLV Hub is a multidimensional community hub with a spacious, creative and homey co-working & event space designed with innovation and collaboration in mind.</p>
+				<p>Having opened in March 2017, the NBN TLV Hub is a multidimensional community hub with a spacious, creative and homey co-working & event space designed with innovation and collaboration in mind.</p>
 				<p>
-					Having opened in March 2017, the NBN TLV Hub is a multidimensional community hub with a spacious, creative and homey co-working & event space designed with innovation and collaboration in mind. The Hub bridges new Olim to the startup, career and community ecosystem of Tel Aviv, serving as
+					The Hub bridges new Olim to the startup, career and community ecosystem of Tel Aviv, serving as
 					a launching pad for their new lives in Israel. Career Advisors sit in the Hub to guide Olim as they navigate a new professional environment. The Hub hosts weekly social events to build community and business seminars and 1-1 professional consultations in accounting, banking, budgeting,
 					legal aspects of setting up a business, marketing, online engagement and business coaching.
 				</p>
@@ -52,24 +66,31 @@ function ContactUs(props) {
 			<div className='head sp'>Contact us</div>
 			<div className='line'> </div>
 
-			<div className='position'>
-				<div className='btn'>
-					<img src={Phone} alt='phone'/> &nbsp; 072-336-9280
+			
+		<div className = "buttonHolder">
+				<div className='button' >
+					<img src={Phone} alt='phone' id = 'phone'/> &nbsp; 072-336-9280
 				</div>
-				<div className='btn'  onClick={handleEmail}>
-					<img src={Envelope} alt='Envelope' /> Email Us
+				<div className='button' onClick={handleEmail}>
+					<img src={Envelope} alt='Envelope' id = 'email'/> nbntlv@nbn.org.il
 				</div>
+			<div className='button' onClick = {openLink}>
+				<img src={www} id = "www"/>https://www.nbn.org.il/nbn-tlv-hub/
 			</div>
-			<div className='btn'>
-				<img src={Phone} alt='phone'/>https://www.nbn.org.il/nbn-tlv-hub/
-			</div>
-      <div className='btn'>
-      <div className='location'>76 Rothschild BLVD Tel Aviv, Israel</div>
-      </div>
+      		<div className='button'>
+      			<img src={home} id = 'home' />76 Rothschild BLVD Tel Aviv, Israel
+      		</div>
+		</div>	
+
+			  
 			<div className='head sp'>Request to be a Host</div>
 			<div className='line'> </div>
+			
 			<div className='btns'>
-				<div className='btn'>Ask to be A Host</div>{' '}
+				<div className='btn' onClick = {handleOrganization}>
+					<img src = {form} id = 'form' />Be An Organization
+				
+				</div>
 			</div>
 			
 		</div>
