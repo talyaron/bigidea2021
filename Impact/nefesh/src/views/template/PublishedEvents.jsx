b45592a8c84fdb8272614ed5eb63e4e05e530f5a
L <skyd4b@gmail.com>
2022-02-07 16:14:59 +0200

    fixed css a little

--- Impact/nefesh/src/views/template/PublishedEvents.jsx
+++ Impact/nefesh/src/views/template/PublishedEvents.jsx
@@ -8,6 +8,7 @@ let page = 'ProfilePage';
 
 function SavedEvents(props) {
 	import('../../styles/page/ProfilePage.css');
+	import('../../styles/page/MainPage.css')
 	const [eventListState, setEventListState] = useState([]);
 
 	let SavedEventsTemp = []
