3dc702570f79d8c3efc56c78129882b94cb8b230
Max Shroyer <Maxshroyer49@gmail.com>
2022-02-07 14:20:57 +0200

    finished

--- Impact/nefesh/src/views/pages/ArticleCreation.jsx
+++ Impact/nefesh/src/views/pages/ArticleCreation.jsx
@@ -166,6 +166,7 @@ function ArticleCreation(props) {
 								);
 							})}
 						</div>
+						</div>
 						<label htmlFor="unselected_tagBox">Unselected Tags:</label>
 						<div name='unselected_tagBox' className='unselected_tagBox'>
 							<div className='tagsMapContainer'>
@@ -182,7 +183,7 @@ function ArticleCreation(props) {
 								})}
 							</div>
 						</div>
-					</div>
+					
 					<div className='buttonContainer23'>
 						<button className='Dragon42 shadow' onClick={saveDraft}>
 							Save Draft
