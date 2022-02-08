de8cbb0da5cad9b8cc2673d5dbc4dc4bb8cbebc9
L <skyd4b@gmail.com>
2022-02-08 23:12:20 +0200

    again

--- Impact/nefesh/src/views/pages/ArticleCreation.jsx
+++ Impact/nefesh/src/views/pages/ArticleCreation.jsx
@@ -101,34 +101,35 @@ function ArticleCreation(props) {
 			navigate('/MainPage')
 		} catch (err) {
 			console.error(err)
-			// create and show the notification
-			const showNotification = () => {
-				// create a new notification
-				const notification = new Notification('Error', {
-					body: 'Not All Fields Were Filled',
-				});
-				// close the notification after 10 seconds
-				setTimeout(() => {
-					notification.close();
-				}, 10 * 1000);
-			};
-			// show an error message
-			const showError = () => {
-				// const error = document.querySelector('.error');
-				// error.style.display = 'block';
-				// error.textContent = 'You blocked the notifications';
-				alert('Notifications disabled');
-			};
-			// check notification permission
-			let granted = false;
-			if (Notification.permission === 'granted') {
-				granted = true;
-			} else if (Notification.permission !== 'denied') {
-				let permission = await Notification.requestPermission();
-				granted = permission === 'granted' ? true : false;
-			}
-			// show notification or error
-			granted ? showNotification() : showError();
+			alert("Not all fields were submitted")
+			// // create and show the notification
+			// const showNotification = () => {
+			// 	// create a new notification
+			// 	const notification = new Notification('Error', {
+			// 		body: 'Not All Fields Were Filled',
+			// 	});
+			// 	// close the notification after 10 seconds
+			// 	setTimeout(() => {
+			// 		notification.close();
+			// 	}, 10 * 1000);
+			// };
+			// // show an error message
+			// const showError = () => {
+			// 	// const error = document.querySelector('.error');
+			// 	// error.style.display = 'block';
+			// 	// error.textContent = 'You blocked the notifications';
+			// 	alert('Notifications disabled');
+			// };
+			// // check notification permission
+			// let granted = false;
+			// if (Notification.permission === 'granted') {
+			// 	granted = true;
+			// } else if (Notification.permission !== 'denied') {
+			// 	let permission = await Notification.requestPermission();
+			// 	granted = permission === 'granted' ? true : false;
+			// }
+			// // show notification or error
+			// granted ? showNotification() : showError();
 		}
 	}
 	function saveDraft() {
